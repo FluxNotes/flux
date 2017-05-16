2eee7b71095f9c553ab54a6db4924095df8f6ff0
Nicole ng <nng@mitre.org>
2017-05-16 13:51:31 -0400

    Removed logo from example code. Started adding in data to the summary panel (added hard coded staging information)

--- src/DataSummary.jsx
+++ src/DataSummary.jsx
@@ -1,5 +1,5 @@
 // React imports
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 // Material UI component imports
 import TextField from 'material-ui/TextField';
@@ -15,45 +15,63 @@ import './DataSummary.css';
 
 class DataSummary extends Component {
 
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.handleHER2StatusChange = this.handleHER2StatusChange.bind(this)
-  }
+        this.handleHER2StatusChange = this.handleHER2StatusChange.bind(this)
+    }
 
-  handleHER2StatusChange (newStatus) { 
-    console.log(this);
-    this.props.onHER2StatusChange(newStatus);
-  }
+    handleHER2StatusChange(newStatus) {
+        console.log(this);
+        this.props.onHER2StatusChange(newStatus);
+    }
 
 
+    render() {
 
-  render() {
-  	const HER2StatusString = `HER2 Status: ${this.props.HER2Status}`;
-  	const ERStatusString   = `ER Status: ${this.props.ERStatus}`;
-  	const PRStatusString   = `PR Status: ${this.props.PRStatus}`;
-  	const HER2CheckboxChecked = (this.props.HER2Status !== "");
-  	const ERCheckboxChecked = (this.props.ERStatus !== "");
-  	const PRCheckboxChecked = (this.props.PRStatus !== "");
+        // Current Staging
+        const StageString = 'Stage: IIA';
+        const StageSubElementsString = 'T2, N0, M0';
+        const StageCheckboxChecked = (this.props.StageStatus !== "");
 
-    return (
-      <div id="data-summary">
-        <List>
-           <Subheader>Summary of Key Data</Subheader>
-           <ListItem primaryText={HER2StatusString} leftCheckbox={<Checkbox checked={HER2CheckboxChecked} />} />
-           <ListItem primaryText={ERStatusString} leftCheckbox={<Checkbox checked={ERCheckboxChecked}/>} />
-           <ListItem primaryText={PRStatusString} leftCheckbox={<Checkbox checked={PRCheckboxChecked}/>} />
-	    </List>
-      </div>
-    );
-  }
+        // Pathology Results
+        const HER2StatusString = `HER2 Status: ${this.props.HER2Status}`;
+        const ERStatusString = `ER Status: ${this.props.ERStatus}`;
+        const PRStatusString = `PR Status: ${this.props.PRStatus}`;
+        const HER2CheckboxChecked = (this.props.HER2Status !== "");
+        const ERCheckboxChecked = (this.props.ERStatus !== "");
+        const PRCheckboxChecked = (this.props.PRStatus !== "");
+
+        return (
+            <div id="data-summary">
+                <List>
+                    <Subheader>Current Staging</Subheader>
+                    <ListItem primaryText={StageString} leftCheckbox={<Checkbox checked={StageCheckboxChecked}/>}/>
+                    <ListItem primaryText={StageSubElementsString} />
+                </List>
+                <List>
+                    <Subheader>Pathology Results</Subheader>
+                    <ListItem primaryText={HER2StatusString} leftCheckbox={<Checkbox checked={HER2CheckboxChecked}/>}/>
+                    <ListItem primaryText={ERStatusString} leftCheckbox={<Checkbox checked={ERCheckboxChecked}/>}/>
+                    <ListItem primaryText={PRStatusString} leftCheckbox={<Checkbox checked={PRCheckboxChecked}/>}/>
+                </List>
+                <List>
+                    <Subheader>Event Dates</Subheader>
+                    <ListItem primaryText={HER2StatusString} leftCheckbox={<Checkbox checked={HER2CheckboxChecked}/>}/>
+                    <ListItem primaryText={ERStatusString} leftCheckbox={<Checkbox checked={ERCheckboxChecked}/>}/>
+                    <ListItem primaryText={PRStatusString} leftCheckbox={<Checkbox checked={PRCheckboxChecked}/>}/>
+                </List>
+            </div>
+        );
+    }
 }
 
-DataSummary.propTypes = { 	
-	HER2Status: 		 PropTypes.string,
-	ERStatus:   		 PropTypes.string,
-	PRStatus:   		 PropTypes.string,
-	onHER2StatusChange:  PropTypes.func.isRequired,
+DataSummary.propTypes = {
+    StageStatus: PropTypes.string,
+    HER2Status: PropTypes.string,
+    ERStatus: PropTypes.string,
+    PRStatus: PropTypes.string,
+    onHER2StatusChange: PropTypes.func.isRequired,
 
 }
 
