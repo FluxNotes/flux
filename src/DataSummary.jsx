b7d02eac65807fad5ebfee3dfb316b438916df66
Nicole ng <nng@mitre.org>
2017-05-17 09:53:59 -0400

    Added staging and event dates to patient summary panel

--- src/DataSummary.jsx
+++ src/DataSummary.jsx
@@ -33,31 +33,49 @@ class DataSummary extends Component {
         const StageCheckboxChecked = (this.props.StageStatus !== "");
 
         // Pathology Results
-        const HER2StatusString = `HER2 Status: ${this.props.HER2Status}`;
-        const ERStatusString = `ER Status: ${this.props.ERStatus}`;
-        const PRStatusString = `PR Status: ${this.props.PRStatus}`;
+        const HGAString = 'HGA: HG2';
+        const HER2StatusString = `HER2 Status: +`;
+        const ERStatusString = `ER Status: +`;
+        const PRStatusString = `PR Status: +`;
+        // const HER2StatusString = `HER2 Status: ${this.props.HER2Status}`;
+        // const ERStatusString = `ER Status: ${this.props.ERStatus}`;
+        // const PRStatusString = `PR Status: ${this.props.PRStatus}`;
         const HER2CheckboxChecked = (this.props.HER2Status !== "");
         const ERCheckboxChecked = (this.props.ERStatus !== "");
         const PRCheckboxChecked = (this.props.PRStatus !== "");
+
+        // Event Dates
+        const DiagnosisDateString = 'Diagnosis: 05/16/2012';
+        const DiagnosisString = 'Breast Cancer - Stage IIA';
+        const SurgeryDateString = 'Surgery: 06/20/2012';
+        const SurgeryString = 'Lumpectomy/sentinel/lymph node biopsy';
+        const RecurrenceDateString = 'Recurrence: 08/02/2015';
+
 
         return (
             <div id="data-summary">
                 <List>
                     <Subheader>Current Staging</Subheader>
                     <ListItem primaryText={StageString} leftCheckbox={<Checkbox checked={StageCheckboxChecked}/>}/>
-                    <ListItem primaryText={StageSubElementsString} />
+                    <ListItem id="staging-subelements" primaryText={StageSubElementsString}/>
                 </List>
                 <List>
                     <Subheader>Pathology Results</Subheader>
-                    <ListItem primaryText={HER2StatusString} leftCheckbox={<Checkbox checked={HER2CheckboxChecked}/>}/>
-                    <ListItem primaryText={ERStatusString} leftCheckbox={<Checkbox checked={ERCheckboxChecked}/>}/>
-                    <ListItem primaryText={PRStatusString} leftCheckbox={<Checkbox checked={PRCheckboxChecked}/>}/>
+                    <ListItem primaryText={HGAString} leftCheckbox={<Checkbox checked={true}/>}/>
+                    <ListItem primaryText={HER2StatusString} leftCheckbox={<Checkbox checked={true}/>}/>
+                    <ListItem primaryText={ERStatusString} leftCheckbox={<Checkbox checked={true}/>}/>
+                    <ListItem primaryText={PRStatusString} leftCheckbox={<Checkbox checked={true}/>}/>
+                    {/*<ListItem primaryText={HER2StatusString} leftCheckbox={<Checkbox checked={HER2CheckboxChecked}/>}/>*/}
+                    {/*<ListItem primaryText={ERStatusString} leftCheckbox={<Checkbox checked={ERCheckboxChecked}/>}/>*/}
+                    {/*<ListItem primaryText={PRStatusString} leftCheckbox={<Checkbox checked={PRCheckboxChecked}/>}/>*/}
                 </List>
                 <List>
                     <Subheader>Event Dates</Subheader>
-                    <ListItem primaryText={HER2StatusString} leftCheckbox={<Checkbox checked={HER2CheckboxChecked}/>}/>
-                    <ListItem primaryText={ERStatusString} leftCheckbox={<Checkbox checked={ERCheckboxChecked}/>}/>
-                    <ListItem primaryText={PRStatusString} leftCheckbox={<Checkbox checked={PRCheckboxChecked}/>}/>
+                    <ListItem primaryText={DiagnosisDateString} leftCheckbox={<Checkbox checked={true}/>}/>
+                    <ListItem id="diagnosis" primaryText={DiagnosisString}/>
+                    <ListItem primaryText={SurgeryDateString} leftCheckbox={<Checkbox checked={true}/>}/>
+                    <ListItem primaryText={SurgeryString}/>
+                    <ListItem primaryText={RecurrenceDateString} leftCheckbox={<Checkbox checked={true}/>}/>
                 </List>
             </div>
         );
