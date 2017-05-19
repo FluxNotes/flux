99a6a407b7ef6bdb3fd83e808430566b715128c7
Carlton Duffett <cjduffett@comcast.net>
2017-05-19 11:14:47 -0400

    Added Staging Form Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-// React Imports: 
+// React Imports:
 import React, { Component } from 'react';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 // Material UI components:
@@ -10,13 +10,14 @@ import AppTopWithDrawer from './AppTopWithDrawer';
 import DemographicSummary from './DemographicSummary';
 import ClinicalNotes from './ClinicalNotes';
 import DataSummary from './DataSummary';
+import FormsPanel from './FormsPanel';
 
 import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
       HER2Status: '+',
       ERStatus:   '',
       PRStatus:   ''
@@ -27,27 +28,27 @@ class App extends Component {
 
   }
 
-  changeHER2Status(newStatus) { 
+  changeHER2Status(newStatus) {
     (newStatus !== "") && this.setState({
       HER2Status: newStatus
     })
   }
 
-  changeERStatus(newStatus) { 
+  changeERStatus(newStatus) {
     console.log('newStatus ' + newStatus);
     (newStatus !== "") && this.setState({
       ERStatus: newStatus
     })
   }
 
-  componentDidUpdate(a, b) { 
+  componentDidUpdate(a, b) {
     console.log('did update')
     console.log(a)
     console.log(b)
     console.log(this)
   }
 
-  changePRStatus(newStatus) { 
+  changePRStatus(newStatus) {
     (newStatus !== "") && this.setState({
       PRStatus: newStatus
     })
@@ -58,7 +59,11 @@ class App extends Component {
       <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
         <div className="App">
           <AppTopWithDrawer />
+<<<<<<< HEAD
           <Grid fluid className="App-content">
+=======
+          <Grid className="App-content" fluid>
+>>>>>>> Added Staging Form Component
             <Row>
               <Col xs={12}>
                 <DemographicSummary />
@@ -66,7 +71,7 @@ class App extends Component {
             </Row>
             <Row center="xs">
               <Col sm={3}>
-                <DataSummary 
+                <DataSummary
                   onHER2StatusChange={this.changeHER2Status}
                   onERStatusChange={this.changeERStatus}
                   onPRStatusChange={this.changePRStatus}
@@ -76,7 +81,7 @@ class App extends Component {
                 />
               </Col>
               <Col sm={6}>
-                <ClinicalNotes 
+                <ClinicalNotes
                   onHER2StatusChange={this.changeHER2Status}
                   onERStatusChange={this.changeERStatus}
                   onPRStatusChange={this.changePRStatus}
@@ -86,11 +91,11 @@ class App extends Component {
                 />
               </Col>
               <Col sm={3}>
-                <p> Placeholder </p>
+                <FormsPanel />
               </Col>
             </Row>
-          </Grid> 
-        </div>  
+          </Grid>
+        </div>
       </MuiThemeProvider>
     );
   }
