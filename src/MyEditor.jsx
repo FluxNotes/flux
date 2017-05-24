ecd5ab08951779ac2152f553cc7d04e028059678
Dylan Phelan <dtphelan1@gmail.com>
2017-05-24 16:18:59 -0400

    Able to detect when keypress happens in t-keyed sections

--- src/MyEditor.jsx
+++ src/MyEditor.jsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import { Editor, Block, Inline, Transform , Raw, Text, Html } from 'slate'
 import AutoReplace from 'slate-auto-replace'
-import { List } from 'immutable'
+import { List , Map } from 'immutable'
 
 // Styling
 import './MyEditor.css';
@@ -24,34 +24,77 @@ function charListFromString(str) {
   }
   return List(arr);
 }
-const stagingText = Text.createFromString('Staging ');
-const tText = Text.createFromString('T:_ ');
-const tBlock = Inline.create({
-  isVoid: false,  
-  key: 'T-staging',
-  type: 'span',
-  nodes: List([tText])
-});
-const nText = Text.createFromString('N:_ ')
-const nBlock = Inline.create({
-  isVoid: false,  
-  key: 'N-staging',
-  type: 'span',
-  nodes: List([nText])
-});
-const mText = Text.createFromString('M:_ ')
-const mBlock = Inline.create({
-  isVoid: false,  
-  key: 'M-staging',
-  type: 'span',
-  nodes: List([mText])
-});
-const bl = Inline.create({
-  key:"staging",
-  isVoid: false,
-  type: "span",
-  nodes:  List([stagingText, tBlock, nBlock, mBlock])
-});
+const test = Raw.deserialize({
+  "nodes": [
+    {
+      "kind": "block",
+      "type": "paragraph",
+      "isVoid": false,
+      "nodes": [
+        {
+          "kind": "text",
+          "ranges": [
+            {
+              "text": "Staging "
+            }
+          ]
+        },
+        {
+          "kind": "inline",
+          "type": "structured-span",
+          "data": {
+            "id": "t-staging",
+          },
+          "nodes": [
+            {
+              "kind": "text",
+              "ranges": [
+                {
+                  "text": "T_ "
+                }
+              ]
+            }
+          ]
+        }, 
+        {
+          "kind": "inline",
+          "type": "structured-span",
+          "data": {
+            "id": "n-staging",
+          },
+          "nodes": [
+            {
+              "kind": "text",
+              "ranges": [
+                {
+                  "text": "N_ "
+                }
+              ]
+            }
+          ]
+        }, 
+        {
+          "kind": "inline",
+          "type": "structured-span",
+          "data": {
+            "id": "m-staging",
+          },
+          "nodes": [
+            {
+              "kind": "text",
+              "ranges": [
+                {
+                  "text": "M_ "
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}, { terse: true });
+
 
 // Add the plugin to your set of plugins...
 const plugins = [
@@ -59,63 +102,32 @@ const plugins = [
     trigger: 'space',
     before: /(\.staging)/,
     transform: (transform, e, data, matches) => {
-      const newTrans = transform.insertInline(bl).moveToRangeOf(tBlock).moveStart(2).moveEnd(-1);
+      const stagingBlock = test.document.nodes.get(0);
+      const newTrans = transform.insertBlock(stagingBlock);
       return newTrans;
     }
   })
 ]
 
+// const BLOCK_TAGS = {
+//   p: 'paragraph',
+//   span: 'span',
+//   div: 'div',
+//   li: 'list-item',
+//   ul: 'bulleted-list',
+//   ol: 'numbered-list',
+//   blockquote: 'quote',
+//   h1: 'heading-one',
+//   h2: 'heading-two',
+// }
 
-const BLOCK_TAGS = {
-  p: 'paragraph',
-  span: 'span',
-  div: 'div',
-  li: 'list-item',
-  ul: 'bulleted-list',
-  ol: 'numbered-list',
-  blockquote: 'quote',
-  h1: 'heading-one',
-  h2: 'heading-two',
-}
-
-const MARK_TAGS = {
-  strong: 'bold',
-  em: 'italic',
-  u: 'underline',
-  s: 'strikethrough',
-  code: 'code'
-}
-
-const RULES = [
-  {
-    deserialize(el, next) {
-      const block = BLOCK_TAGS[el.tagName]
-      if (!block) return
-      return {
-        kind: 'block',
-        type: block,
-        nodes: next(el.children)
-      }
-    }, 
-    serialize(el, next) { 
-
-    }
-  },
-  {
-    deserialize(el, next) {
-      const mark = MARK_TAGS[el.tagName]
-      if (!mark) return
-      return {
-        kind: 'mark',
-        type: mark,
-        nodes: next(el.children)
-      }
-    }
-  }
-]
-const serializer = new Html({ rules: RULES });
-
-
+// const MARK_TAGS = {
+//   strong: 'bold',
+//   em: 'italic',
+//   u: 'underline',
+//   s: 'strikethrough',
+//   code: 'code'
+// }
 const initialState = Raw.deserialize({
   nodes: [
     {
@@ -124,15 +136,37 @@ const initialState = Raw.deserialize({
       nodes: [
         {
           kind: 'text',
-          text: 'A line of text in a paragraph.'
+          text: '.staging'
         }
       ]
     }
   ]
 }, { terse: true })
-function e(event, data, state) { 
-  // const newTrans = state.transform().moveToRangeOf(nBlock).moveStart(2).moveEnd(-1);
-  // console.log(newTrans)
+
+
+function getNodeById(nodes, id) { 
+  return nodes.find(function(v, k, iter) {
+    if (v.data) {
+      return v.data.get('id') === id;
+    } else { 
+      return false;
+    }
+  });
+}
+
+function getKeysForNode(curNode, keys) { 
+  console.log(curNode);
+  if(curNode.key) { 
+    console.log(`new key: ${curNode.key}`)
+    keys.push(curNode.key)
+  }
+  if (curNode.nodes) { 
+    for(const child of curNode.nodes) { 
+      console.log(`Checking child node: ${child}`);
+      keys = getKeysForNode(child, keys);
+    }
+  }
+  return keys;
 }
 
 // Define our app...
@@ -144,14 +178,16 @@ class MyEditor extends React.Component {
     schema: {
       nodes: {
         'paragraph':     props => <p {...props.attributes}>{props.children}</p>,
-        'span':          props => <span {...props.attributes}>{props.children}</span>,
+        'structured-span':          (props) => {
+          const id = (props.node) ? props.node.data.get('id') : '';
+          return <span id={id} {...props.attributes}>{props.children}</span>;
+        },
+        'span':           props => <span {...props.attributes}>{props.children}</span>,
         'div':           props => <div {...props.attributes}>{props.children}</div>,
         'list-item':     props => <li {...props.attributes}>{props.children}</li>,
         'bulleted-list': props => <ul {...props.attributes}>{props.children}</ul>,
         'numbered-list': props => <ol {...props.attributes}>{props.children}</ol>,
-        'block-quote':   props => <blockquote {...props.attributes}>{props.children}</blockquote>,
-        'heading-one':   props => <h1 {...props.attributes}>{props.children}</h1>,
-        'heading-two':   props => <h2 {...props.attributes}>{props.children}</h2>,
+        'block-quote':   props => <blockquote {...props.attributes}>{props.children}</blockquote>
       },
       marks: { 
         'bold':          props => <strong>{props.children}</strong>,
@@ -172,48 +208,41 @@ class MyEditor extends React.Component {
     //   console.log('abile to find');
     // }
     // state = state.transform().moveToRangeOf(nBlock).moveStart(2).moveEnd(-1).apply()
-    console.log(state)
+    // console.log(state)
+    // console.log(state.selection)
+    // console.log(state.selection.startKey)
+    // console.log(state.selection.startOffset)
+
+    // const docBlock = state.document.nodes.get(1);
+    // if(tNode) { 
+    //   console.log('in this transform')
+    // const nNode = getNodeById(docBlock.nodes, 'n-staging');
+    // const mNode = getNodeById(docBlock.nodes, 'm-staging');
+
+    //   state = state.transform().moveToRangeOf(tNode).moveStart(1).moveEnd(-1).apply();
+    // }
+
     this.setState({ state })
   }
 
   onKeyDown = (event, data, state) => {
-    if (!event.ctrlKey) {
-      switch (event.which) {
-        case 48: {
-          return e(event, data, state);
-        }  
-        case 49: {
-          return e(event, data, state);
-        } 
-        case 50: {
-          return e(event, data, state);
-        } 
-        case 51: {
-          return e(event, data, state);
-        } 
-        case 52: {
-          return e(event, data, state);
-        } 
-        case 53: {
-          return e(event, data, state);
-        } 
-        case 54: {
-          return e(event, data, state);
-        } 
-        case 55: {
-          return e(event, data, state);
-        } 
-        case 56: {
-          return e(event, data, state);
-        } 
-        case 57: {
-          return e(event, data, state);
-        } 
-        default: {  
-          break;
+    console.log('\n\nlooping...')
+    for(const parentNode of state.document.nodes) { 
+      const tNode = getNodeById(parentNode.nodes, 't-staging');
+      const nNode = getNodeById(parentNode.nodes, 'n-staging');
+      const mNode = getNodeById(parentNode.nodes, 'm-staging');
+
+      if(tNode) { 
+        const tKeys = getKeysForNode(tNode, []);
+        console.log(tKeys)
+        // console.log(tNode.key)
+        // console.log(state.selection.startKey)
+        if (state.selection.startKey in tKeys) { 
+          console.log('success')
         }
       }
-    } 
+    }
+    
   }
   // Render the editor.
   render = () => {
