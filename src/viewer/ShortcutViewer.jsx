bc9615f9b041d23c1acaec7c130b2100988cc6ad
Nicole ng <nng@mitre.org>
2017-07-11 11:15:36 -0400

    Added copy and restart buttons. Right now no implementation yet

--- src/viewer/ShortcutViewer.jsx
+++ src/viewer/ShortcutViewer.jsx
@@ -3,6 +3,8 @@ import React, {Component} from 'react';
 
 // material-ui
 import Paper from 'material-ui/Paper';
+import Divider from 'material-ui/Divider';
+import RaisedButton from 'material-ui/RaisedButton';
 // Styling
 import './ShortcutViewer.css';
 
@@ -15,13 +17,28 @@ class ShortcutViewer extends Component {
             string = "Choose a template from the left panel";
         }
         else {
-            if(this.props.currentShortcut) {
+            if (this.props.currentShortcut) {
                 string = this.props.currentShortcut.getAsString();
             }
         }
         return (
             <div id="shortcut-viewer" className="dashboard-panel">
                 <Paper className="panel-content trio">
+                    <div id="viewer-buttons">
+                        <RaisedButton
+                            className="btn_template"
+                            label="Copy"
+                            onClick={(e) => this._handleClick(e)}
+                        />
+
+                        <RaisedButton
+                            className="btn_template"
+                            label="Restart"
+                            onClick={(e) => this._handleClick(e)}
+                        />
+                    </div>
+
+                    <Divider className="divider"/>
                     <div>
                         {string}
                     </div>
@@ -29,6 +46,11 @@ class ShortcutViewer extends Component {
             </div>
         )
     }
+
+    _handleClick(e) {
+        console.log("clicked a button")
+    }
+
 }
 
 export default ShortcutViewer;
