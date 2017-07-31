b9853d5e244fa1ce99890f944ac57e0ba13ad905
Nicole ng <nng@mitre.org>
2017-07-31 12:56:33 -0400

    Set initial state of drop down to be placeholder text (this fixes the issue of not being able to select the first entry in the list when the first entry is the default value on initial page render)

--- src/structuredFieldComponents/DropdownStructuredComponent.jsx
+++ src/structuredFieldComponents/DropdownStructuredComponent.jsx
@@ -9,6 +9,7 @@ class DropdownStructuredComponent extends Component {
 		return (
 			<span className='sf-subfield' {...this.props.attributes}>
 					<select onFocus={this.props.handleDropdownFocus} onChange={this.props.handleDropdownSelection}>
+						<option selected disabled hidden>Select</option>
 						{this.props.items.map(function(item, index) {
 							return <option key={item} value={item}>{item}</option>;
 						})}
