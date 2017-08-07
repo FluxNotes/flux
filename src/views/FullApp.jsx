0ea323fca8fd9e6a7293a3991a17a5b2fdfae111
Nicole <nng@mitre.org>
2017-08-07 15:23:17 -0400

    Revert "Added functionality to switch title depending on if app is in patient…"

--- src/views/FullApp.jsx
+++ src/views/FullApp.jsx
@@ -385,7 +385,7 @@ class FullApp extends Component {
         return (
             <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
                 <div className="FullApp">
-                    <NavBar title="Flux Notes"/>
+                    <NavBar />
                     <Grid className="FullApp-content" fluid>
                         <Row center="xs">
                             <Col sm={4}>
