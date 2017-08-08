86d820cb62f9c43424a662904b1079f834088613
Nicole ng <nng@mitre.org>
2017-08-08 11:46:39 -0400

    Created new react component "FormList" for list items. Played around with styling slightly to start reflecting Involution mockups for Flux Notes Lite (decreases app width size, changed paper layout)

---
+++ src/viewer/ShortcutViewer.jsx
@@ -0,0 +1,63 @@
+// React imports
+import React, {Component} from 'react';
+import CopyToClipboard from 'react-copy-to-clipboard';
+
+// material-ui
+import Paper from 'material-ui/Paper';
+import Divider from 'material-ui/Divider';
+import RaisedButton from 'material-ui/RaisedButton';
+// Styling
+import './ShortcutViewer.css';
+
+class ShortcutViewer extends Component {
+
+    render() {
+        let string = "";
+
+        if (this.props.currentShortcut == null) {
+            string = "Choose a template from the left panel";
+        }
+        else {
+            if (this.props.currentShortcut) {
+                string = this.props.currentShortcut.getAsString();
+            }
+        }
+        return (
+            <div id="shortcut-viewer" className="dashboard-panel">
+
+
+					<CopyToClipboard text={string}>
+                        <RaisedButton
+                            className="btn_viewer"
+                            label="Copy"
+                            onClick={(e) => this._handleClick(e)}
+                        />
+					</CopyToClipboard>
+
+                        <RaisedButton
+                            className="btn_viewer"
+                            label="Reset"
+                            onClick={(e) => this._handleResetClick(e)}
+                        />
+
+
+                    <Divider className="divider"/>
+                    <div>
+                        {string}
+                    </div>
+
+            </div>
+        )
+    }
+
+    _handleClick(e) {
+        console.log("clicked copy button")
+    }
+
+    _handleResetClick(e) {
+        e.preventDefault();
+        this.props.onShortcutUpdate(null);
+    }
+}
+
+export default ShortcutViewer;
