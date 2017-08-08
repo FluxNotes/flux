bd4c1893d052403c096bbb7021107a7f2f651b58
Nicole ng <nng@mitre.org>
2017-08-08 11:46:39 -0400

    Created new react component "FormList" for list items. Played around with styling slightly to start reflecting Involution mockups for Flux Notes Lite (decreases app width size, changed paper layout)

--- src/views/SlimApp.jsx
+++ src/views/SlimApp.jsx
@@ -4,11 +4,13 @@ import {Grid, Row, Col} from 'react-flexbox-grid';
 // Material UI components:
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+import Paper from 'material-ui/Paper';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 // Application components:
 import NavBar from '../nav/NavBar';
 import ShortcutViewer from '../viewer/ShortcutViewer';
-import FormTray from '../forms/FormTray';
+// import FormTray from '../forms/FormTray';
+import FormList from '../forms/FormList';
 // Shortcut Classes
 import ProgressionShortcut from '../shortcuts/ProgressionShortcut';
 import ToxicityShortcut from '../shortcuts/ToxicityShortcut';
@@ -26,66 +28,64 @@ class SlimApp extends Component {
             currentShortcut: null
         };
     }
-	
+
     /* 
      * Change the current shortcut to be the new type of shortcut  
      */
     changeShortcut = (shortcutType) => {
         // console.log("structured field entered: " + field);
-        if (Lang.isNull(shortcutType)) {   
+        if (Lang.isNull(shortcutType)) {
             this.setState({
                 currentShortcut: null
             });
-        } else { 
-            switch (shortcutType.toLowerCase()) { 
-                case "progression": 
+        } else {
+            switch (shortcutType.toLowerCase()) {
+                case "progression":
                     this.setState({
                         currentShortcut: new ProgressionShortcut(this.handleShortcutUpdate)
                     });
                     break;
 
-                case "toxicity": 
+                case "toxicity":
                     this.setState({
                         currentShortcut: new ToxicityShortcut(this.handleShortcutUpdate)
                     });
                     break;
 
-                default: 
+                default:
                     console.error(`Error: Trying to change shortcut to ${shortcutType.toLowerCase()}, which is an invalid shortcut type`);
             }
         }
     }
-	
+
     /* 
      * Change the current shortcut to be the new type of shortcut  
      */
-	handleShortcutUpdate = (s) =>{
-	    console.log(`Updated: ${s}`);
+    handleShortcutUpdate = (s) => {
+        console.log(`Updated: ${s}`);
         (s !== "") && this.setState({currentShortcut: s});
     }
 
     render() {
-	
+
         return (
             <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
                 <div className="SlimApp">
                     <NavBar title="Flux Notes Lite"/>
                     <Grid className="SlimApp-content" fluid>
-                        <Row center="xs">
-                           <Col sm={5}>
-                              <FormTray
-									shortcuts={['progression', 'toxicity']}
-                                    currentShortcut={this.state.currentShortcut}
-									changeShortcut={this.changeShortcut}
-                                />
-                           </Col>
-                            <Col sm={7}>
-                                <ShortcutViewer
-                                    currentShortcut={this.state.currentShortcut}
-                                    onShortcutUpdate={this.handleShortcutUpdate}
-                                />
-                            </Col>
-                        </Row>
+                        <Paper className="panel-content trio">
+                            <Row center="xs">
+                                <Col sm={3}>
+                                    <FormList />
+                                </Col>
+                                <Col sm={9}>
+                                    <ShortcutViewer
+                                        currentShortcut={this.state.currentShortcut}
+                                        onShortcutUpdate={this.handleShortcutUpdate}
+                                    />
+                                </Col>
+                            </Row>
+                        </Paper>
                     </Grid>
                 </div>
             </MuiThemeProvider>
