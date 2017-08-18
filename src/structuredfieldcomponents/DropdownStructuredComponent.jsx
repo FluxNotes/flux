739e114eaedf4f4048e5757a0908f1f28d4e9b80
mtorchio <mtorchio@mitre.org>
2017-08-18 16:51:50 +0100

    Styled structured component and notified the SHortcut object of changes

--- src/structuredfieldcomponents/DropdownStructuredComponent.jsx
+++ src/structuredfieldcomponents/DropdownStructuredComponent.jsx
@@ -1,28 +1,12 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import Lang from 'lodash';
-//import Select from 'react-select';
+import Select from 'react-select';
 // Be sure to include styles at some point, probably during your bootstrapping
 import 'react-select/dist/react-select.css';
 
-var Select = require('react-select'); //dupe with import??
-
-/*var options = [
-  { value: 'Tis', label: 'Tis' },
-  { value: 'T0', label: 'T0' },
-  { value: 'T1', label: 'T1' },
-  { value: 'T2', label: 'T2' },
-  { value: 'T3', label: 'T3' },
-  //['Tis', 'T0', 'T1', 'T2', 'T3','T4'] is what we would get from StagingShortcut.getStructuredFieldSpecification()
-];*/
-
-// original onChange function mentioned in <Select onChange, from sample code
-function logChange(val) {
-  console.log("Selected: " + JSON.stringify(val));
-}
-
 class DropdownStructuredComponent extends Component {
 	_createSelectionHandler(name) {
-		console.log("_createSelectionHandler(): selected: " + JSON.stringify(name));
 		return function(evt) {
 			this.props.handleDropdownSelection(name, evt);
 		}.bind(this);
@@ -32,6 +16,12 @@ class DropdownStructuredComponent extends Component {
 		this.dropDown.value = value;
 	}
 
+	// Finds the Select element in the DOM and pulls data-key off its parent
+	onDropdownFocus(proxy, event){
+		const element = ReactDOM.findDOMNode(this.dropDown);
+		const dataKey = element.parentElement.getAttribute('data-key');
+		this.props.handleDropdownFocus(proxy, event, dataKey);
+	}
 
 
 	render = () => {
@@ -39,17 +29,18 @@ class DropdownStructuredComponent extends Component {
 		let value = this.props.value;
 		let items = this.props.items;
 		let dropdownOptions = this.props.items.map(function(item, index) {
-							return {value: item, label: item};}) // only gives 2 out of 3 ?
-		console.log(dropdownOptions);
+							return {value: item, label: item};})
 
 		return (        
-			<span className='sf-subfield' {...this.props.else} style={{width: "50"}}>
+			<span className='sf-subfield' {...this.props.else}>
 				<Select
+					data-offset-key={datakey}
+					ref={(input) => { this.dropDown = input; }}
 					name="form-field-name"
 					value="_____"
 					options={dropdownOptions}
-					onFocus={this.props.handleDropdownFocus} 
-					onChange={this._createSelectionHandler}
+					onFocus={this.onDropdownFocus.bind(this)} 
+					onChange={this._createSelectionHandler(this.props.name)}
 					/>
 			</span>
 		);
@@ -57,27 +48,3 @@ class DropdownStructuredComponent extends Component {
 } 
 
 export default DropdownStructuredComponent;
-/* inside return span:
-<select data-offset-key={datakey} 
-							defaultValue='Select' 
-							ref={(input) => { this.dropDown = input; }}
-							onFocus={this.props.handleDropdownFocus} 
-							onChange={this._createSelectionHandler(this.props.name)}>
-						<option disabled hidden>Select</option>
-						{this.props.items.map(function(item, index) {
-							return <option key={item} value={item}>{item}</option>;
-						})}
-					</select> 
-					----------------------------------------------
-
-					<select data-offset-key={datakey} 
-							value={this.props.value}
-							ref={(input) => { this.dropDown = input; }}
-							onFocus={this.props.handleDropdownFocus} 
-							onChange={this.createSelectionHandler(this.props.name)}>
-						{this.props.items.map(function(item, index) {
-							return <option key={item} value={item}>{item}</option>;
-						})}
-					</select>
-
-*/
