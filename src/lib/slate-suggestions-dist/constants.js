41db0d8ce2c1cac17728a2a8a8af6c8a0847b5c7
Dylan Phelan <dtphelan1@gmail.com>
2017-08-28 11:55:31 -0400

    Suggestion to insert now aligned with dropdown and text on page

--- src/lib/slate-suggestions-dist/constants.js
+++ src/lib/slate-suggestions-dist/constants.js
@@ -1,9 +1,4 @@
-"use strict"; // eslint-disable-line
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var UP_ARROW_KEY = exports.UP_ARROW_KEY = 38; // eslint-disable-line
-var DOWN_ARROW_KEY = exports.DOWN_ARROW_KEY = 40;// eslint-disable-line
-var ENTER_KEY = exports.ENTER_KEY = 13;// eslint-disable-line
-var RESULT_SIZE = exports.RESULT_SIZE = 5;// eslint-disable-line
+export const UP_ARROW_KEY = 38
+export const DOWN_ARROW_KEY = 40
+export const ENTER_KEY = 13
+export const RESULT_SIZE = 5
