de45a8989b987d9742ca7a750911617360b7b65e
Dylan Phelan <dtphelan1@gmail.com>
2017-09-27 09:17:29 -0400

    Added test of insertion buttons in data summary panel

--- test/ui/FullApp.js
+++ test/ui/FullApp.js
@@ -19,10 +19,24 @@ test('Typing an inserterShortcut in the editor results in a structured data inse
     await t
         .expect(structuredField.innerText)
         .contains(new Patient().getName());
-})
+});
 
 fixture('Patient Mode - Data Summary Panel') 
     .page(startPage);
+
+test('Clicking to insert a captured data element results in that text pasted into the editor', async t => { 
+    const editor = Selector("div[data-slate-editor='true']");
+    const summaryButtons = Selector("#summary-list div table .captured .button-hover")
+    const numButtons = await summaryButtons.count;
+
+    console.log(numButtons);
+    for(let i = 0; i < numButtons; i++) { 
+        await t
+            .click(summaryButtons.nth(i))
+            .expect(await editor.innerText)
+            .contains(await summaryButtons.nth(i).innerText);
+    }
+});
 
 fixture('Patient Mode - Timeline') 
     .page(startPage);
