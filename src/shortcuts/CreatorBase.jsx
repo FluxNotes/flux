996df55e76d0a0b3515a0f10b6d1b7a00910a0d8
Greg Quinn <gquinn@mitre.org>
2017-10-18 13:21:42 -0400

    work on implementing get and set of attribute values

--- src/shortcuts/CreatorBase.jsx
+++ src/shortcuts/CreatorBase.jsx
@@ -16,6 +16,15 @@ export default class CreatorBase extends Shortcut {
             this.isObjectNew = false;
         }
 		this.setValueObject(this.object);
+        
+        // get attribute descriptions
+        const metadataVOA = this.metadata["valueObjectAttributes"];
+        this.valueObjectAttributes = {};
+        metadataVOA.forEach((attrib) => {
+            attrib["attributePath"] = attrib["attribute"].split(".");
+            this.valueObjectAttributes[attrib.name] = attrib;
+        });
+		this.setAttributeValue = this.setAttributeValue.bind(this);
     }
     
 	initialize(contextManager) {
@@ -44,5 +53,58 @@ export default class CreatorBase extends Shortcut {
 
     getAsString() { 
         const structuredPhrase = this.metadata["structuredPhrase"];
+        //#staging #${T} #${N} #${M}
+        console.log("structured phrase: " + structuredPhrase);
+        let last = 0, valueName, value;
+        let start = structuredPhrase.indexOf("${"), end;
+        let result = "";
+        let haveAValue = false;
+        while (start !== -1) {
+            result += structuredPhrase.substring(last, start);
+            end = structuredPhrase.indexOf("}", start + 2);
+            valueName = structuredPhrase.substring(start + 2, end);
+            value = this.getAttributeValue(valueName);
+            if (value === '') {
+                value = '?';
+            } else {
+                haveAValue = true;
+            }
+            result += value;
+            last = end + 1;
+            start = structuredPhrase.indexOf("${", last);
+        }
+        if (last < structuredPhrase.length) {
+            result += structuredPhrase.substring(last);
+        }
+        if (!haveAValue) {
+            return this.text;
+        }
+        return result;
     }
+
+	getAttributeValue(name) {
+        const voa = this.valueObjectAttributes[name];
+        const attributePath = voa["attributePath"];
+        const len = attributePath.length;
+        let i;
+        let result = this.valueObject;
+        for (i = 0; i < len; i++) {
+            result = result[attributePath[i]];
+        }
+        return result;
+    }
+
+	setAttributeValue(name, value, publishChanges = true) {
+        const voa = this.valueObjectAttributes[name];
+        if (Lang.isUndefined(voa)) throw new Error("Unknown attribute '" + name + "' for structured phrase '" + this.text + "'");
+        const setObject = voa["setObject"];
+        const setMethod = voa["setMethod"];
+        console.log("invoke method '" + setMethod + "' on object '" + setObject + "'");
+        setObject[setMethod](this.valueObject, value);
+        if (this.isContext()) this.updateContextStatus();
+		if (this.onUpdate) this.onUpdate(this);
+		if (publishChanges) {
+			this.notifyValueChangeHandlers(name);
+		}
+	}
 }
