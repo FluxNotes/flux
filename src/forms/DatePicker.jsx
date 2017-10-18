ad7afc6053c8737db4e2213ea8ae83b289c5e761
Nicole ng <nng@mitre.org>
2017-10-18 13:00:06 -0400

    Started creating a separate date picker component

---
+++ src/forms/DatePicker.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import moment from 'moment';
+import DayPickerInput from 'react-day-picker/DayPickerInput';
+import 'react-day-picker/lib/style.css';
+import './DatePicker.css';
+
+
+class DatePicker extends Component {
+
+}
+
+export default DatePicker;
