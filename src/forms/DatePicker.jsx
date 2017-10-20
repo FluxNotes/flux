2ca725fb766ab0194ffdb5b0ebaab3a4641c8b4f
Nicole ng <nng@mitre.org>
2017-10-20 08:44:13 -0400

    Added comments to DatePicker component

--- src/forms/DatePicker.jsx
+++ src/forms/DatePicker.jsx
@@ -4,6 +4,7 @@ import DayPickerInput from 'react-day-picker/DayPickerInput';
 import 'react-day-picker/lib/style.css';
 import './DatePicker.css';
 
+// Changing DATE_FORMAT updates how the date is displayed in the date picker
 const DATE_FORMAT = 'MM/DD/YYYY';
 
 class DatePicker extends Component {
@@ -17,6 +18,7 @@ class DatePicker extends Component {
         }
     }
 
+    // When user selects a date via the date picker, the date is passed back up to the parent component
     _handleDateChange(selectedDate) {
         this.props.handleDateChange(selectedDate);
     }
@@ -26,6 +28,8 @@ class DatePicker extends Component {
         const {selectedDate} = this.state;
         const formattedDate = selectedDate ? moment(selectedDate).format(DATE_FORMAT) : '';
 
+        // dateToSet is the date user wants to set as a default date
+        // If no date is passed into the date picker component, the default date is placeholder text (the date format)
         if (this.props.dateToSet != null) {
             formattedDateToSet = new moment(this.props.dateToSet, "D MMM YYYY ").format(DATE_FORMAT);
         } else {
