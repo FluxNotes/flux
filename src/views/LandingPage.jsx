24f1cd9708a3239ca44721b29b819c08f435d890
Julia Afeltra <jafeltra@mitre.org>
2017-10-20 13:58:43 -0400

    Added padding between each section

--- src/views/LandingPage.jsx
+++ src/views/LandingPage.jsx
@@ -13,49 +13,59 @@ class LandingPage extends Component {
                     
                     <Divider className="divider"/>
                     <h3><img src="./logos/ICARE.png" alt="ICARE Data Study" className="landing-logo" height="22px"></img>ICARE Data Study</h3>
-                    <p>
-                        Flux Notes supports the ICARE data study capture of treatment data during the
-                        authoring of clinical notes. The goal of the ICARE data study is to support the collection of
-                        high quality real-world data (RWD) in a way that enables clinical oncology research.
-                    </p>
+                    <div className="section">
+                        <p>
+                            Flux Notes supports the ICARE data study capture of treatment data during the
+                            authoring of clinical notes. The goal of the ICARE data study is to support the collection of
+                            high quality real-world data (RWD) in a way that enables clinical oncology research.
+                        </p>
+                    </div>
                     
                     <h3><img src="./logos/FluxNotesLogo.png" alt="Flux Notes" className="landing-logo" height="22px"></img>About Flux Notes</h3>
-                    <p> 
-                        The purpose of Flux Notes Lite is to help clinicians capture disease status and toxicity 
-                        information in a structured data format that can be easily analyzed and shared 
-                        across multiple clinical care sites.
-                    </p>
-                    <p>
-                        This application provides an easy mechanism for clinicians to generate disease status and toxicity
-                        information via an interface and allow them to copy the resulting structured shorthand phrase and
-                        paste it into their EHR notes entry.
-                    </p>
+                    <div className="section">
+                        <p> 
+                            The purpose of Flux Notes Lite is to help clinicians capture disease status and toxicity 
+                            information in a structured data format that can be easily analyzed and shared 
+                            across multiple clinical care sites.
+                        </p>
+                        <p>
+                            This application provides an easy mechanism for clinicians to generate disease status and toxicity
+                            information via an interface and allow them to copy the resulting structured shorthand phrase and
+                            paste it into their EHR notes entry.
+                        </p>
+                    </div>
                     
                     <h3><img src="./logos/SHR.png" alt="SHR" className="landing-logo" height="22px"></img>Standard Health Record</h3>
-                    <p>
-                        This tool is one of multiple prototypes in development by MITRE with the end goal of providing
-                        complete, accurate, and computable records to inform the best care for each individual.
-                    </p>
-                    <p>
-                        This application leverages the <a href="http://standardhealthrecord.org/" target="_blank" id="shr-link">
-                        SHR (Standard Health Record)</a> format for capturing data.
-                    </p>
+                    <div className="section">
+                        <p>
+                            This tool is one of multiple prototypes in development by MITRE with the end goal of providing
+                            complete, accurate, and computable records to inform the best care for each individual.
+                        </p>
+                        <p>
+                            This application leverages the <a href="http://standardhealthrecord.org/" target="_blank" id="shr-link">
+                            SHR (Standard Health Record)</a> format for capturing data.
+                        </p>
+                    </div>
                     
                     <Divider className="divider"/>
                     <h3>Clinical Trials</h3>
-                    <p>
-                        For the PATINA clinical trial endpoints, click <a href="/patina" id="patina-link">here</a>
-                    </p>
+                    <div className="section">
+                        <p>
+                            For the PATINA clinical trial endpoints, click <a href="/patina" id="patina-link">here</a>
+                        </p>
+                    </div>
                     
                     <Divider className="divider"/>
                     <h3>Partner Organanizations</h3>
-                    <p>
-                        <img src="./logos/Alliance.png" alt="Alliance for Clinical Trials in Oncology" className="partner-logo" height="50px"></img>
-                        <img src="./logos/ASCO.png" alt="American Society of Clinical Oncology" className="partner-logo" height="50px"></img>
-                        <img src="./logos/BWH.png" alt="Brigham and Women's Hospital" className="partner-logo" height="50px"></img>
-                        <img src="./logos/DanaFarber.png" alt="Dana-Farber Cancer Institute" className="partner-logo" height="50px"></img>
-                        <img src="./logos/MITRE.png" alt="MITRE Corporation" className="partner-logo mitre-logo" height="20px"></img>
-                    </p>
+                    <div className="section">
+                        <p>
+                            <img src="./logos/Alliance.png" alt="Alliance for Clinical Trials in Oncology" className="partner-logo" height="50px"></img>
+                            <img src="./logos/ASCO.png" alt="American Society of Clinical Oncology" className="partner-logo" height="50px"></img>
+                            <img src="./logos/BWH.png" alt="Brigham and Women's Hospital" className="partner-logo" height="50px"></img>
+                            <img src="./logos/DanaFarber.png" alt="Dana-Farber Cancer Institute" className="partner-logo" height="50px"></img>
+                            <img src="./logos/MITRE.png" alt="MITRE Corporation" className="partner-logo mitre-logo" height="20px"></img>
+                        </p>
+                    </div>
                 </div>
             </div>
         )
