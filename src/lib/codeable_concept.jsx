ba13f58e4f60ca9676457541e28cb7587d9e6d62
Dan Lee <daniellee@mitre.org>
2017-10-26 14:03:10 -0400

    Added function to codeable concept lib to get CodeableConcept object from possible value and list of options.

--- src/lib/codeable_concept.jsx
+++ src/lib/codeable_concept.jsx
@@ -1,7 +1,11 @@
+import Lang from 'lodash/lang';
 import CodeableConcept from '../model/shr/core/CodeableConcept';
 import Coding from '../model/shr/core/Coding';
 import DisplayText from '../model/shr/core/DisplayText';
 
+/*
+ *  Converts a tuple with the properties value, codeSystem, and displayText into a CodeableConcept object
+ */
 exports.getCodeableConceptFromTuple = (tuple) => {
     let codeableConcept = new CodeableConcept();
 
@@ -15,4 +19,29 @@ exports.getCodeableConceptFromTuple = (tuple) => {
     codeableConcept.displayText = displayText;
 
     return codeableConcept;
+}
+
+/*
+ * Function returns a CodeableConcept object when given a possibleValue and a list of options
+ * possibleValue is a string
+ * options is a list of objects with properties name, codeSystem, code
+ */
+exports.getCodeableConceptFromOptions = (possibleValue, options) => {
+    const index = options.findIndex((option) => { return option.name.toLowerCase() === possibleValue.toLowerCase()});
+    let tuple = {
+        value: "",
+        codeSystem: "",
+        displayText: ""
+    };
+
+    let value = index === -1 ? null : options[index];
+    if(!Lang.isNull(value)) {
+        tuple = {
+            value: value.code,
+            codeSystem: value.codeSystem,
+            displayText: value.name
+        };
+    }
+
+    return exports.getCodeableConceptFromTuple(tuple);
 }
