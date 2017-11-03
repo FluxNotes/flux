4d6dc512465e1eaf4d62715593f3304b64912102
Dan Lee <daniellee@mitre.org>
2017-11-03 13:20:47 -0400

    Initial FluxProgression implementation.

---
+++ src/model/wrapper/FluxProgression.js
@@ -0,0 +1,55 @@
+import Progression from '../shr/oncology/Progression';
+// import CodeableConcept from '../core/CodeableConcept';
+// import Coding from '../core/Coding';
+// import DisplayText from '../core/DisplayText';
+import Evidence from '../observation/Evidence';
+import lookup from '../../lib/progression_lookup.jsx';
+
+// FluxProgression class to hide codeableconcepts
+class FluxProgression extends Progression {
+    constructor(json) {
+        super(json);
+    }
+
+    /**
+     *  Getter for status
+     *  This will return the displayText string from CodeableConcept Value
+     */
+    get status() {
+        return this._codeableConcept.coding[0].displayText.value;
+    }
+
+    /**
+     *  Setter for status
+     *  The setter method is expecting a status sting
+     *  The method will lookup the corresponding coding/codesystem and set the _codeableConcept property
+     */
+    set status(status) {
+        this._codeableConcept = lookup.getStatusCodeableConcept(status);
+    }
+
+    /**
+     *  Getter for reasons
+     *  This will return an array of displayText strings from Evidence array
+     */
+    get evidence() {
+        return this._evidence.map((e) => {
+            return e.value.displayText.value;
+        });
+    }
+
+    /**
+     *  Setter for reasons
+     *  The method is expecting an array of reason strings
+     *  The method will lookup the corresponding coding/codesystem and set the evidence array
+     */
+    set evidence(evidence) {
+        this._evidence = evidence.map((e) => {
+            let ev = new Evidence();
+            ev.value = lookup.getEvidenceCodeableConcept(e);   
+            return ev;
+        });
+    }
+}
+
+export default FluxProgression;
