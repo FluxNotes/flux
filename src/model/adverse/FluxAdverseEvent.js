14b0664a55627890e2817ec4fdb97136a200bab3
Dan Lee <danlee1025@gmail.com>
2017-11-06 20:12:28 -0500

    Added comments and tests for FluxAdverseEvent

--- src/model/adverse/FluxAdverseEvent.js
+++ src/model/adverse/FluxAdverseEvent.js
@@ -18,28 +18,55 @@ class FluxAdverseEvent extends AdverseEvent{
         }
     }
 
+    /*
+     *  Getter for adverseEvent
+     *  This will return the displayText string from CodeableConcept value
+     */
     get adverseEvent() {
         return this._codeableConcept.coding[0].displayText.value;
     }
 
+    /*
+     *  Setter for adverseEvent
+     *  This function is expecting an adverseEvent string
+     *  This function will lookup the corresponding coding/codesystem and set the _codeableConcept property
+     */
     set adverseEvent(adverseEvent) {
         this._codeableConcept = lookup.getAdverseEventCodeableConcept(adverseEvent);
     }
 
+    /*
+     *  Getter for adverseEventGrade
+     *  This will return the displayText string from adverseEventGrade value
+     */
     get adverseEventGrade() {
         return this._adverseEventGrade.value.coding[0].displayText.value;
     }
 
+    /*
+     *  Setter for adverseEventGrade
+     *  This function is expecting an adverseEventGrade string
+     *  This function will lookup the corresponding coding/codesystem and set the _adverseEventGrade property
+     */
     set adverseEventGrade(grade) {
         let adverseEventGrade = new AdverseEventGrade();
         adverseEventGrade.value = lookup.getAdverseEventGradeCodeableConcept(grade);
         this._adverseEventGrade = adverseEventGrade;
     }
 
+    /*
+     *  Getter for causeCategory
+     *  This will return the displayText string from causeCategory value
+     */
     get causeCategory() {
         return this._causeCategory.value.coding[0].displayText.value;
     }
 
+    /*
+     *  Setter for causeCategory
+     *  This function is expecting an causeCategory string
+     *  This function will lookup the corresponding coding/codesystem and set the _causeCategory property
+     */
     set causeCategory(causeCategory) {
         let c = new CauseCategory();
         c.value = lookup.getAttributionCodeableConcept(causeCategory);
