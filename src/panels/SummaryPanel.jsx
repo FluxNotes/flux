5609ec771629a08227f5d640f8459eaa482c60bf
Dylan Phelan <dtphelan1@gmail.com>
2017-11-28 14:12:24 -0500

    downloaded minimap

--- src/panels/SummaryPanel.jsx
+++ src/panels/SummaryPanel.jsx
@@ -2,28 +2,34 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import DataSummaryPanel from '../summary/DataSummaryPanel';
 import TimelinePanel from '../timeline/TimelinePanel';
+import Minimap from 'react-minimap';
 import './SummaryPanel.css';
 
 class SummaryPanel extends Component { 
     render () { 
         return (
             <div>
-                <DataSummaryPanel
-                    isWide={this.props.isWide}
-                    patient={this.props.appState.patient}
-                    condition={this.props.appState.condition}
-                    summaryMetadata={this.props.appState.summaryMetadata}
-                    onItemClicked={this.props.handleSummaryItemSelected}
-                    allowItemClick={true}
-                />
-                <h1 className="panel-heading">
-                    Timeline 
-                </h1>
-                <TimelinePanel
-                    isWide={this.props.isWide}
-                    patient={this.props.appState.patient} 
-                    condition={this.props.appState.condition}
-                />
+                <Minimap selector=".panel-heading">
+                    <h1 className="panel-heading">
+                        Condition Summary
+                    </h1>
+                    <DataSummaryPanel
+                        isWide={this.props.isWide}
+                        patient={this.props.appState.patient}
+                        condition={this.props.appState.condition}
+                        summaryMetadata={this.props.appState.summaryMetadata}
+                        onItemClicked={this.props.handleSummaryItemSelected}
+                        allowItemClick={true}
+                    />
+                    <h1 className="panel-heading">
+                        Timeline 
+                    </h1>
+                    <TimelinePanel
+                        isWide={this.props.isWide}
+                        patient={this.props.appState.patient} 
+                        condition={this.props.appState.condition}
+                    />
+                </Minimap>
             </div>
         );
     }
