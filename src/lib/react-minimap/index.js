eab42179fbc2dd80f8bb11d0d7672e435272b73c
Dylan Phelan <dtphelan1@gmail.com>
2017-12-05 15:01:32 -0500

    made small changes to make minimap library more inline with original

--- src/lib/react-minimap/index.js
+++ src/lib/react-minimap/index.js
@@ -1,2 +1,2 @@
-export default Minimap from './react-minimap.js';
-export {default as Child} from './components/Child';
+export default Minimap from './react-minimap.js'
+export {default as Child} from './components/Child'
