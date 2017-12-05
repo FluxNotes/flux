fcd02566b5f638a3fa0a5ab14fb33e9615a523bf
Nicole ng <nng@mitre.org>
2017-12-05 09:58:55 -0500

    Added functionality to display total clinical notes and adds a "show more notes" button if not all the notes are rendered. Fixed some css styling

--- src/notes/NoteAssistant.jsx
+++ src/notes/NoteAssistant.jsx
@@ -3,9 +3,10 @@ import Divider from 'material-ui/Divider';
 import Paper from 'material-ui/Paper';
 import ContextTray from '../context/ContextTray';
 import Button from '../elements/Button';
+import Select from 'material-ui/Select';
+import MenuItem from 'material-ui/Menu/MenuItem';
 
 import './NoteAssistant.css';
-
 
 class NoteAssistant extends Component {
 
@@ -13,26 +14,54 @@ class NoteAssistant extends Component {
         super(props);
 
         this.state = {
-            noteAssistantMode: "context-tray"
+            noteAssistantMode: "context-tray",
+            sortIndex: null,
+            maxNotesToDisplay: 4,
+            notesNotDisplayed: null,
+            notesToDisplay: []
         };
     }
 
+    componentWillMount() {
+
+        // Set default value for sort selection
+        this.selectSort(0);
+
+        // Generate notesToDisplay array which will be used to render the notes in clinical notes view
+        let allNotes = this.props.patient.getNotes();
+        for (let i = 0; i < this.state.maxNotesToDisplay; i++) {
+            this.state.notesToDisplay.push(allNotes[i]);
+        }
+
+        // Set the number of notes that are not being displayed
+        let missingNotes = allNotes.length - this.state.maxNotesToDisplay;
+
+        this.setState({ notesNotDisplayed: missingNotes });
+    }
+
+    // Switch view. Currently only switching from context tray to clinical notes is available
+    // Cannot currently switch from clinical notes view back to current note view
     toggleView() {
-        this.setState(
-            {
-                noteAssistantMode: "clinical-notes"
-            }
-        );
+        this.setState({ noteAssistantMode: "clinical-notes" });
+    }
+
+    selectSort(sortIndex) {
+        this.setState({ sortIndex: sortIndex });
     }
 
     test() {
-        console.log("clicked on paper");
+        console.log("clicked on new note");
     }
 
+    // Render the content for the Note Assistant panel
     renderNoteAssistantContent(noteAssistantMode) {
+
+        const numberOfPreviousNotes = this.props.patient.getNotes().length;
 
         switch (noteAssistantMode) {
             case "context-tray":
+
+                // Render the context tray
                 return (
                     <div>
                         <span className="button-hover clinical-notes-btn" onClick={() => {
@@ -46,21 +75,32 @@ class NoteAssistant extends Component {
                         />
                     </div>
                 );
+
+            // Render the clinical notes view which includes new note button, resume note button,
+            // number of previous notes label, sort selection, and preview of previous notes
             case "clinical-notes":
                 return (
                     <div className="clinical-notes-panel">
                         <Paper className="note-new" onClick={() => {
                             this.test()
                         }}>
-                            {this.renderNoteSVG()}
-                            <span><i className="fa fa-plus" aria-hidden="true"></i>New note</span>
+                            <span><i className="fa fa-plus" aria-hidden="true"></i> New note</span>
+                            {/*{this.renderNewNoteSVG()}*/}
                         </Paper>
-
                         <Button raised
                                 className="resume-note-btn"
                         >Resume in-progress note</Button>
 
-                        {this.renderedNotes()}
+                        <span className="previous-notes-label">{numberOfPreviousNotes} previous notes</span>
+
+                        {this.renderSortSelection()}
+
+                        {this.renderNotes()}
+
+                        {this.renderMoreNotesButton()}
+
+                        {/*<span*/}
+                            {/*className="view-more-notes-label">Notes not displayed: {this.state.notesNotDisplayed}</span>*/}
 
                     </div>
                 );
@@ -71,13 +111,29 @@ class NoteAssistant extends Component {
         }
     }
 
-    renderedNotes() {
-        return this.props.patient.getNotes().map((item, i) =>
-            <Paper key={i} className="note" onClick={() => {
-                this.test()
-            }}>
+    renderSortSelection() {
+        return (
+            <div className="sort-selection">
+                <span className="sort-label">Sort by </span>
+                <Select
+                    className="sort-select"
+                    value={this.state.sortIndex}
+                    onChange={(event) => this.selectSort(event.target.value)}
+                >
+                    <MenuItem className="sort-item" value={0}>Date</MenuItem>
+                    <MenuItem className="sort-item" value={1}>Subject</MenuItem>
+                    <MenuItem className="sort-item" value={2}>Hospital</MenuItem>
+                </Select>
+            </div>
+        );
+    }
 
-                {this.renderNoteSVG()}
+    renderNotes() {
+
+        return this.state.notesToDisplay.map((item, i) =>
+            <Paper key={i} className="note">
+
+                {/*{this.renderClinicalNoteSVG()}*/}
 
                 <table className="notes-table">
                     <tbody>
@@ -85,7 +141,7 @@ class NoteAssistant extends Component {
                         <td className="existing-note-date" width="15%">{item.date}</td>
                     </tr>
                     <tr>
-                        <td className="existing-note-date" id="existing-note-subject">{item.subject}</td>
+                        <td className="existing-note-subject" id="existing-note-subject">{item.subject}</td>
                     </tr>
                     <Divider />
                     <tr>
@@ -100,30 +156,61 @@ class NoteAssistant extends Component {
         );
     }
 
-    renderNoteSVG() {
+    renderMoreNotesButton() {
+        if (this.state.notesNotDisplayed > 0) {
+            return (
+                <Button raised
+                        className="more-notes-btn"
+
+                >View {this.state.notesNotDisplayed} more clinical note(s)</Button>
+            );
+        }
+
+    }
+
+    renderClinicalNoteSVG() {
         return (
-            <svg width="160" height="17">
-                <defs>
-                    <filter id="f1" x="0" y="0" width="200%" height="200%">
-                        <feOffset result="offOut" in="SourceGraphic" dx="20" dy="20"/>
-                        <feBlend in="SourceGraphic" in2="offOut" mode="normal"/>
-                    </filter>
-                </defs>
-                <rect x="144" y="0" rx="2" ry="2" width="17" height="17"
-                      fill="white" stroke="#999" strokeWidth="1" opacity="0.5" filter="url(#f1)"/>
-                <line x1="145" y1="0" x2="200" y2="52" stroke="#999" strokeWidth="1" filter="url(#f1)"/>
+            <svg width="160" height="160">
+
+                <rect x="143" y="0" rx="2" ry="2" width="17" height="17"
+                      fill="white" stroke="#999" strokeWidth="1" opacity="0.5"/>
+                <line x1="143" y1="0" x2="200" y2="52" stroke="#999" strokeWidth="1"/>
+
+                <text x="20" y="20" fontFamily="sans-serif" fontSize="20px" fill="red">Date</text>
+                <text x="20" y="40" fontFamily="sans-serif" fontSize="20px" fill="red">note subject</text>
+
+                <line x1="10" y1="50" x2="150" y2="50" stroke="#999" strokeWidth="1"/>
+                <text x="20" y="60" fontFamily="sans-serif" fontSize="20px" fill="red">hospital</text>
+            </svg>
+        );
+    }
+
+    renderNewNoteSVG() {
+        return (
+            <svg width="11px" height="11px" viewBox="0 0 11 11" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd" strokeLinecap="square">
+                    <g id="Group-Copy-3" transform="translate(0.660429, 0.452719)" stroke="#16253E"
+                       strokeWidth="1.5552">
+                        <path d="M0.465836188,4.96173944 L8.70583619,4.96173944" id="Line"></path>
+                        <path d="M4.59517238,1.04666266 L4.59517238,9.28624923" id="Line-Copy"></path>
+                    </g>
+                </g>
+                <text x="0" y="0" fontFamily="sans-serif" fontSize="20px" fill="red">New note</text>
             </svg>
         );
     }
 
     render() {
 
+        // If the note viewer is editable then we want to be able to edit notes and view the context tray
         if (this.props.isNoteViewerEditable) {
             return (
                 <div>
                     {this.renderNoteAssistantContent(this.state.noteAssistantMode)}
                 </div>
             );
+
+            // If the note viewer is read only the we want to be able to view the clinical notes
         } else {
             return (
                 <div>
