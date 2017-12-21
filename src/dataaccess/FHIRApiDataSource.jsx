d0db7b8722c6453b27543e5f5d7ed962957d4085
Dan Lee <daniellee@mitre.org>
2017-12-21 08:28:44 -0500

    Removed unused import and added hardcodedfhirpatient.

--- src/dataaccess/FHIRApiDataSource.jsx
+++ src/dataaccess/FHIRApiDataSource.jsx
@@ -1,18 +1,14 @@
 import IDataSource from './IDataSource';
 import PatientRecord from '../patient/PatientRecord';
 import request from 'sync-request';
-import axios from 'axios';
 
 class FHIRApiDataSource extends IDataSource {
-    constructor() { 
-        super();
-    }
     getPatient(id) {
         const url = 'https://syntheticmass.mitre.org/fhir/Patient/58b3663e3425def0f0f6a229';
         console.log(url);
         const res = request('GET', url);
         console.log(res.getBody());
-
+        return new PatientRecord(null);
     }
     getListOfPatients() {
         console.error("listing of patients is not implemented in fhirapidataSource.");
