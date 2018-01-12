a5d57ad04374d2f4b038f6066f7a82624549fd21
Nicole ng <nng@mitre.org>
2018-01-12 15:36:17 -0500

    Initial commit. Created range chart using svg. Currently displaying the chart in each section for testing purposes. Currently using hard coded values

--- src/summary/TargetedDataSection.jsx
+++ src/summary/TargetedDataSection.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import Button from '../elements/Button';
 import TabularListVisualizer from './TabularListVisualizer'; //ordering of these lines matters
@@ -7,6 +7,8 @@ import NarrativeNameValuePairsVisualizer from './NarrativeNameValuePairsVisualiz
 import BandedLineChartVisualizer from './BandedLineChartVisualizer';
 import ProgressionLineChartVisualizer from './ProgressionLineChartVisualizer';
 import TimelineEventsVisualizer from '../timeline/TimelineEventsVisualizer';
+
+
 import './TargetedDataSection.css';
 
 class TargetedDataSection extends Component {
@@ -22,18 +24,18 @@ class TargetedDataSection extends Component {
             chosenVisualizer: null
         };
     }
-    
+
     componentDidUpdate() {
         const optionsForSection = this.getOptions(this.props.section);
         const defaultOrTabular = optionsForSection.length > 0 ? optionsForSection[0] : 'tabular';
         if (this.state.defaultVisualizer !== defaultOrTabular ||
             (this.state.chosenVisualizer !== null && !optionsForSection.includes(this.state.chosenVisualizer))) {
-            this.setState({defaultVisualizer: defaultOrTabular, chosenVisualizer: null });
+            this.setState({defaultVisualizer: defaultOrTabular, chosenVisualizer: null});
         }
     }
 
     handleViewChange = (chosenVisualizer) => {
-        this.setState({ chosenVisualizer });
+        this.setState({chosenVisualizer});
     }
 
     checkVisualization = () => {
@@ -52,11 +54,16 @@ class TargetedDataSection extends Component {
         return (
             <svg width="17px" height="17px" viewBox="0 0 17 17" version="1.1" xmlns="http://www.w3.org/2000/svg">
                 <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd" opacity="0.8">
-                    <g id="Group-4-Copy" transform="translate(0.884726, 0.440969)" stroke={strokeColor} strokeWidth="1.62" fill="#FFFFFF">
-                        <rect id="Rectangle-9-Copy-7" x="0.445109978" y="0.73198638" width="7.21076743" height="7.21833702"></rect>
-                        <rect id="Rectangle-9-Copy-8" x="0.445109978" y="8.26516734" width="7.21076743" height="7.21833702"></rect>
-                        <rect id="Rectangle-9-Copy-9" x="7.81454794" y="0.73198638" width="7.21076743" height="7.21833702"></rect>
-                        <rect id="Rectangle-9-Copy-10" x="7.81454794" y="8.26516734" width="7.21076743" height="7.21833702"></rect>
+                    <g id="Group-4-Copy" transform="translate(0.884726, 0.440969)" stroke={strokeColor}
+                       strokeWidth="1.62" fill="#FFFFFF">
+                        <rect id="Rectangle-9-Copy-7" x="0.445109978" y="0.73198638" width="7.21076743"
+                              height="7.21833702"></rect>
+                        <rect id="Rectangle-9-Copy-8" x="0.445109978" y="8.26516734" width="7.21076743"
+                              height="7.21833702"></rect>
+                        <rect id="Rectangle-9-Copy-9" x="7.81454794" y="0.73198638" width="7.21076743"
+                              height="7.21833702"></rect>
+                        <rect id="Rectangle-9-Copy-10" x="7.81454794" y="8.26516734" width="7.21076743"
+                              height="7.21833702"></rect>
                     </g>
                 </g>
             </svg>
@@ -68,7 +75,8 @@ class TargetedDataSection extends Component {
         const strokeColor = visualization === "narrative" ? "#3F3F3F" : "#CCCCCC";
         return (
             <svg width="17px" height="15px" viewBox="0 0 17 15" version="1.1" xmlns="http://www.w3.org/2000/svg">
-                <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd" strokeLinecap="square" opacity="0.8">
+                <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd" strokeLinecap="square"
+                   opacity="0.8">
                     <g id="Group-3-Copy" transform="translate(0.567421, 0.048197)" stroke={strokeColor} strokeWidth="2">
                         <path d="M1.03162221,1 L7.83111001,1" id="Line-4"></path>
                         <path d="M1.03162221,7 L15.1251513,7" id="Line-4-Copy"></path>
@@ -87,23 +95,29 @@ class TargetedDataSection extends Component {
             <svg width="17px" height="17px" viewBox="0 0 17 17" version="1.1" xmlns="http://www.w3.org/2000/svg">
                 <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
                     <g id="Group-39" stroke={strokeColor} strokeWidth="1.8">
-                        <path d="M0.936953125,0.9428125 L0.936953125,15.8228125 L15.8169531,15.8228125 L15.8169531,0.9428125 L0.936953125,0.9428125 Z" id="Rectangle-3"></path>
-                        <polyline id="Path-3" strokeLinejoin="round" points="0.71875 11.0977783 5.125 6.69152832 9.5 11.2852783 12.34375 7.97277832 15.625 11.3477783"></polyline>
+                        <path
+                            d="M0.936953125,0.9428125 L0.936953125,15.8228125 L15.8169531,15.8228125 L15.8169531,0.9428125 L0.936953125,0.9428125 Z"
+                            id="Rectangle-3"></path>
+                        <polyline id="Path-3" strokeLinejoin="round"
+                                  points="0.71875 11.0977783 5.125 6.69152832 9.5 11.2852783 12.34375 7.97277832 15.625 11.3477783"></polyline>
                     </g>
                 </g>
             </svg>
         );
     }
 
-    lineChartView = () => { 
+    lineChartView = () => {
         const visualization = this.checkVisualization();
         const strokeColor = visualization === "chart" ? "#3F3F3F" : "#CCCCCC";
         return (
             <svg width="17px" height="17px" viewBox="0 0 17 17" version="1.1" xmlns="http://www.w3.org/2000/svg">
                 <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
                     <g id="Group-39" stroke={strokeColor} stroke-width="1.62">
-                        <path d="M0.936953125,0.9428125 L0.936953125,15.8228125 L15.8169531,15.8228125 L15.8169531,0.9428125 L0.936953125,0.9428125 Z" id="Rectangle-3"></path>
-                        <polyline id="Path-3" stroke-linejoin="round" points="0.71875 11.0977783 5.125 6.69152832 9.5 11.2852783 12.34375 7.97277832 15.625 11.3477783"></polyline>
+                        <path
+                            d="M0.936953125,0.9428125 L0.936953125,15.8228125 L15.8169531,15.8228125 L15.8169531,0.9428125 L0.936953125,0.9428125 Z"
+                            id="Rectangle-3"></path>
+                        <polyline id="Path-3" stroke-linejoin="round"
+                                  points="0.71875 11.0977783 5.125 6.69152832 9.5 11.2852783 12.34375 7.97277832 15.625 11.3477783"></polyline>
                     </g>
                 </g>
             </svg>
@@ -121,7 +135,7 @@ class TargetedDataSection extends Component {
         } else if (type === 'chart') {
             icon = this.lineChartView();
         }
-        
+
         if (icon !== null) {
             return (
                 <Button
@@ -150,7 +164,7 @@ class TargetedDataSection extends Component {
             }
         } else if (section.type === "Events") {
             options.push('graphic');
-        } else if (section.type === "ValueOverTime") { 
+        } else if (section.type === "ValueOverTime") {
             options.push('chart');
         } else if (section.type === "DiseaseStatusValues") {
             options.push('chart');
@@ -190,6 +204,7 @@ class TargetedDataSection extends Component {
                             isWide={isWide}
                         />
                     );
+
                 } else {
                     return null;
                 }
@@ -221,8 +236,8 @@ class TargetedDataSection extends Component {
                     return null;
                 }
             }
-            case 'ValueOverTime': { 
-                if (visualization === 'chart') { 
+            case 'ValueOverTime': {
+                if (visualization === 'chart') {
                     return (
                         <BandedLineChartVisualizer
                             patient={patient}
@@ -233,12 +248,12 @@ class TargetedDataSection extends Component {
                             isWide={isWide}
                         />
                     );
-                } else { 
+                } else {
                     return null;
                 }
             }
-            case 'DiseaseStatusValues' : { 
-                if (visualization === 'chart') { 
+            case 'DiseaseStatusValues' : {
+                if (visualization === 'chart') {
                     return (
                         <ProgressionLineChartVisualizer
                             patient={patient}
@@ -249,9 +264,9 @@ class TargetedDataSection extends Component {
                             isWide={isWide}
                         />
                     );
-                } else { 
+                } else {
                     return null;
-                }   
+                }
             }
             case 'Events': {
                 if (visualization === 'graphic') {
@@ -272,6 +287,29 @@ class TargetedDataSection extends Component {
         }
     }
 
+    renderRangeChart() {
+
+        return (
+
+            <svg width="250" height="150">
+                  <text x="20" y="20" fontFamily="sans-serif" fontSize="20px" fill="black">Aromasin 25 mg qd</text>
+                  <line x1="10" y1="45" x2="205" y2="45" stroke="black" strokeWidth="0.5"  />
+                  <line x1="10" y1="35" x2="10" y2="55" stroke="black" strokeWidth="0.5"  />
+                  <text x="0" y="70" fontFamily="sans-serif" fontSize="14px" fill="black">5 mg</text>
+
+                  <line x1="205" y1="35" x2="205" y2="55" stroke="black" strokeWidth="0.5"  />
+                  <text x="195" y="70" fontFamily="sans-serif" fontSize="14px" fill="black">20 mg</text>
+
+                <line x1="65" y1="35" x2="65" y2="55" stroke="black" strokeWidth="0.5"  />
+                <text x="65" y="70" fontFamily="sans-serif" fontSize="14px" fill="black">10 mg</text>
+
+                <circle cx="220" cy="45" r="8" stroke="red" strokeWidth="3" fill="red" />
+
+
+            </svg>
+        );
+    }
+
     render() {
         const visualizationOptions = this.getOptions(this.props.section);
 
@@ -282,7 +320,11 @@ class TargetedDataSection extends Component {
                     {this.renderVisualizationOptions(visualizationOptions)}
                 </h2>
                 {this.renderSection(this.props.section)}
+
+                {this.renderRangeChart()}
             </div>
+
+
         );
     }
 }
