503fd0871e4d4cc2094397ae578d65630d909868
Julia Afeltra <jafeltra@mitre.org>
2018-01-30 16:11:51 -0500

    Update FluxTNMStage wrapper

--- src/model/oncology/FluxTNMStage.js
+++ src/model/oncology/FluxTNMStage.js
@@ -1,12 +1,11 @@
-// import CodeableConcept from '../shr/core/CodeableConcept';
-// import M_Stage from '../shr/oncology/M_Stage';
-// import N_Stage from '../shr/oncology/N_Stage';
-// import OccurrenceTime from '../shr/core/OccurrenceTime';
+import M_Stage from '../shr/oncology/M_Stage';
+import N_Stage from '../shr/oncology/N_Stage';
+import ClinicallyRelevantTime from '../shr/finding/ClinicallyRelevantTime';
 import TNMStage from '../shr/oncology/TNMStage';
-// import T_Stage from '../shr/oncology/T_Stage';
-// import Lang from 'lodash';
+import T_Stage from '../shr/oncology/T_Stage';
+import Lang from 'lodash';
 import lookup from '../../lib/tnmstage_lookup.jsx';
-// import staging from '../../lib/staging.jsx';
+import staging from '../../lib/staging.jsx';
 
 // FluxTNMStage class to hide codeableconcepts
 class FluxTNMStage {
@@ -24,7 +23,7 @@ class FluxTNMStage {
     /**
      *  Setter for staging
      *  This function is expecting a stage string
-     *  The function will lookup the corresponding coding/codesystem and set the _codeableConcept property
+     *  The function will lookup the corresponding coding/codesystem and set the TNMStage entry value property
      */
     set stage(stage) {
         this._tnmStage.value = lookup.getStagingCodeableConcept(stage);
@@ -32,118 +31,113 @@ class FluxTNMStage {
 
     /**
      *  Getter for t_Stage
-     *  This will return the displayText string from _t_Stage
+     *  This will return the displayText string from T_Stage
      */
-    // get t_Stage() {
-    //     return this._tnmStage.t_Stage.value.coding[0].displayText.value;
-    // }
+    get t_Stage() {
+        let tStage;
+        this._tnmStage._observationComponent.forEach(o => {
+            if (o instanceof T_Stage) {
+                tStage = o;
+            }
+        });
+        return tStage.value.coding[0].displayText.value;
+    }
 
-    // /*
-    //  *  Setter for t_Stage
-    //  *  This function expecting a tStage string
-    //  *  This function will lookup the corresponding coding/codesystem and set the _t_Stage property
-    //  */
-    // set t_Stage(tStage) {
-    //     let t = new T_Stage();
-    //     t.value = lookup.getTStageCodeableConcept(tStage);
-    //     this._t_Stage = t;
-    //     this._calculateStage();
-    // }
+    /*
+     *  Setter for T_Stage
+     *  This function expecting a tStage string
+     *  This function will lookup the corresponding coding/codesystem and set the T_Stage value on the observationComponent property
+     */
+    set t_Stage(tStage) {
+        let t = new T_Stage();
+        t.value = lookup.getTStageCodeableConcept(tStage);
+        this._tnmStage._observationComponent.push(t);
+        this._calculateStage();
+    }
     
-    // /**
-    //  *  Getter for n_Stage
-    //  *  This will return the displayText string from _n_Stage
-    //  */
-    // get n_Stage() {
-    //     return this._n_Stage.value.coding[0].displayText.value;
-    // }
+    /**
+     *  Getter for N_Stage
+     *  This will return the displayText string from N_Stage
+     */
+    get n_Stage() {
+        let nStage;
+        this._tnmStage._observationComponent.forEach(o => {
+            if (o instanceof N_Stage) {
+                nStage = o;
+            }
+        });
+        return nStage.value.coding[0].displayText.value;
+    }
 
-    // /*
-    //  *  Setter for n_Stage
-    //  *  This function expecting a nStage string
-    //  *  This function will lookup the corresponding coding/codesystem and set the _n_Stage property
-    //  */
-    // set n_Stage(nStage) {
-    //     let n = new N_Stage();
-    //     n.value = lookup.getNStageCodeableConcept(nStage);
-    //     this._n_Stage = n;
-    //     this._calculateStage();
-    // }
+    /*
+     *  Setter for N_Stage
+     *  This function expecting a nStage string
+     *  This function will lookup the corresponding coding/codesystem and set the N_Stage value on the observationComponent property
+     */
+    set n_Stage(nStage) {
+        let n = new N_Stage();
+        n.value = lookup.getNStageCodeableConcept(nStage);
+        this._tnmStage._observationComponent.push(n);
+        this._calculateStage();
+    }
 
-    // /**
-    //  *  Getter for m_Stage
-    //  *  This will return the displayText string from _m_Stage
-    //  */
-    // get m_Stage() {
-    //     return this._m_Stage.value.coding[0].displayText.value;
-    // }
+    /**
+     *  Getter for M_Stage
+     *  This will return the displayText string from M_Stage
+     */
+    get m_Stage() {
+        let mStage;
+        this._tnmStage._observationComponent.forEach(o => {
+            if (o instanceof M_Stage) {
+                mStage = o;
+            }
+        });
+        return mStage.value.coding[0].displayText.value;
+    }
 
-    // /*
-    //  *  Setter for m_Stage
-    //  *  This function expecting a mStage string
-    //  *  This function will lookup the corresponding coding/codesystem and set the _m_Stage property
-    //  */
-    // set m_Stage(mStage) {
-    //     let m = new M_Stage();
-    //     m.value = lookup.getMStageCodeableConcept(mStage);
-    //     this._m_Stage = m;
-    //     this._calculateStage();
-    // }
+    /*
+     *  Setter for M_Stage
+     *  This function expecting a mStage string
+     *  This function will lookup the corresponding coding/codesystem and set the M_Stage value on the observationComponent property
+     */
+    set m_Stage(mStage) {
+        let m = new M_Stage();
+        m.value = lookup.getMStageCodeableConcept(mStage);
+        this._tnmStage._observationComponent.push(m);
+        this._calculateStage();
+    }
 
-    // /*
-    //  * Getter for status
-    //  * This will return the value string from _status
-    //  */
-    // get status() {
-    //     if (this._status) {
-    //         return this._status.value;
-    //     } else {
-    //         return "";
-    //     }
-    // }
+    /*
+     * Getter for clinicallyRelevantTime
+     * This will return the value string from _clinicallyRelevantTime
+     */
+    get clinicallyRelevantTime() {
+        if (Lang.isUndefined(this._tnmStage._clinicallyRelevantTime)) return null;
+        return this._tnmStage._clinicallyRelevantTime.value;
+    }
 
-    // /*
-    //  * Setter for status
-    //  * This function is expecting a status string
-    //  * This function will create a new Status object and set the value to status
-    //  */
-    // set status(status) {
-    //     let s = new Status();
-    //     s.value = status;
-    //     this._status = s;
-    // }
-
-    // /*
-    //  * Getter for occurrenceTime
-    //  * This will return the value string from _occurrenceTime
-    //  */
-    // get occurrenceTime() {
-    //     if (Lang.isUndefined(this._occurrenceTime)) return null;
-    //     return this._occurrenceTime.value;
-    // }
-
-    // /*
-    //  * Setter for occurrenceTime
-    //  * This function is expecting an occurrenceTime string
-    //  * This function will create a new OccurrenceTime object and set the value to occurrenceTime
-    //  */
-    // set occurrenceTime(occurrenceTime) {
-    //     let o = new OccurrenceTime();
-    //     o.value = occurrenceTime;
-    //     this._occurrenceTime = o;
-    // }
+    /*
+     * Setter for clinicallyRelevantTime
+     * This function is expecting an clinicallyRelevantTime string
+     * This function will create a new ClinicallyRelevantTime object and set the value to clinicallyRelevantTime
+     */
+    set clinicallyRelevantTime(clinicallyRelevantTime) {
+        let t = new ClinicallyRelevantTime();
+        t.value = clinicallyRelevantTime;
+        this._tnmStage._clinicallyRelevantTime = t;
+    }
     
-    // _calculateStage() {
-    //     const t = this.t_Stage;
-    //     const n = this.n_Stage;
-    //     const m = this.m_Stage;
-    //     //console.log("calculateStage: " + t + " " + n + " " + m);
-    //     if (t.length === 0 || n.length === 0 || m.length === 0) {
-    //         this.stage = '';
-    //         return; // not complete value
-    //     }
-    //     this.stage = staging.breastCancerPrognosticStage(t, n, m);        
-    // }
+    _calculateStage() {
+        const t = this.t_Stage;
+        const n = this.n_Stage;
+        const m = this.m_Stage;
+        // console.log("calculateStage: " + t + " " + n + " " + m);
+        if (t.length === 0 || n.length === 0 || m.length === 0) {
+            this.stage = '';
+            return; // not complete value
+        }
+        this.stage = staging.breastCancerPrognosticStage(t, n, m);
+    }
 }
 
 export default FluxTNMStage;
