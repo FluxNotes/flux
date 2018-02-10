814624d87b89ff46a417fbac8641a5232a3c6a5c
Quinn <GQUINN@MITRE.ORG>
2018-02-10 11:32:28 -0500

    update to AppManager and FullApp and HardCodedPatientReadOnlyDataSource to support a 2nd demo patient and patient id specified in AppManager metadata

--- src/apps/AppManager.jsx
+++ src/apps/AppManager.jsx
@@ -90,14 +90,25 @@ export default class AppManager {
                 isExact: true,
                 dataSource: 'HardCodedReadOnlyDataSource',
                 shortcuts: []
-            },  {
+            },
+            {
+                path: '/myd18',
+                display: 'Flux Notes™',
+                app: FullApp,
+                isExact: true,
+                dataSource: 'HardCodedReadOnlyDataSource',
+                patientId: '788dcbc3-ed18-470c-89ef-35ff91854c7e',
+                shortcuts: []
+            },
+            {
                 path: '/p2',
                 display: 'Flux Notes™',
                 app: FullApp,
                 isExact: false,
                 dataSource: 'RestApiDataSource',
                 shortcuts: []
-            }, {
+            },
+            {
                 path: '/p3',
                 display: 'Flux Notes™',
                 app: FullApp,
