e5eda424a7c71ce62ed2fc006be4acdb8ba203f1
Quinn <GQUINN@MITRE.ORG>
2018-03-09 11:45:37 -0500

    partial switch to ResearchSubject

---
+++ src/model/research/FluxResearchSubject.js
@@ -0,0 +1,136 @@
+import ResearchSubject from '../shr/research/ResearchSubject';
+import Title from '../shr/core/Title';
+import Details from '../shr/core/Details';
+import Identifier from '../shr/core/Identifier';
+import Entry from '../shr/base/Entry';
+import EntryType from '../shr/base/EntryType';
+import EffectiveTimePeriod from '../shr/core/EffectiveTimePeriod';
+import TimePeriodStart from '../shr/core/TimePeriodStart';
+import TimePeriodEnd from '../shr/core/TimePeriodEnd';
+import Lang from 'lodash';
+
+class FluxResearchSubject {
+    constructor(json) {
+        this._researchSubject = ResearchSubject.fromJSON(json);
+        if (!this._researchSubject.entryInfo) {
+            let entry = new Entry();
+            entry.entryType = new EntryType();
+            entry.entryType.uri = 'http://standardhealthrecord.org/spec/shr/research/ResearchSubject';
+            this._researchSubject.entryInfo = entry;
+        }
+    }
+
+    get entryInfo() {
+        return this._researchSubject.entryInfo;
+    }
+
+    /**
+     *  Getter for title
+     *  This will return the displayText value from the Title object
+     */
+    get title() {
+        if (this._researchSubject.study.title) {
+            return this._researchSubject.study.title.value;
+        } else {
+            return "";
+        }
+    }
+
+    /**
+     *  Setter for title
+     *  The setter method is expecting a title sting
+     *  The method will create a Title object and set the value to the title string
+     */
+    set title(title) {
+        if (!this._researchSubject.study) {
+            this._researchSubject.study = new FluxStudy();
+        }
+        if (Lang.isNull(title)) {
+            this._researchSubject.study.title = null;
+            return;
+        }
+        let titleObj = new Title();
+        titleObj.value = title; 
+        this._researchSubject.study.title = titleObj;
+    }
+    /**
+     *  Getter for detail
+     *  This will return the displayText value from the Details object
+     */    
+    get details() {
+        if (this._researchSubject.details) {
+            return this._researchSubject.details.value;
+        } else {
+            return "";
+        }
+    }
+
+    /**
+     *  Setter for details
+     *  The setter method is expecting a details sting
+     *  The method will create a Details object and set the value to the detail string
+     */
+    set details(details) {
+        if (Lang.isNull(details)) return;
+        let detailsObj = new Details();
+        detailsObj.value = details; 
+        this._researchSubject.details = detailsObj;
+    }    
+
+    /**
+     *  Getter for identifier
+     *  This will return the displayText value from the Identifier object
+     */
+    get identifier() {
+        return this._researchSubject.identifier.value;
+    }
+
+    /**
+     *  Setter for identifier
+     *  The setter method is expecting a identifier sting
+     *  The method will create an Identifier object and set the value to the identifier string
+     */
+    set identifier(identifier) {
+        let i = new Identifier();
+        i.value = identifier;
+        this._researchSubject.identifier = i;
+    }
+
+    get enrollmentDate() {
+        if (!this._researchSubject.effectiveTimePeriod || !this._researchSubject.effectiveTimePeriod.timePeriodStart) return null;
+        return this._researchSubject.effectiveTimePeriod.timePeriodStart.value;
+    }
+  
+    set enrollmentDate(val) {
+        if (Lang.isNull(val) && this._researchSubject.effectiveTimePeriod) {
+            this._researchSubject.effectiveTimePeriod.timePeriodStart = null;
+            return;
+        }
+        if (!this._researchSubject.effectiveTimePeriod) {
+            this._researchSubject.effectiveTimePeriod = new EffectiveTimePeriod();
+        }
+        let timePeriodStart = new TimePeriodStart();
+        timePeriodStart.value = val;
+        this._researchSubject.effectiveTimePeriod.timePeriodStart = timePeriodStart;
+    }
+  
+    get endDate() {
+        if (!this._researchSubject.effectiveTimePeriod || !this._researchSubject.effectiveTimePeriod.timePeriodEnd) return null;
+        return this._researchSubject.effectiveTimePeriod.timePeriodEnd.value;
+    }
+  
+    set endDate(val) {
+        if (Lang.isNull(val) && this._researchSubject.effectiveTimePeriod) {
+            this._researchSubject.effectiveTimePeriod.timePeriodEnd = null;
+            return;
+        }
+        if (!this._researchSubject.effectiveTimePeriod) {
+            this._researchSubject.effectiveTimePeriod = new EffectiveTimePeriod();
+        }
+        let timePeriodEnd = new TimePeriodEnd();
+        timePeriodEnd.value = val;
+        this._researchSubject.effectiveTimePeriod.timePeriodEnd = timePeriodEnd;
+    }
+}
+
+export default FluxResearchSubject;
