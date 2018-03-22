7b8a170c9c5d25271599f9227ce646736c4f6114
Noranda Brown <nebrown@mitre.org>
2018-03-22 10:53:20 -0400

    Code cleanup

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,15 +4,3 @@ import { routerReducer } from 'react-router-redux';
 export default combineReducers({
   routing: routerReducer
 });
-
-
-// import { combineReducers } from 'redux';
-// // import { routerReducer } from 'react-router-redux';
-// import { ConnectedRouter, routerReducer, routerMiddleware, push } from 'react-router-redux';
-
-// const rootReducer = combineReducers({
-//   routing: routerReducer
-//   // additional reducers added here
-// });
-
-// export default rootReducer;
