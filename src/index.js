a5485d5f158b30715fd4015be56b0d72c6d7b7c4
Noranda Brown <nebrown@mitre.org>
2018-03-22 09:29:43 -0400

    Removed unused files

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
 // import 'es6-shim';
 // import 'babel-polyfill';
-// import injectTapEventPlugin from 'react-tap-event-plugin';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
 // import Root from './components/Root';
 // import configureStore from './store/configureStore';
@@ -14,6 +14,10 @@ import { ConnectedRouter } from 'react-router-redux';
 import store, { history } from './store/configureStore';
 import App from './containers/App';
 import './index.css';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
 const target = document.querySelector('#root');
 
@@ -34,9 +38,7 @@ render(
 
 // window.store = store;
 
-// // Needed for onTouchTap
-// // http://stackoverflow.com/a/34015469/988941
-// injectTapEventPlugin();
+
 
 // render(
 //   <Root store={store} />,
