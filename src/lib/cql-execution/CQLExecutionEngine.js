9f2f5cb57945ed290d17d58e3a7417f3627d5458
Dan Lee <daniellee@mitre.org>
2018-05-31 13:01:00 -0400

    Added logic to use CodeService and pass to CQL Execution Engine.

--- src/lib/cql-execution/CQLExecutionEngine.js
+++ src/lib/cql-execution/CQLExecutionEngine.js
@@ -1,28 +1,36 @@
 import cql from 'cql-execution';
 import cqlfhir from 'cql-exec-fhir';
-import exampleCql from './example/gender';
-import examplePatient1 from './example/exampleFHIRPatient1.json';
-import examplePatient2 from './example/exampleFHIRPatient2.json';
+import CodeService from './codeservice/CodeService';
+import valueset_db from './vsac_cache/valueset-db.json';
+import exampleCql from './example/cql/DiabeticFootExam';
+import examplePatient1 from './example/patients/exampleFHIRPatient1.json';
+import examplePatient2 from './example/patients/exampleFHIRPatient2.json';
+import examplePatient3 from './example/patients/No_Foot_Exam.json';
 
 /*
  *  returns CQL execution results when passed in a measure and set of patients
- *  cql is a javascript file compiled from coffeescript, which was converted from a CQL file defining logic
+ *  cqlLogic is a javascript file compiled from coffeescript, which was converted from a CQL file defining logic
  *  psource is an array of patients each represented as a FHIR bundle
  */ 
 exports.getCQLResults = (cqlLogic, psource) => {
-    // Executes measure against the patient source
+    // Set up library
     const lib = new cql.Library(cqlLogic);
-    const executor = new cql.Executor(lib);
 
-    // const patientSource = new cql.PatientSource(psource);
+    // Create Patient Source
     const patientSource = cqlfhir.PatientSource.FHIRv102();
+
+    // Load patientsource with patients
     patientSource.loadBundles(psource);
     
+    // Set up code service and load valuesets
+    const codeService = new CodeService(valueset_db);
+    
+    const executor = new cql.Executor(lib, codeService);
     return executor.exec(patientSource);
 }
 
 // returns CQL Results using example measure(age.cql -> age.coffee -> (age.js, age.js.map))
 // and example patient source
 exports.getExampleCQLResults = () => {
-    return exports.getCQLResults(exampleCql, [examplePatient1, examplePatient2]);
+    return exports.getCQLResults(exampleCql, [examplePatient1, examplePatient2, examplePatient3]);
 }
