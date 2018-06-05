cea95711cad490b4d39cddac9d584502f5e4dab3
Jessica <Jessica.O_Flanagan@tufts.edu>
2018-06-05 13:18:40 -0400

    Updating getItemListForClinicalTrialEligibility to always return a value

--- src/summary/SummaryMetadata.jsx
+++ src/summary/SummaryMetadata.jsx
@@ -846,13 +846,12 @@ export default class SummaryMetadata {
     }
 
     getItemListForClinicalTrialEligibility = () => {
-        const result = CQLExecutionEngine.getCQLResults(PALLAScql, [PALLAS_eligiblePatient, PALLAS_ineligiblePatient]);
-        var eligibility = "Not eligible";
-        if (result) {
+        const result = CQLExecutionEngine.getCQLResults(PALLAScql, [PALLAS_eligiblePatient]);
+        let eligibility = "Not eligible";
+        if (result.patientResults['93d8b432-f097-4c82-b111-8577e1d9d89f'].MeetsInclusionCriteria) {
             eligibility = "Potentially eligible";
         }
-        if (result.patientResults['93d8b432-f097-4c82-b111-8577e1d9d89f'].MeetsInclusionCriteria)
-        return [[{value: 'PALLAS'}, 'Palbociclib Collaborative Adjuvant Study', eligibility]];
+        return [[{ value: 'PALLAS' }, 'Palbociclib Collaborative Adjuvant Study', eligibility]];
     }
 
     getItemListForAllergies = (patient, currentConditionEntry) => {
