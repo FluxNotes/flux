083c1f8222c922ee68bfe03e5094f48cc5325b73
Lucy <lxu@mitre.org>
2018-06-14 15:49:54 -0400

    playing around with activecontexts

--- src/notes/EditorToolbarBreadcrumbs.jsx
+++ src/notes/EditorToolbarBreadcrumbs.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import 'font-awesome/css/font-awesome.min.css';
+import './EditorToolbar.css';
 import './EditorToolbarBreadcrumbs.css';
+import ShortcutViewModeContent from '../context/ShortcutViewModeContent';
+// import Context from '../context/Context';
 
 class EditorToolbarBreadcrumbs extends React.Component {
     render = () => {
+        let activeContexts = ShortcutViewModeContent.getActiveContexts();
         return (
-            <div> Testing Breadcrumb component </div>
+            <div className="menu toolbar-breadcrumbs"> {activeContexts}} </div>
         )
     }
 
