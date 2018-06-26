fe9cba04366a75cc1ae6491c880c80048cb00964
Dylan Phelan <dtphelan1@gmail.com>
2018-06-26 10:54:15 -0400

    left some <<< in a rebase

--- src/summary/SummaryMetadata.jsx
+++ src/summary/SummaryMetadata.jsx
@@ -605,7 +605,6 @@ export default class SummaryMetadata {
                             {
                                 name: "Potential to enroll",
                                 headings: ["Name", "Criteria Fit", "Opened", "Description"],
-<<<<<<< HEAD
                                 itemsFunction: this.getItemListForClinicalTrialEligibility,
                                 actions: [
                                     {
@@ -625,12 +624,6 @@ export default class SummaryMetadata {
                                 itemsFunction: this.getItemListToDisplayMissingCriteria
                             }
                         ]   
-=======
-                                itemsFunction: this.getItemListForClinicalTrialEligibility
-
-                            }
-                        ]
->>>>>>> 7a11c87d... Added clinical trial code back
                     },
                     {
                         name: "Allergies",
