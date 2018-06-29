1be7a070b7384b6f0a81d592ced821231bf7eddd
Dan Lee <daniellee@mitre.org>
2018-06-29 09:30:44 -0400

    Initial additon of PLACEHOLDERS view in ContextTray.

--- src/context/ContextTray.jsx
+++ src/context/ContextTray.jsx
@@ -1,14 +1,16 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-import TemplateViewModeContent from './TemplateViewModeContent'
-import ShortcutViewModeContent from './ShortcutViewModeContent'
-import './ContextTray.css';
+import TemplateViewModeContent from "./TemplateViewModeContent";
+import ShortcutViewModeContent from "./ShortcutViewModeContent";
+import "./ContextTray.css";
 
 export default class ContextTray extends Component {
     //these are the possible view mode constants
     TEMPLATE_VIEW = 0;
     SHORTCUT_VIEW = 1;
+    PLACEHOLDER_VIEW = 2;
+    
     constructor(props) {
         super(props);
 
@@ -16,17 +18,27 @@ export default class ContextTray extends Component {
             // viewMode keeps track of which context is active
             // 0 when Templates are selected. 1 when Patient is selected
             // In editor, viewMode is incremented by 1 for each context added (i.e @condition, #disease status)
-            viewMode: this.SHORTCUT_VIEW,
+            viewMode: this.SHORTCUT_VIEW
         };
     }
 
-    handleTemplateSectionClick = () => this.setState({ 
-        viewMode: this.TEMPLATE_VIEW 
-    })
-    
-    handlePatientSectionClick = () => this.setState({ 
-        viewMode: this.SHORTCUT_VIEW 
-    })
+    handleTemplateSectionClick = () => {
+        this.setState({
+            viewMode: this.TEMPLATE_VIEW
+        });
+    };
+
+    handleShortcutSectionClick = () => {
+        this.setState({
+            viewMode: this.SHORTCUT_VIEW
+        });
+    };
+
+    handlePlaceholderSectionClick = () => {
+        this.setState({
+            viewMode: this.PLACEHOLDER_VIEW
+        });
+    };
 
     render() {
         const { viewMode } = this.state;
@@ -35,38 +47,57 @@ export default class ContextTray extends Component {
             <div className="context-tray">
                 <section>
                     <div className="view-mode-section-menu">
-                    <div
-                        className={`view-mode-section-item${viewMode === this.TEMPLATE_VIEW ? ' selected' : ''}`}
-                        onClick={this.handleTemplateSectionClick}
-                    >
-                        TEMPLATES
-                    </div>                        
+                        <div
+                            className={`view-mode-section-item${
+                                viewMode === this.TEMPLATE_VIEW
+                                    ? " selected"
+                                    : ""
+                                }`}
+                            onClick={this.handleTemplateSectionClick}
+                        >
+                            TEMPLATES
+                        </div>
 
-                    <div
-                        className={`view-mode-section-item${viewMode === this.SHORTCUT_VIEW ? ' selected' : ''}`}
-                        onClick={this.handlePatientSectionClick}
-                    >
-                        SHORTCUTS
-                    </div>
+                        <div
+                            className={`view-mode-section-item${
+                                viewMode === this.SHORTCUT_VIEW
+                                    ? " selected"
+                                    : ""
+                                }`}
+                            onClick={this.handleShortcutSectionClick}
+                        >
+                            SHORTCUTS
+                        </div>
+
+                        <div
+                            className={`view-mode-section-item${
+                                viewMode === this.PLACEHOLDER_VIEW
+                                    ? " selected"
+                                    : ""
+                                }`}
+                            onClick={this.handlePlaceholderSectionClick}
+                        >
+                            PLACEHOLDERS
+                        </div>
                     </div>
                 </section>
 
-                {viewMode === this.TEMPLATE_VIEW &&
+                {viewMode === this.TEMPLATE_VIEW && (
                     <TemplateViewModeContent
                         onShortcutClicked={this.props.onShortcutClicked}
                         patient={this.props.patient}
                         setInsertingTemplate={this.props.setInsertingTemplate}
                     />
-                }
+                )}
 
-                {viewMode === this.SHORTCUT_VIEW &&
+                {viewMode === this.SHORTCUT_VIEW && (
                     <ShortcutViewModeContent
                         contextManager={this.props.contextManager}
                         handleClick={this.handleShortcutClick}
                         onShortcutClicked={this.props.onShortcutClicked}
                         shortcutManager={this.props.shortcutManager}
                     />
-                }
+                )}
             </div>
         );
     }
@@ -77,5 +108,5 @@ ContextTray.proptypes = {
     onShortcutClicked: PropTypes.func.isRequired,
     patient: PropTypes.object.isRequired,
     setInsertingTemplate: PropTypes.func.isRequired,
-    shortcutManager: PropTypes.object.isRequired,
-}
+    shortcutManager: PropTypes.object.isRequired
+};
