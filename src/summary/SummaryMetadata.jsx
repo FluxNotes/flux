e10f075d9040b092f55883b5ff8a8a44e66cba2a
Quinn <GQUINN@MITRE.ORG>
2018-08-10 16:35:20 -0400

    fixed capitalization on source information modal

--- src/summary/SummaryMetadata.jsx
+++ src/summary/SummaryMetadata.jsx
@@ -1352,15 +1352,15 @@ export default class SummaryMetadata {
     determineSource = (patient, entry) => {
         if (entry.sourceClinicalNoteReference) return entry.sourceClinicalNoteReference;
         let result = "";
-        if (entry.author) result += "recorded by " + entry.author;
-        if (entry.informant) result += (result.length > 0 ? " " : "") + "based on information from " + entry.informant;
+        if (entry.author) result += "Recorded by " + entry.author;
+        if (entry.informant) result += (result.length > 0 ? " b" : "B") + "ased on information from " + entry.informant;
         if (entry.relatedEncounterReference) {
             const relatedEncounter = patient.getEntryFromReference(entry.relatedEncounterReference);
-            result += (result.length > 0 ? " " : "") + "from encounter on " + new moment(relatedEncounter.actionContext.occurrenceTimeOrPeriod.timePeriod.timePeriodStart.value, 'D MMM YYY HH:mm Z').format('D MMM YYY hh:mm a');
+            result += (result.length > 0 ? " f" : "F") + "rom encounter on " + new moment(relatedEncounter.actionContext.occurrenceTimeOrPeriod.timePeriod.timePeriodStart.value, 'D MMM YYY HH:mm Z').format('D MMM YYY hh:mm a');
         } else if (entry.creationTime) {
-            result += (result.length > 0 ? " " : "") + "on " + entry.creationTime.format('D MMM YYY hh:mm a');
+            result += (result.length > 0 ? " o" : "O") + "n " + entry.creationTime.format('D MMM YYY hh:mm a');
         } else if (entry.diagnosisDate) {
-            result += (result.length > 0 ? " " : "") + "clinically recognized on " + new moment(entry.diagnosisDate, 'D MMM YYYY').format('D MMM YYYY');
+            result += (result.length > 0 ? " c" : "C") + "linically recognized on " + new moment(entry.diagnosisDate, 'D MMM YYYY').format('D MMM YYYY');
         }
         return result;
     }
