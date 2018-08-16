7bfff7bcf52956d0bddcce5d1cc26536079258ae
Jessica <Jessica.O_Flanagan@tufts.edu>
2018-08-16 13:04:09 -0400

    Adding new svg for POC icon

--- src/notes/NoteAssistant.jsx
+++ src/notes/NoteAssistant.jsx
@@ -514,7 +514,11 @@ export default class NoteAssistant extends Component {
                     onClick={() => {
                         this.toggleView("poc")
                     }}>
+<<<<<<< HEAD
                     <svg viewBox="0 -2 19 17">
+=======
+                    <svg viewBox="0 0 19 17">
+>>>>>>> Adding new svg for POC icon
                         <g id="icon-export" stroke={this.poc_stroke} stroke-width="1" fill="none" fill-rule="evenodd">
                             <g id="Artboard" transform="translate(-151.000000, -186.000000)" stroke={this.poc_stroke}>
                                 <g id="tray---POC-selected" transform="translate(151.000000, 186.000000)">
