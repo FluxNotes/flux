6e193cd813416477115c55866ab2e8a7acc99b5d
nng <nng@mitre.org>
2018-08-17 15:47:36 -0400

    Added left margin to note name so pencil icon doesn't overlap with left divider and scroll bar. Added space after "Signed Date: "

--- src/notes/FluxNotesEditor.jsx
+++ src/notes/FluxNotesEditor.jsx
@@ -1303,7 +1303,7 @@ class FluxNotesEditor extends React.Component {
                                     <p className="note-description-detail"><span className="note-description-detail-name">Source: </span><span className="note-description-detail-value">{source}</span></p>
                                 </Col>
                                 <Col xs={5}>
-                                    <p className="note-description-detail"><span className="note-description-detail-name">Signed Date:</span><span className="note-description-detail-value">{date}</span></p>
+                                    <p className="note-description-detail"><span className="note-description-detail-name">Signed Date: </span><span className="note-description-detail-value">{date}</span></p>
                                 </Col>
                             </Row>
                         </Col>
