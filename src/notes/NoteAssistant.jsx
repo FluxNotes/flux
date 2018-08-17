9568e4d03a9be3c9cf9982b0c8c0aaff033fc4d1
Dan Lee <daniellee@mitre.org>
2018-08-17 15:07:11 -0400

    Fixing typos with stroke-width and fill-rule

--- src/notes/NoteAssistant.jsx
+++ src/notes/NoteAssistant.jsx
@@ -515,7 +515,7 @@ export default class NoteAssistant extends Component {
                         this.toggleView("poc")
                     }}>
                     <svg viewBox="0 -2 19 17">
-                        <g id="icon-export" stroke={this.poc_stroke} stroke-width="1" fill="none" fill-rule="evenodd">
+                        <g id="icon-export" stroke={this.poc_stroke} strokeWidth="1" fill="none" fillRule="evenodd">
                             <g id="Artboard" transform="translate(-151.000000, -186.000000)" stroke={this.poc_stroke}>
                                 <g id="tray---POC-selected" transform="translate(151.000000, 186.000000)">
                                     <g id="Group">
