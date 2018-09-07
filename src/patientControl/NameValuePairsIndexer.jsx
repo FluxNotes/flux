0ebab59f6670099c4f177e6a543b4759b5f11a28
Matt Gramigna <mgramigna@mitre.org>
2018-09-07 10:16:31 -0400

    Implemented NameValuePairs indexer

--- src/patientControl/NameValuePairsIndexer.jsx
+++ src/patientControl/NameValuePairsIndexer.jsx
@@ -2,7 +2,14 @@ import BaseIndexer from './BaseIndexer';
 
 class NameValuePairsIndexer extends BaseIndexer {
     indexData(section, subsection, data, searchIndex) {
-
+        data.forEach(obj => {
+            searchIndex.addSearchableData({
+                section,
+                subsection: "",
+                valueTitle: obj.name,
+                value: obj.value
+            });
+        })
     }
 }
 
