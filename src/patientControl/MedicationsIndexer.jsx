39eccbecb14804169873c0c23a2c2453ee530b5d
Matt Gramigna <mgramigna@mitre.org>
2018-09-07 12:53:45 -0400

    Added Medications type indexer

---
+++ src/patientControl/MedicationsIndexer.jsx
@@ -0,0 +1,51 @@
+import BaseIndexer from './BaseIndexer';
+
+class MedicationsIndexer extends BaseIndexer {
+    indexData(section, subsection, data, searchIndex) {
+        data.forEach(item => {
+            searchIndex.addSearchableData({
+                section,
+                subsection: "",
+                valueTitle: "Dosage",
+                value: `${item.medication.amountPerDose.value} ${item.medication.amountPerDose.units}`
+            });
+
+            searchIndex.addSearchableData({
+                section,
+                subsection: "",
+                valueTitle: "Medication",
+                value: item.medication.medication
+            });
+
+            searchIndex.addSearchableData({
+                section,
+                subsection: "",
+                valueTitle: "Timing",
+                value: item.medication.timingOfDoses.value
+            });
+
+            searchIndex.addSearchableData({
+                section,
+                subsection: "",
+                valueTitle: "Prescribed",
+                value: item.medication.whenPrescribed
+            });
+
+            searchIndex.addSearchableData({
+                section,
+                subsection: "",
+                valueTitle: "Route",
+                value: item.medication.routeIntoBody
+            });
+
+            searchIndex.addSearchableData({
+                section,
+                subsection: "",
+                valueTitle: "Prescribed By",
+                value: item.medication.prescribedBy
+            });
+        });
+    }
+}
+
+export default MedicationsIndexer;
