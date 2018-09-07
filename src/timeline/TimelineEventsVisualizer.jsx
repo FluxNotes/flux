040b31032cd309ee9e68eaf4260244f7103d2f9a
Matt Gramigna <mgramigna@mitre.org>
2018-09-07 09:03:50 -0400

    Updated SummaryMetadata to use itemsFunction across all types and use unnamed subsections when there are none

--- src/timeline/TimelineEventsVisualizer.jsx
+++ src/timeline/TimelineEventsVisualizer.jsx
@@ -70,7 +70,7 @@ class TimelineEventsVisualizer extends Component {
         // Create groups and items to display on the timeline
         let items = [];
         section.data.forEach((item, i) => {
-            items = items.concat(item.eventsFunction(patient, condition, this.getMaxGroup(items) + 1));
+            items = items.concat(item.itemsFunction(patient, condition, this.getMaxGroup(items) + 1));
         });
 
         // Assign every item an ID and onClick handler
