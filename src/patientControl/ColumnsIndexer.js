9cedbe02f53340b10afcd507239b7e9206d7332e
Quinn <GQUINN@MITRE.ORG>
2018-09-08 22:48:59 -0400

    got columns indexer working with true columns types. had to send in headings to use for titles but result looks good! Basically, the Columns Indexer supports a Columns case where there are 2 columns with no headings so they are label and value or a true table of data where the column headings are titles and each cell is a value

--- src/patientControl/ColumnsIndexer.js
+++ src/patientControl/ColumnsIndexer.js
@@ -1,19 +1,32 @@
 import BaseIndexer from './BaseIndexer';
-import Lang from 'lodash';
 
 class ColumnIndexer extends BaseIndexer {
-    indexData(section, subsection, data, searchIndex) {
-        let list = data.items || [];
-        list.forEach(([title, valueObject]) => {
-            if(Lang.isObject(valueObject)) {
+    indexData(section, subsection, data, searchIndex, subsectionDescriptor) {
+        if (subsectionDescriptor.headings) {
+            // true tabular where each item is a column of data
+            // add each column value using title of the column heading
+            data.forEach((row) => {
+                row.forEach((col, columnNumber) => {
+                    searchIndex.addSearchableData({
+                        section,
+                        subsection,
+                        valueTitle: subsectionDescriptor.headings[columnNumber],
+                        value: col.value || "Missing Data"
+                    });    
+                });
+            });
+        } else {
+            // ones that are really just a name and value so more of a layout than columns/tabular
+            // add each row with 1st column as title and 2nd column as value
+            data.forEach(([title, valueObject]) => {
                 searchIndex.addSearchableData({
                     section,
                     subsection,
                     valueTitle: title.value,
                     value: valueObject.value || "Missing Data"
                 });
-            }
-        });
+            });
+        }
     }
 }
 
