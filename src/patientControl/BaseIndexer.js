4e90b7ad06c260489da6c9cf1316fda9c24fdfb8
Quinn <GQUINN@MITRE.ORG>
2018-09-10 17:47:09 -0400

    don't index subsections with blank name

--- src/patientControl/BaseIndexer.js
+++ src/patientControl/BaseIndexer.js
@@ -2,12 +2,14 @@ import { Component } from 'react';
 
 class BaseIndexer extends Component {
     indexData(section, subsection, data, searchIndex) {
-        searchIndex.addSearchableData({
-            section,
-            subsection,
-            valueTitle: "subsection",
-            value: subsection
-        });
+        if (subsection) {
+            searchIndex.addSearchableData({
+                section,
+                subsection,
+                valueTitle: "subsection",
+                value: subsection
+            });
+        }
     }
 }
 
