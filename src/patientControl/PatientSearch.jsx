56cb846b44abe4c4bc62a6eff59b938baf5089d9
Laclark87 <LACLARK@MITRE.ORG>
2018-09-10 11:28:28 -0400

    deleted old code

--- src/patientControl/PatientSearch.jsx
+++ src/patientControl/PatientSearch.jsx
@@ -190,7 +190,6 @@ class PatientSearch extends React.Component {
 
     // Teach Autosuggest how to calculate the input value for every given suggestion.
     getSuggestionValue = suggestion => { 
-        //return suggestion.contentSnapshot;
         return this.state.value;
     }
 
