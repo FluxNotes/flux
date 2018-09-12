7f867dddecacee1436105ab6176c6e118c62b530
Quinn <GQUINN@MITRE.ORG>
2018-09-12 18:01:48 -0400

    slipping @vitals into templates for pilot

--- src/context/TemplateViewModeContent.jsx
+++ src/context/TemplateViewModeContent.jsx
@@ -7,8 +7,8 @@ export default class TemplateViewModeContent extends Component {
     // These don't need to be stored as state since they're not being updated
     templates = [
         { name: 'progress note', content: 'REASON FOR VISIT:\n@reason for next visit @condition\n\nONCOLOGY HISTORY:\n@ONCOHIST\n\nREVIEW OF SYSTEMS:\n\nALLERGIES:\n@ALLERGIES\n\nMEDICATIONS:\n@active medications\n\nPHYSICAL EXAM:\n\nASSESSMENT:\n\nPLAN:\n\n' },
-        { name: 'follow-up', content: 'REASON FOR VISIT:\n@patient following up for her @condition.\n\nONCOLOGY HISTORY:\n@ONCOHIST\n\nHISTORY OF PRESENT ILLNESS:\n\n\nREVIEW OF SYSTEMS:\n@ROS\n\nALLERGIES:\n@ALLERGIES\n\nMEDICATIONS:\n@active medications\n\nPHYSICAL EXAM:\n\n\nASSESSMENT:\n\n\nPLAN:\n\n' },
-        { name: 'initial consult', content: 'REASON FOR VISIT:\n@patient presenting with @condition for initial consult.\n\nHISTORY OF PRESENT ILLNESS:\n\n\nREVIEW OF SYSTEMS:\n@ROS\n\nALLERGIES:\n@ALLERGIES\n\nMEDICATIONS:\n@active medications\n\nPHYSICAL EXAM:\n\n\nASSESSMENT:\n\n\nPLAN:\n\n' },
+        { name: 'follow-up', content: 'REASON FOR VISIT:\n@patient following up for her @condition.\n\nONCOLOGY HISTORY:\n@ONCOHIST\n\nHISTORY OF PRESENT ILLNESS:\n\n\nREVIEW OF SYSTEMS:\n@ROS\n\nALLERGIES:\n@ALLERGIES\n\nMEDICATIONS:\n@active medications\n\nPHYSICAL EXAM:\n@Vitals\n\nASSESSMENT:\n\n\nPLAN:\n\n' },
+        { name: 'initial consult', content: 'REASON FOR VISIT:\n@patient presenting with @condition for initial consult.\n\nHISTORY OF PRESENT ILLNESS:\n\n\nREVIEW OF SYSTEMS:\n@ROS\n\nALLERGIES:\n@ALLERGIES\n\nMEDICATIONS:\n@active medications\n\nPHYSICAL EXAM:\n@Vitals\n\nASSESSMENT:\n\n\nPLAN:\n\n' },
         { name: 'op note', content: 'op note' },
         { name: 'test', content: 'FOLLOW UP:\nPatient is showing signs of @condition @ONCOHIST @condition @ONCOHIST\n\nMEDICATIONS:\n@medication\n\nProcedures:\n@procedure' },
         { name: 'consult note', content: '@patient presenting with ' }
