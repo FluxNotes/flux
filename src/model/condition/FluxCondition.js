a8d8a837f58b2f1eadb819f58ce03664c5db1048
Dan Lee <daniellee@mitre.org>
2018-09-13 12:39:11 -0400

    Changed content of label for diagnosis date to include actual date.

--- src/model/condition/FluxCondition.js
+++ src/model/condition/FluxCondition.js
@@ -28,8 +28,10 @@ class FluxCondition {
     }
 
     getPotentialDiagnosisDates() {
-        return [ {  date: this.diagnosisDate,
-                    label: 'diagnosis date'
+        const date = this.diagnosisDate;
+
+        return [ {  date,
+                    label: `diagnosis ${date}`,
         }];
     }
 
