ecdc5eba6836df2a1019dc3ae5c0da3107073fa1
nng <nng@mitre.org>
2018-10-05 16:42:55 -0400

    Removed code to manually resize chart since this is handled using the responsive container. This also fixes the bug where the dots on the data points don't show up on intial render of the charts

--- src/summary/BandedLineChartVisualizer.jsx
+++ src/summary/BandedLineChartVisualizer.jsx
@@ -15,27 +15,12 @@ class BandedLineChartVisualizer extends Component {
     constructor(props) {
         super(props);
 
-        this.updateState = true;
+        // this.updateState = true;
         // This var will be used 
         this.state = {
             chartWidth: 600,
             chartHeight: 400,
         }
-    }
-
-    // Make sure to update data and resize the component when its contents update.
-    componentDidUpdate = () => {
-        if (this.updateState) {
-            this.updateState = false;
-        } else {
-            this.updateState = true;
-            setTimeout(this.resize, 450);
-        }
-    }
-
-    // Adds appropriate event listeners for tracking resizing
-    componentDidMount = () => {
-        setTimeout(this.resize, 450);
     }
 
     // Turns dates into numeric representations for graphing
@@ -67,7 +52,6 @@ class BandedLineChartVisualizer extends Component {
             }
             return rangeValues;
         }, [processedData[0][xVarNumber], processedData[0][xVarNumber]]);
-
     }
 
     // Use min/max info to build ticks for the 
@@ -94,17 +78,7 @@ class BandedLineChartVisualizer extends Component {
         return (value) => {
             return `${value} ${unit}`;
         }
-    }
-
-    // Updates the dimensions of the chart
-    resize = () => {
-        if (!this.chartParentDiv) return;
-        const chartParentDivWidth = this.chartParentDiv.offsetWidth;
-
-        this.setState({
-            chartWidth: chartParentDivWidth,
-        })
-    }
+    }  
 
     renderSubsectionChart = (subsection, patient, condition) => {
         // FIXME: Should start_time be a magic string?
@@ -171,10 +145,7 @@ class BandedLineChartVisualizer extends Component {
         }
 
         return (
-            <div
-                ref={(chartParentDiv) => {
-                    this.chartParentDiv = chartParentDiv;
-                }}
+            <div          
                 key={yVar}
             >
                 <div className="sub-section-heading">
@@ -183,7 +154,7 @@ class BandedLineChartVisualizer extends Component {
                     </h2>
                 </div>
                 <ResponsiveContainer
-                  height={this.state.chartHeight}
+                    height={this.state.chartHeight}
                 >                 
                     <LineChart
                         data={processedData}
