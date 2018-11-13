0bbd5b68f5838c4332e2624f6055082a18af129c
Dylan Phelan <dtphelan1@gmail.com>
2018-11-13 12:39:18 -0500

    Update TemplateViewModeContent.jsx

--- src/context/TemplateViewModeContent.jsx
+++ src/context/TemplateViewModeContent.jsx
@@ -9,8 +9,7 @@ export default class TemplateViewModeContent extends Component {
         { name: 'physical exam', content: 'Vitals: @vitals\nSkin:\nLymph:' },
         { name: 'follow-up', content: 'REASON FOR VISIT:\n@patient following up on @condition on @date of visit. @reason for next visit.\n\nONCOLOGY HISTORY:\n@ONCOHIST\n\nHISTORY OF PRESENT ILLNESS:\n\n\nREVIEW OF SYSTEMS:\n@ROS\n\nALLERGIES:\n@ALLERGIES\n\nMEDICATIONS:\n@active medications\n\nPHYSICAL EXAM:\n\n\nASSESSMENT:\n\n\nPLAN:\n\n' },
         { name: 'initial consult', content: 'REASON FOR VISIT:\n@patient presenting with @condition for initial consult on @date of visit. @reason for next visit. @referred by.\n\nHISTORY OF PRESENT ILLNESS:\n\n\nREVIEW OF SYSTEMS:\n@ROS\n\nALLERGIES:\n@ALLERGIES\n\nMEDICATIONS:\n@active medications\n\nPHYSICAL EXAM:\n\n\nASSESSMENT:\n\n\nPLAN:\n\n' },
-        { name: 'test', content: 'FOLLOW UP:\nPatient is showing signs of @condition @ONCOHIST @condition @ONCOHIST\n\nMEDICATIONS:\n@medication\n\nProcedures:\n@procedure' },
-        { name: 'consult note', content: '@patient presenting with ' }
+        { name: 'test', content: 'FOLLOW UP:\nPatient is showing signs of @condition @ONCOHIST @condition @ONCOHIST\n\nMEDICATIONS:\n@medication\n\nProcedures:\n@procedure' }
     ];
 
     // Insert the content of the template as you would a shortcut
