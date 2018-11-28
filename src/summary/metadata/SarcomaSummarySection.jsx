52bba92be7b1a59381567b1e386b564f4a59b293
Dan Lee <daniellee@mitre.org>
2018-11-28 12:38:05 -0500

    Commented out mitosis and tumor size from narrative section in sarcoma summary.

--- src/summary/metadata/SarcomaSummarySection.jsx
+++ src/summary/metadata/SarcomaSummarySection.jsx
@@ -24,20 +24,20 @@ export default class SarcomaSummarySection extends MetadataSection {
                         ".Status"
                     ]
                 },
-                {
-                    defaultTemplate: "Mitosis is ${.Mitosis}.",
-                    dataMissingTemplate: "Mitosis is ${.Mitosis}.",
-                    useDataMissingTemplateCriteria: [
-                        ".Mitosis"
-                    ]
-                },
-                {
-                    defaultTemplate: "Tumor is ${.Tumor Size}.",
-                    dataMissingTemplate: "Tumor is ${.Tumor Size}.",
-                    useDataMissingTemplateCriteria: [
-                        ".Tumor Size"
-                    ]
-                },
+                // {
+                //     defaultTemplate: "Mitosis is ${.Mitosis}.",
+                //     dataMissingTemplate: "Mitosis is ${.Mitosis}.",
+                //     useDataMissingTemplateCriteria: [
+                //         ".Mitosis"
+                //     ]
+                // },
+                // {
+                //     defaultTemplate: "Tumor is ${.Tumor Size}.",
+                //     dataMissingTemplate: "Tumor is ${.Tumor Size}.",
+                //     useDataMissingTemplateCriteria: [
+                //         ".Tumor Size"
+                //     ]
+                // },
                 {
                     defaultTemplate: "Genetics are ${.Genetics}.",
                     dataMissingTemplate: "Genetics are ${.Genetics}.",
