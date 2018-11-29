ad1802f2ed161a9b2b75f6f64ef54f8b80a40b15
Dan Lee <daniellee@mitre.org>
2018-11-29 13:32:44 -0500

    #toxicity working...

--- src/model/adverse/FluxToxicReaction.js
+++ src/model/adverse/FluxToxicReaction.js
@@ -2,6 +2,8 @@ import FluxAdverseEvent from './FluxAdverseEvent';
 import ToxicReaction from '../shr/adverse/ToxicReaction';
 import Entry from '../shr/base/Entry';
 import EntryType from '../shr/base/EntryType';
+import Reference from '../Reference';
+import SpecificFocusOfFinding from '../shr/base/SpecificFocusOfFinding.js';
 
 // FluxToxicReaction class to hide codeableconcepts
 class FluxToxicReaction extends FluxAdverseEvent {
@@ -14,6 +16,28 @@ class FluxToxicReaction extends FluxAdverseEvent {
             entry.entryType = new EntryType();
             entry.entryType.uri = 'http://standardhealthrecord.org/spec/shr/adverse/ToxicReaction';
             this._adverseEvent.entryInfo = entry;
+        }
+    }
+
+    get specificFocusOfFinding() {
+        if (this._adverseEvent.specificFocusOfFinding) {
+            return this._adverseEvent.specificFocusOfFinding;
+        }
+        return null;
+    }
+
+    set specificFocusOfFinding(val) {
+        this._adverseEvent.specificFocusOfFinding = val;
+    }
+
+    setSpecificFocusOfFinding(obj) {
+        if (!obj) {
+            this.specificFocusOfFinding = null;
+        } else {
+            let ref = new Reference(obj.entryInfo.shrId, obj.entryInfo.entryId, obj.entryInfo.entryType);
+            let sff = new SpecificFocusOfFinding();
+            sff.value = ref;
+            this.specificFocusOfFinding = sff;
         }
     }
 
