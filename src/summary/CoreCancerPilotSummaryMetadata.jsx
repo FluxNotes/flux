b6388b181151f2bca2f957acaf18b67cdc64903b
Quinn <GQUINN@MITRE.ORG>
2019-01-02 12:26:50 -0500

    remove commented out code

--- src/summary/CoreCancerPilotSummaryMetadata.jsx
+++ src/summary/CoreCancerPilotSummaryMetadata.jsx
@@ -1,10 +1,8 @@
 import SummaryMetadata from './SummaryMetadata';
 import DefaultCoreCancerPilotMetadata from './metadata/DefaultCoreCancerPilotMetadata';
-//import SarcomaCoreCancerPilotMetadata from './metadata/SarcomaCoreCancerPilotMetadata';
 import McodeMetadata from './metadata/McodeMetadata';
 import FluxCancerDisorder from '../model/mcode/FluxCancerDisorder';
 import FunctionMatcher from './matchers/FunctionMatcher';
-//import StringMatcher from './matchers/StringMatcher';
 import AlwaysMatcher from './matchers/AlwaysMatcher';
 
 export default class CoreCancerPilotSummaryMetadata extends SummaryMetadata {
@@ -13,7 +11,6 @@ export default class CoreCancerPilotSummaryMetadata extends SummaryMetadata {
         this.setForceRefresh = setForceRefresh;
 
         this.hardCodedMetadata = [
-//            { "enabled": true, "type": StringMatcher, "matchString": "http://snomed.info/sct/420120006", "metadata": SarcomaCoreCancerPilotMetadata },
             { "enabled": true, "type": FunctionMatcher, "matchFunction": (condition) => condition instanceof FluxCancerDisorder, "metadata": McodeMetadata },
             { "enabled": true, "type": AlwaysMatcher, "metadata": DefaultCoreCancerPilotMetadata },
         ];
