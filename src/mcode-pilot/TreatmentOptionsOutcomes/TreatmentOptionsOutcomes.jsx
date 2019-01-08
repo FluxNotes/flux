797614af6bf645da73710d83aa6b9ff332a8217a
kghoreshi <kghoreshi@mitre.org>
2019-01-08 10:08:37 -0500

    added bar chart components to table

--- src/mcode-pilot/TreatmentOptionsOutcomes/TreatmentOptionsOutcomes.jsx
+++ src/mcode-pilot/TreatmentOptionsOutcomes/TreatmentOptionsOutcomes.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Table, { TableCell, TableHead, TableBody, TableRow } from 'material-ui/Table';
+import BarChart from '../Visualizations/BarChart';
 import './TreatmentOptionsOutcomes.css';
 
 let id = 0;
@@ -16,13 +17,85 @@ const rows = [
     createData('none (actively monitoring)'),
 ];
 
+const outcome_survival = {
+    'none (actively monitoring)':{
+        'yr1':{
+            'survived':67,
+            'cancerDeath': 37,
+            'otherDeath': 5
+        },
+        'yr2':{
+            'survived': 54,
+            'cancerDeath': 50,
+            'otherDeath':10
+        },
+        'yr3':{
+            'survived':43,
+            'cancerDeath':61,
+            'otherDeath':4
+        }
+    },
+    'chemotherapy':{
+        'yr1':{
+            'survived':14,
+            'cancerDeath':3,
+            'otherDeath':1
+        },
+        'yr2':{
+            'survived':12,
+            'cancerDeath':5,
+            'otherDeath':1
+        },
+        'yr3':{
+            'survived':11,
+            'cancerDeath':6,
+            'otherDeath':1
+        }
+
+    },
+    'hormonal therapy':{
+        'yr1':{
+            'survived':43,
+            'cancerDeath':23,
+            'otherDeath':4
+        },
+        'yr2':{
+            'survived':39,
+            'cancerDeath':27,
+            'otherDeath':4
+        },
+        'yr3':{
+            'survived':32,
+            'cancerDeath':34,
+            'otherDeath':4
+        }
+    },
+    'surgery & radiation':{
+        'yr1':{
+            'survived':23,
+            'cancerDeath':32,
+            'otherDeath':5
+        },
+        'yr2':{
+            'survived':19,
+            'cancerDeath':35,
+            'otherDeath':4
+        },
+        'yr3':{
+            'survived':15,
+            'cancerDeath':37,
+            'otherDeath':4
+        }
+    }
+}
 
 export default class TreatmentOptionsOutcomes extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            placeholder: {}
+            placeholder: {},
+            activeRow:'none (actively monitoring)'
         };
     }
 
@@ -81,9 +154,13 @@ export default class TreatmentOptionsOutcomes extends Component {
                                         {row.name}
                                     </TableCell>
                                     <TableCell >({Math.round(Math.random() * 100)})</TableCell>
-                                    <TableCell ><div className="prog-fill-top"><div className="prog-fill" style={{ "width": Math.random() * 100 }}></div></div></TableCell>
-                                    <TableCell ><div className="prog-fill-top"><div className="prog-fill" style={{ "width": Math.random() * 100 }}></div></div></TableCell>
-                                    <TableCell ><div className="prog-fill-top"><div className="prog-fill" style={{ "width": Math.random() * 100 }}></div></div></TableCell>
+                                    {Object.keys(outcome_survival[row.name]).map(year=>{
+                                        return(<TableCell >
+                                            <BarChart 
+                                                values={outcome_survival[row.name][year]}
+                                                compareTo={outcome_survival[this.state.activeRow][year]}/>
+                                        </TableCell>)
+                                    })}
                                     <TableCell > 0.5 .4</TableCell>
                                     <TableCell > 15% ^ 4</TableCell>
                                     <TableCell > neuropathy (4%) <br /> other thing (19%)</TableCell>
