e3e5c25602d8bdd35ea1b9b5d7f87108a4bd4bad
Noranda Brown <nebrown@mitre.org>
2019-02-06 22:49:53 -0500

    Fixed styling after merge

--- src/mcode-pilot/utils/filterTreatmentData.js
+++ src/mcode-pilot/utils/filterTreatmentData.js
@@ -22,7 +22,7 @@ function isSimilarPatient(treatmentDataPatient, similarPatientProps) {
                 const value = _.lowerCase(options[option].value);
 
                 // demographics
-                const { demographics, diseaseStatus , tumorMarkers, treatments} = treatmentDataPatient;
+                const { demographics, diseaseStatus, tumorMarkers, treatments} = treatmentDataPatient;
                 const { race, gender, birthDate } = demographics;
 
                 if (option === 'age') {
@@ -45,35 +45,34 @@ function isSimilarPatient(treatmentDataPatient, similarPatientProps) {
                 } else if (option === 'gender' && value !== _.lowerCase(gender)) {
                     return false;
                 } else if (option === 'ER' && (!tumorMarkers.er || _.lowerCase(tumorMarkers.er) !== _.lowerCase(value))) {
-                     return false
+                    return false;
                 } else if (option === 'PR' && (!tumorMarkers.pr || _.lowerCase(tumorMarkers.pr) !== _.lowerCase(value))) {
-                     return false
+                    return false;
                 } else if (option === 'HER2' && (!tumorMarkers.her2 || _.lowerCase(tumorMarkers.her2) !== _.lowerCase(value))) {
-                     return false
+                    return false;
                 } else if (option === 'stage' && (!diseaseStatus.stage || _.lowerCase(diseaseStatus.stage) !== _.lowerCase(value))) {
-                     return false
+                    return false;
                 } else if (option === 'grade' && (!diseaseStatus.grade || diseaseStatus.grade !== value)) {
-                     return false
+                    return false;
                 }  else if (option === 'receivedRadTherapy') {
-                    let hadTreatmentOption = (value === 'yes')
-                    let hadTreatment = treatments.includes('radiation')
-                    if((!hadTreatmentOption && hadTreatment) || (hadTreatmentOption && !hadTreatment)){
-                      return false;
+                    let hadTreatmentOption = value === 'yes';
+                    let hadTreatment = treatments.includes('radiation');
+                    if ((!hadTreatmentOption && hadTreatment) || (hadTreatmentOption && !hadTreatment)) {
+                        return false;
                     }
                 } else if (option === 'receivedChemo') {
-                    let hadTreatmentOption = (value === 'yes')
-                    let hadTreatment = treatments.includes('chemo') || treatments.includes('chemotherapy')
-                    if((!hadTreatmentOption && hadTreatment) || (hadTreatmentOption && !hadTreatment)){
-                      return false;
-                  }
+                    let hadTreatmentOption = value === 'yes';
+                    let hadTreatment = treatments.includes('chemo') || treatments.includes('chemotherapy');
+                    if ((!hadTreatmentOption && hadTreatment) || (hadTreatmentOption && !hadTreatment)) {
+                        return false;
+                    }
                 } else if (option === 'hadSurgery') {
-                    let hadTreatmentOption = (value === 'yes')
-                    let hadTreatment = treatments.includes('surgery')
-                    if((!hadTreatmentOption && hadTreatment) || (hadTreatmentOption && !hadTreatment)){
-                      return false;
+                    let hadTreatmentOption = value === 'yes';
+                    let hadTreatment = treatments.includes('surgery');
+                    if ((!hadTreatmentOption && hadTreatment) || (hadTreatmentOption && !hadTreatment)) {
+                        return false;
                     }
                 }
-
             }
         }
     }
