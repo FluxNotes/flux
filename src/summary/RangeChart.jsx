4ba1030007e471f72a5cf394608e6a2eea8caa9d
sgober <sgober@mitre.org>
2019-02-14 14:00:06 -0500

    added format for new medication visualizer line, range and tick marks

--- src/summary/RangeChart.jsx
+++ src/summary/RangeChart.jsx
@@ -8,99 +8,133 @@ import './RangeChart.css';
 class RangeChart extends Component {
 
     render() {
+        console.log('lower value: ' + this.props.lowerValue);
+        console.log('upper value: ' + this.props.upperValue);
+        console.log('typical value: ' + this.props.typicalValue);
+        console.log('value: ' + this.props.value);
 
         // Calculate typical value tick mark placement
         const lineLengthPixels = 240;
         // x position in pixels for where the line begins
         const lineStartXPixels = 10;
+        // calculate padding around lower and upper values (typical range)
+        const lowerValuePadding = (this.props.lowerValue === 0) ? 
+            ((this.props.upperValue - this.props.lowerValue) / - 10): (this.props.lowerValue - (this.props.lowerValue / 10));
+        const upperValuePadding = this.props.upperValue + (this.props.upperValue / 10);
+        // calculate the lower & upper bound values for the graph which takes into account the actual value of the dosage
+        const lowerBound = (this.props.value > lowerValuePadding) ? lowerValuePadding : this.props.value;
+        const upperBound = (this.props.value < upperValuePadding) ? upperValuePadding : this.props.value;
+
         let numberOfPixelsPerUnit;
         if (Lang.isNull(this.props.upperValue) || Lang.isNull(this.props.lowerValue)) {
             numberOfPixelsPerUnit = 0;
         } else {
-            numberOfPixelsPerUnit = lineLengthPixels /(this.props.upperValue - this.props.lowerValue);
+            numberOfPixelsPerUnit = lineLengthPixels / (upperBound - lowerBound);
         }
+
         let typicalValueXPixels;
         if (numberOfPixelsPerUnit === 0 || Lang.isNull(this.props.typicalValue)) {
             typicalValueXPixels = 0;
         } else {
-            typicalValueXPixels = ((this.props.typicalValue - this.props.lowerValue) * numberOfPixelsPerUnit)+ lineStartXPixels; // x=30 is where the line begins
+            typicalValueXPixels = ((this.props.typicalValue - lowerBound) * numberOfPixelsPerUnit) + lineStartXPixels;
         }
 
-        let dotColor = 'black';
-
-        let valueXPixels = null;
-        const distanceFromLineInPixels = 12;
-
-        let svgForDataPoint;
-        // if value is null, don't plot a dot for it
-        if (Lang.isNull(this.props.value)) {
-            svgForDataPoint = null;
+        let lowerValueXPixels, upperValueXPixels;
+        if (numberOfPixelsPerUnit === 0 || Lang.isNull(this.props.lowerValue) || Lang.isNull(this.props.upperValue)) {
+            lowerValueXPixels = 0;
+            upperValueXPixels = 0;
+        } else if (this.props.lowerValue === this.props.upperValue) {
+            let sarah = ((this.props.lowerValue - lowerBound) * numberOfPixelsPerUnit) + lineStartXPixels;
+            lowerValueXPixels = sarah - 5;
+            upperValueXPixels = sarah - 5
         } else {
-            // If value is out of range, set the dot and value color to red and place it outside of the lower or upper mark
-            if (this.props.value < this.props.lowerValue || this.props.value > this.props.upperValue) {
-
-                // set the dot color
-                dotColor = 'red';
-
-                // If the value is below the lower bound, set the x position to the left of the line
-                if (this.props.value < this.props.lowerValue) {
-                    valueXPixels = lineStartXPixels - distanceFromLineInPixels;
-                }
-
-                // Else the value is above the upper bound so set the x position to the right of the line
-                else {
-                    valueXPixels = lineLengthPixels + distanceFromLineInPixels + lineStartXPixels;
-                }
-
-            }
-            // Set the value and dot color to black and calculate where to place it on the line
-            else {
-
-                // set the dot color
-                dotColor = 'black'
-
-                // Calculate the x position of the dot
-                valueXPixels =  ((this.props.value - this.props.lowerValue) * numberOfPixelsPerUnit)+ lineStartXPixels;
-            }
-            svgForDataPoint = <circle cx={valueXPixels} cy="50" r="5" strokeWidth="3" fill={dotColor} />;
+            lowerValueXPixels = ((this.props.lowerValue - lowerBound) * numberOfPixelsPerUnit) + lineStartXPixels;
+            upperValueXPixels = ((this.props.upperValue - lowerBound) * numberOfPixelsPerUnit) + lineStartXPixels;
         }
-        
-        let viewBoxDimensions  = "";
 
+        // TO DO: add dot SVG
+        // let dotColor = 'black';
+
+        // let valueXPixels = null;
+        // const distanceFromLineInPixels = 12;
+
+        // let svgForDataPoint;
+        // // if value is null, don't plot a dot for it
+        // if (Lang.isNull(this.props.value)) {
+        //     svgForDataPoint = null;
+        // } else {
+        //     // If value is out of range, set the dot and value color to red and place it outside of the lower or upper mark
+        //     if (this.props.value < this.props.lowerValue || this.props.value > this.props.upperValue) {
+
+        //         // set the dot color
+        //         dotColor = 'red';
+
+        //         // If the value is below the lower bound, set the x position to the left of the line
+        //         if (this.props.value < this.props.lowerValue) {
+        //             valueXPixels = lineStartXPixels - distanceFromLineInPixels;
+        //         }
+
+        //         // Else the value is above the upper bound so set the x position to the right of the line
+        //         else {
+        //             valueXPixels = lineLengthPixels + distanceFromLineInPixels + lineStartXPixels;
+        //         }
+
+        //     }
+        //     // Set the value and dot color to black and calculate where to place it on the line
+        //     else {
+
+        //         // set the dot color
+        //         dotColor = 'black'
+
+        //         // Calculate the x position of the dot
+        //         valueXPixels =  ((this.props.value - this.props.lowerValue) * numberOfPixelsPerUnit)+ lineStartXPixels;
+        //     }
+        //     svgForDataPoint = <circle cx={valueXPixels} cy="50" r="5" strokeWidth="3" fill={dotColor} />;
+        // }
+        
+        let viewBoxDimensions  = '';
         if (this.props.isWide) {
-            viewBoxDimensions = "10 36 250 110";
+            viewBoxDimensions = '10 36 250 110';
         } else {
-            viewBoxDimensions = "10 10 250 100";
+            viewBoxDimensions = '10 10 250 100';
+        }
+
+        let svgForRangeBar = null;
+        let svgForRangeLowerText = null;
+        let svgForRangeUpperText = null;
+        if (lowerValueXPixels !== 0 && upperValueXPixels !== 0) {
+            svgForRangeBar = <line x1={lowerValueXPixels} y1="50" x2={upperValueXPixels} y2="50" stroke="#DDD" strokeWidth="5" />;
+            if (upperValueXPixels - lowerValueXPixels > 50) {
+                svgForRangeLowerText = <text x={lowerValueXPixels - 3} y="75" fontFamily="sans-serif" fontSize="12px" fill="#333">{this.props.lowerValue}</text>;
+                svgForRangeUpperText = <text x={upperValueXPixels - 3} y="75" fontFamily="sans-serif" fontSize="12px" fill="#333">{this.props.upperValue}</text>;
+            }
         }
         
-        let svgForTypicalTick1, svgForTypicalTick2;
-        if (typicalValueXPixels === 0 || this.props.typicalValue === this.props.lowerValue || this.props.typicalValue === this.props.upperValue) {
-            svgForTypicalTick1 = null;
-            svgForTypicalTick2 = null;
-        } else {
-            svgForTypicalTick1 = <line x1={typicalValueXPixels} y1="40" x2={typicalValueXPixels} y2="60" stroke="#000" strokeWidth="0.5" />;
-            svgForTypicalTick2 = <text x={typicalValueXPixels - 15} y="75" fontFamily="sans-serif" fontSize="12px" fill="#333">{this.props.typicalValue} {this.props.unit}</text>;
+        let svgForTypicalTick = null;
+        let svgForTypicalText = null;
+        if (typicalValueXPixels !== 0) {
+            svgForTypicalTick = <line x1={typicalValueXPixels} y1="45" x2={typicalValueXPixels} y2="55" stroke="#979797" strokeWidth="1" />;
+            if (this.props.typicalValue !== this.props.lowerValue && this.props.typicalValue !== this.props.upperValue) {
+                svgForTypicalText = <text x={typicalValueXPixels - 3} y="75" fontFamily="sans-serif" fontSize="12px" fill="#333">{this.props.typicalValue}</text>;      
+            }
         }
         return (
             <div>
                 <svg width="100%" height="6em" viewBox={viewBoxDimensions}>
                     {/*Main line*/}
-                    <line x1={lineStartXPixels} y1="50" x2={lineStartXPixels + lineLengthPixels} y2="50" stroke="#000" strokeWidth="0.5" />
+                    <line x1={lineStartXPixels} y1="50" x2={lineStartXPixels + lineLengthPixels} y2="50" stroke="#C2C2C2" strokeWidth="0.5" />
 
-                    {/*Lower value tick*/}
-                    <line x1={lineStartXPixels} y1="40" x2={lineStartXPixels} y2="60" stroke="#000" strokeWidth="0.5" />
-                    <text x={lineStartXPixels - 20} y="75" fontFamily="sans-serif" fontSize="12px" fill="#333">{this.props.lowerValue} {this.props.unit}</text>
-
-                    {/*Upper value tick*/}
-                    <line x1={lineStartXPixels + lineLengthPixels} y1="40" x2={lineStartXPixels + lineLengthPixels} y2="60" stroke="#000" strokeWidth="0.5" />
-                    <text x={lineStartXPixels + lineLengthPixels - 20} y="75" fontFamily="sans-serif" fontSize="12px" fill="#333">{this.props.upperValue} {this.props.unit}</text>
+                    {/*Value range bar*/}
+                    {svgForRangeBar}
+                    {svgForRangeLowerText}
+                    {svgForRangeUpperText}
 
                     {/*Typical value tick*/}
-                    {svgForTypicalTick1}
-                    {svgForTypicalTick2}
+                    {svgForTypicalTick}
+                    {svgForTypicalText}
 
                     {/*Data point for the value*/}
-                    {svgForDataPoint}
+                    {/* {svgForDataPoint} */}
                 </svg>
             </div>
         );
