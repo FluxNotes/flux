663c9b92df804ccd5c06534dd71806e626c9fbaf
Noranda Brown <nebrown@mitre.org>
2019-02-27 22:38:47 -0500

    Decoupled similar patients from compass components -- moved to filterTreatmentData util which will eventually be replaced by an API

--- src/mcode-pilot/visualizations/TableLegend/TableLegend.jsx
+++ src/mcode-pilot/visualizations/TableLegend/TableLegend.jsx
@@ -4,21 +4,24 @@ import PropTypes from 'prop-types';
 import './TableLegend.css';
 
 export default class TableLegend extends Component {
-
     render() {
+        const { includedRow } = this.props;
+
         let message;
-        if(this.props.includedRow instanceof Array) {
+        if (Array.isArray(includedRow)) {
             // the row is empty
-            message = "survived with treatment"
-        }else{
-            const name = this.props.includedRow.name;
-            const noTreatment = this.props.treatmentNames.noTreatment===name;
-            const hasAnd = name.indexOf('&') > -1
-            message = "survived with " + (noTreatment?'no treatment':name) + (hasAnd||noTreatment?"":" alone")
+            message = "survived with treatment";
+        } else {
+            const name = includedRow.name;
+            const noTreatment = name === 'none (actively monitoring)';
+            const hasAnd = name.indexOf('&') > -1;
+            message = `survived with ${noTreatment ? 'no treatment' : name} ${hasAnd || noTreatment ? "" : " alone"}`;
         }
-        const mainStyle = {"width": `45px`};
-        const treatmentStyle = {"width":"10px", "backgroundColor":"#91bd7b"};
-        const redTreatmentStyle = {"width": "14px", "background": `repeating-linear-gradient(
+
+        const mainStyle = { "width": "45px" };
+        const treatmentStyle = { "width": "10px", "backgroundColor": "#91bd7b" };
+        const cancerStyle = { "width": "20px" };
+        const redTreatmentStyle = { "width": "14px", "background": `repeating-linear-gradient(
             45deg,
             transparent,
             transparent 2px,
@@ -30,11 +33,11 @@ export default class TableLegend extends Component {
             #FFFFFF,
             #FFFFFF
           )`};
-        const cancerStyle = {"width":"20px"};
+
         return (
             <div className="table-legend">
-            <div className="wrapper">
-                <div className="legend-entry">
+                <div className="wrapper">
+                    <div className="legend-entry">
                         <div className="prog-fill-container">
                             <div className="prog-fill" style={mainStyle}/>
                         </div>
@@ -61,9 +64,7 @@ export default class TableLegend extends Component {
                         </div>
                         <span className="legend-text">cancer related deaths</span>
                     </div>
-                
-            </div>
-
+                </div>
             </div>
         );
     }
@@ -73,6 +74,5 @@ TableLegend.propTypes = {
     includedRow: PropTypes.oneOfType([
         PropTypes.object,
         PropTypes.array
-    ]),
-    treatmentNames: PropTypes.object
+    ])
 };
