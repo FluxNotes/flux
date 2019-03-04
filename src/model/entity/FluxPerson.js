4f996dc53eddc44d5f33a505fd892176569021ad
Dylan Hall <dehall@mitre.org>
2019-03-04 06:55:01 -0500

    update name getter for when nameAsText not present

--- src/model/entity/FluxPerson.js
+++ src/model/entity/FluxPerson.js
@@ -11,9 +11,21 @@ class FluxPerson {
 
     get name() {
         if (!this._person.humanName
-            || this._person.humanName.length < 1
-            || !this._person.humanName[0].nameAsText) return null;
-        return this._person.humanName[0].nameAsText.value;
+             || this._person.humanName.length < 1) return null;
+        const nameObj = this._person.humanName[0];
+        if (nameObj.nameAsText) return nameObj.nameAsText.value;
+
+        // The object doesn't contain a canonical text version of the name, so we'll construct one.
+        // Note that different cultures have different name schemes.
+        // this just uses a western scheme of Given-Family-Suffix
+
+        const namePieces = [];
+
+        if (nameObj.givenName) namePieces.push( ...nameObj.givenName.map(gn => gn.value) );
+        if (nameObj.familyName) namePieces.push( ...nameObj.familyName.map(fn => fn.value) );
+        if (nameObj.suffix) namePieces.push( ...nameObj.suffix.map(s => s.value) );
+
+        return namePieces.join(' ');
     }
 
     get dateOfBirth() {
