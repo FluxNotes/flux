1408c6395605b5ee47600b6c568e354ed4eb5fbc
Dylan Phelan <dtphelan1@gmail.com>
2019-03-07 08:42:01 -0500

    TNM nested in parens after stageGroup

--- src/model/oncocore/FluxTNMStageGroup.js
+++ src/model/oncocore/FluxTNMStageGroup.js
@@ -32,7 +32,7 @@ export default class FluxTNMStageGroup extends FluxEntry {
             }
             return accumulatedString
         }, '');
-        return stageComponentsAsString;
+        return `(${stageComponentsAsString$})`;
     }
 
     /**
