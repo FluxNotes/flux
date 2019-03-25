6cf5fac86904787885d2c01bf3a47ed94761a625
kghoreshi <kghoreshi@mitre.org>
2019-03-25 17:09:36 -0400

    fix jane and add output to patient date  updater

--- src/patient-date-updater/app.js
+++ src/patient-date-updater/app.js
@@ -3,11 +3,11 @@ const program = require('commander');
 const flatten = require('flat');
 const moment = require('moment');
 
-let input;
-let entryid;
+let input, entryid;
 program
-    .usage('<path-to-patient-json> <encounter-entryid>')
-    .arguments('<path-to-patient-json> <encounter-entryid>')
+    .usage('<path-to-patient-json> [encounter-entryid]')
+    .arguments('<path-to-patient-json> [encounter-entryid]')
+    .option('-o, --output', 'Output all dates in the record')
     .action((pathToPatientJson, encounterEntryid) => {
         input = pathToPatientJson;
         entryid = encounterEntryid;
@@ -20,30 +20,39 @@ if (typeof input === 'undefined') {
     console.error('\x1b[31m', 'Missing path to patient JSON', '\x1b[0m');
     program.help();
 }
-
+const output = program.output;
 const patientEntries = JSON.parse(fs.readFileSync(input, 'utf8'));
-const encounter = patientEntries.find(entry => entry.EntryType.Value === 'http://standardhealthrecord.org/spec/shr/encounter/ConsultRequested' && entry.EntryId === entryid);
-
-// Encounter not found in patient entries so exit the program
-if (encounter === undefined) {
-    console.error(`Encounter with entryid ${entryid} not found.`);
-    process.exit(1);
+let encounter;
+if(entryid) {
+    encounter = patientEntries.find(entry => entry.EntryType.Value === 'http://standardhealthrecord.org/spec/shr/encounter/ConsultRequested' && entry.EntryId === entryid);
+    // Encounter not found in patient entries so exit the program
+    if (encounter === undefined) {
+        console.error(`Encounter with entryid ${entryid} not found.`);
+        process.exit(1);
+    }
 }
 
 // Save backup
 fs.writeFileSync(`${input}.backup`, JSON.stringify(patientEntries, null, 4), 'utf8');
 console.log(`Saved backup JSON file to ${input}.backup`);
 
-// encounterDateValue grabs the correct date value based on whether the patient JSON is in mCODE v0.1 or v0.5
-const encounterDateValue = encounter.Encounter.TimePeriod.TimePeriodStart ? encounter.Encounter.TimePeriod.TimePeriodStart.Value : encounter.Encounter.TimePeriod.BeginDateTime.Value;
-const encounterDate = moment(encounterDateValue, 'D MMM YYYY HH:mm ZZ').startOf('day');
-const today = moment().startOf('day');
-const deltaDuration = moment.duration(today.diff(encounterDate));
+let today, deltaDuration;
+if(encounter) {
+    // encounterDateValue grabs the correct date value based on whether the patient JSON is in mCODE v0.1 or v0.5
+    const encounterDateValue = encounter.Encounter.TimePeriod.TimePeriodStart ? encounter.Encounter.TimePeriod.TimePeriodStart.Value : encounter.Encounter.TimePeriod.BeginDateTime.Value;
+    const encounterDate = moment(encounterDateValue, 'D MMM YYYY HH:mm ZZ').startOf('day');
+    today = moment().startOf('day');
+    deltaDuration = moment.duration(today.diff(encounterDate));
+}
+
 
 patientEntries.forEach((entry, i) => {
     // flatten each entry to have only one level of properties to make it easier to traverse
     const flattenedEntry = flatten(entry);
     let change = false;
+    if(output) {
+        console.log("\u001b[1;37m "+flattenedEntry.EntryId +'\t' + flattenedEntry["EntryType.Value"].split('/').slice(-1)[0]);
+    }
 
     for (const key in flattenedEntry) {
         const value = flattenedEntry[key];
@@ -55,22 +64,54 @@ patientEntries.forEach((entry, i) => {
         if (moment(value, 'DD MMM YYYY', true).isValid()) {
             const date = moment(value, 'DD MMM YYYY');
 
-            date.add(deltaDuration);
-            flattenedEntry[key] = date.format('DD MMM YYYY');
-            change = true;
+
+            if(encounter) {
+                date.add(deltaDuration);
+                flattenedEntry[key] = date.format('DD MMM YYYY');
+
+                if(date > today && output) {
+                    console.log(`\u001b[1;31m \t${key}: ${value}`);
+                }else if(output){
+                    console.log(`\u001b[1;37m \t${key}: ${value}`);
+                }
+                change = true;
+            }else if(output){
+                console.log(`\u001b[1;37m \t${key}: ${value}`);
+            }
+
         } else if (moment(value, 'D MMM YYYY', true).isValid()) {
             const date = moment(value, 'D MMM YYYY');
-
-            date.add(deltaDuration);
             flattenedEntry[key] = date.format('DD MMM YYYY');
-            change = true;
+
+            if(encounter) {
+                date.add(deltaDuration);
+                if(date > today && output) {
+                    console.log(`\u001b[1;31m \t${key}: ${value}`);
+                }else if(output){
+                    console.log(`\u001b[1;37m \t${key}: ${value}`);
+                }
+                change = true;
+            }else if(output){
+                console.log(`\u001b[1;37m \t${key}: ${value}`);
+            }
+
         } else if (moment(value, 'D MMM YYYY HH:mm ZZ', true).isValid()) {
             const date = moment(value, 'D MMM YYYY HH:mm ZZ');
 
-            // Keep encounter time
-            date.add(deltaDuration.asDays(), 'd');
-            flattenedEntry[key] = date.format('D MMM YYYY HH:mm ZZ');
-            change = true;
+            if(encounter) {
+                // Keep encounter time
+                date.add(deltaDuration.asDays(), 'd');
+                flattenedEntry[key] = date.format('D MMM YYYY HH:mm ZZ');
+
+                if(date > today && output) {
+                    console.log(`\u001b[1;31m \t${key}: ${value}`);
+                }else if(output){
+                    console.log(`\u001b[1;37m \t${key}: ${value}`);
+                }
+                change = true;
+            }else if(output) {
+                console.log(`\u001b[1;37m \t${key}: ${value}`);
+            }
         }
     }
 
