3a70ec18228518d8c629b79d997ebd02cb938ff1
Julia Afeltra <jafeltra@mitre.org>
2019-04-02 17:05:33 -0400

    Support initiating shortcuts as incomplete items
    - Calculate if initiating shortcut is complete
    - Calculate if attribute shortcut is complete
    - Add concept of isRequired to child shortcuts
    - Support rendering incomplete shortcuts in editor with red underline

--- src/shortcuts/SingleHashtagKeyword.jsx
+++ src/shortcuts/SingleHashtagKeyword.jsx
@@ -1,5 +1,5 @@
 import EntryShortcut from './EntryShortcut';
-
+import Lang from 'lodash';
 export default class SingleHashtagKeyword extends EntryShortcut {
     constructor(onUpdate, metadata, patient, shortcutData) {
         super(metadata);
@@ -8,5 +8,39 @@ export default class SingleHashtagKeyword extends EntryShortcut {
         this._initializeValueObject(metadata, patient, shortcutData);
         this._initializeValueObjectAttributes(metadata);
         this.setAttributeValue = this.setAttributeValue.bind(this);
+    }
+
+    hasRequiredAndIncompleteVOA() {
+        const voaList = this.metadata["valueObjectAttributes"];
+        let value, isSettable, isRequired;
+        let result = false;
+        voaList.forEach((voa) => {
+            value = this.getAttributeValue(voa.name);
+            // Shortcuts.json defines isSettable as a string true
+            isSettable = Lang.isUndefined(voa.isSettable) ? false : (voa.isSettable === "true");
+            isRequired = Lang.isUndefined(voa.isRequired) ? false : voa.isRequired;
+            if (!isRequired) {
+                return;
+            } else {
+                if (isSettable) {
+                    if (Lang.isArray(value) && value.length === 0) {
+                        result = true;
+                        return;
+                    } else if (!this.getAttributeIsSet(voa.name)) {
+                        result = true;
+                        return;
+                    }
+                }
+            }
+        });
+        return result;
+    }
+
+    get isComplete() {
+        return !this.hasRequiredAndIncompleteVOA();
+    }
+
+    isChildRequired(child) {
+        return this.valueObjectAttributes[child.metadata.parentAttribute].isRequired;
     }
 }
