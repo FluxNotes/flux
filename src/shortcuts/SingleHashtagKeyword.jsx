10201a249349d31e1d46dfed05e3bf825110af43
Matthew Gramigna <mgramigna@mitre.org>
2019-04-05 11:55:11 -0400

    Initiating shortcuts are complete if they have any children, incomplete otherwise

--- src/shortcuts/SingleHashtagKeyword.jsx
+++ src/shortcuts/SingleHashtagKeyword.jsx
@@ -11,32 +11,6 @@ export default class SingleHashtagKeyword extends EntryShortcut {
         this.setAttributeValue = this.setAttributeValue.bind(this);
     }
 
-    hasRequiredAndIncompleteVOA() {
-        const voaList = this.metadata["valueObjectAttributes"];
-        let value, isSettable, isRequired;
-        let result = false;
-        voaList.forEach((voa) => {
-            value = this.getAttributeValue(voa.name);
-            // Shortcuts.json defines isSettable as a string true
-            isSettable = Lang.isUndefined(voa.isSettable) ? false : (voa.isSettable === "true");
-            isRequired = Lang.isUndefined(voa.isRequired) ? false : voa.isRequired;
-            if (!isRequired) {
-                return;
-            } else {
-                if (isSettable) {
-                    if (Lang.isArray(value) && value.length === 0) {
-                        result = true;
-                        return;
-                    } else if (!this.getAttributeIsSet(voa.name)) {
-                        result = true;
-                        return;
-                    }
-                }
-            }
-        });
-        return result;
-    }
-
     getAttributeIsSetByLabel(name) {
         return this.isSetByLabel[name];
     }
@@ -46,10 +20,6 @@ export default class SingleHashtagKeyword extends EntryShortcut {
     }
 
     get isComplete() {
-        return !this.hasRequiredAndIncompleteVOA();
-    }
-
-    isChildRequired(child) {
-        return this.valueObjectAttributes[child.metadata.parentAttribute].isRequired;
+        return this.hasChildren();
     }
 }
