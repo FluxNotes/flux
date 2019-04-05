92c511cc81a2a5b98e179fde527ac046739d06f1
Dan Lee <daniellee@mitre.org>
2019-04-05 09:43:51 -0400

    label insertion removes selectable options from context tray.

--- src/shortcuts/SingleHashtagKeyword.jsx
+++ src/shortcuts/SingleHashtagKeyword.jsx
@@ -3,6 +3,7 @@ import Lang from 'lodash';
 export default class SingleHashtagKeyword extends EntryShortcut {
     constructor(onUpdate, metadata, patient, shortcutData) {
         super(metadata);
+        this.isSetByLabel = {};
         this.onUpdate = onUpdate;
         this.patient = patient;
         this._initializeValueObject(metadata, patient, shortcutData);
@@ -34,6 +35,14 @@ export default class SingleHashtagKeyword extends EntryShortcut {
             }
         });
         return result;
+    }
+
+    getAttributeIsSetByLabel(name) {
+        return this.isSetByLabel[name];
+    }
+
+    setAttributeIsSetByLabel(name, val) {
+        this.isSetByLabel[name] = val;
     }
 
     get isComplete() {
