37d0ce30aec1b53dcb7b63103c9273204b32d4f0
Dylan Phelan <dtphelan1@gmail.com>
2019-04-08 08:50:55 -0400

    Update FullApp.test.js

--- src/__test__/backend/views/FullApp.test.js
+++ src/__test__/backend/views/FullApp.test.js
@@ -1055,8 +1055,8 @@ describe('6 FluxNotesEditor', function() {
         expect(deceasedButton).to.have.lengthOf(1);
         deceasedButton.simulate('click');
 
-        expect(notesPanelWrapper.find('.structured-field-creator')).to.have.length(1);
-        expect(notesPanelWrapper.find('.structured-field-creator').text()).to.contain('deceased');
+        expect(notesPanelWrapper.find('.structured-field-creator-incomplete')).to.have.length(1);
+        expect(notesPanelWrapper.find('.structured-field-creator-incomplete').text()).to.contain('deceased');        
 
         const deceasedChild = notesPanelWrapper.find('.context-tray').find('div.context-options-header[title="Date"]');
         expect(deceasedChild).to.have.lengthOf(1);
