237aa2182f911738394afd78db83d81ee69f8821
Dylan Phelan <dtphelan1@gmail.com>
2019-04-08 08:35:05 -0400

    Fixed basic creatorBase's like #deceased

--- src/shortcuts/CreatorBase.jsx
+++ src/shortcuts/CreatorBase.jsx
@@ -3,10 +3,23 @@ import EntryShortcut from './EntryShortcut';
 export default class CreatorBase extends EntryShortcut {
     constructor(onUpdate, metadata, patient, shortcutData) {
         super(metadata);
+        this.isSetByLabel = {};
         this.onUpdate = onUpdate;
         this.patient = patient;
         this._initializeValueObject(metadata, patient, shortcutData);
         this._initializeValueObjectAttributes(metadata);
         this.setAttributeValue = this.setAttributeValue.bind(this);
+    }
+
+    getAttributeIsSetByLabel(name) {
+        return this.isSetByLabel[name];
+    }
+
+    setAttributeIsSetByLabel(name, val) {
+        this.isSetByLabel[name] = val;
+    }
+
+    get isComplete() {
+        return this.hasChildren();
     }
 }
