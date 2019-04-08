20de933fe3a0b7bb32fe5d51981c0232b19929fe
Dan Lee <daniellee@mitre.org>
2019-04-08 11:03:55 -0400

    Added getters for FluxMedicationChange and FluxMedicationRequested so reduce medication shortcut would correctly show as completed.

--- src/model/medication/FluxMedicationRequested.js
+++ src/model/medication/FluxMedicationRequested.js
@@ -176,6 +176,10 @@ class FluxMedicationRequested extends FluxEntry {
         this._medicationRequested.dosage.doseAmount.value.number.decimal = amount;
     }
 
+    get dose() {
+        return this.amountPerDose ? this.amountPerDose.value : null;
+    }
+
     /*
      *  Getter for timingOfDoses
      *  Returns object with value and units
