351bddf9815317667416174c638284bf5c1ae4e5
Dylan Phelan <dtphelan1@gmail.com>
2019-04-15 11:07:11 -0400

    removed commented code; capture output of insertStructuredField in trans

--- src/notes/FluxNotesEditor.jsx
+++ src/notes/FluxNotesEditor.jsx
@@ -308,7 +308,7 @@ class FluxNotesEditor extends React.Component {
         }
         shortcut.initialContextPosition = initialContextPosition;
 
-        this.insertStructuredFieldTransform(transform, shortcut).collapseToStartOfNextText().apply();
+        transform = this.insertStructuredFieldTransform(transform, shortcut).collapseToStartOfNextText().apply();
         if (shortcut.isComplete === false) {
             this.contextManager.removeShortcutFromContext(shortcut);
             this.contextManager.contextUpdated();
@@ -419,7 +419,6 @@ class FluxNotesEditor extends React.Component {
 
         transform = this.resetShortcutData(shortcut, transform);
         return transform.apply();
-        // return this.insertStructuredFieldTransform(transform, shortcut).collapseToStartOfNextText().focus().apply();
     }
 
     // consider reusing this method to replace code in choseSuggestedShortcut function
