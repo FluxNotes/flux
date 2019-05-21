0fee526f990239980b11c181b90e038fe7450f35
sgober <sgober@mitre.org>
2019-05-21 12:23:43 -0400

    initial move over of code

--- src/context/ContextListOptions.jsx
+++ src/context/ContextListOptions.jsx
@@ -8,17 +8,16 @@ class ContextListOptions extends React.Component {
         return (
             <ul>
                 {contexts.map((context, index) => {
-                    return (
-                        <ContextItem
-                            key={context.key}
-                            index={index}
-                            context={context}
-                            selectedIndex={this.props.selectedIndex}
-                            setSelectedIndex={this.props.setSelectedIndex}
-                            onSelected={this.props.onSelected}
-                            closePortal={this.props.closePortal}
-                            state={state} />
-                    );
+                    return <ContextItem
+                        key={context.key}
+                        index={index}
+                        context={context}
+                        selectedIndex={this.props.selectedIndex}
+                        setSelectedIndex={this.props.setSelectedIndex}
+                        onSelected={this.props.onSelected}
+                        closePortal={this.props.closePortal}
+                        state={state}
+                    />
                 })}
             </ul>
         );
@@ -32,6 +31,6 @@ ContextListOptions.propTypes = {
     selectedIndex: PropTypes.number.isRequired,
     setSelectedIndex: PropTypes.func.isRequired,
     state: PropTypes.object.isRequired,
-};
+}
 
 export default ContextListOptions;
