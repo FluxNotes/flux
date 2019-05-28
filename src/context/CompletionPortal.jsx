14e79c518a8ff8508156c1d6e52e51787e6660f3
Dylan Phelan <dtphelan1@gmail.com>
2019-05-28 15:45:15 -0400

    Separated completion portal from contextListOptions

---
+++ src/context/CompletionPortal.jsx
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import Portal from 'react-portal';
+import PropTypes from 'prop-types';
+import './CompletionPortal.css'
+
+class CompletionPortal extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            style: {}
+        };
+    }
+
+    componentDidMount() {
+        this.adjustPosition();
+    }
+
+    /*
+     * Adjust the rendering position of the menu
+     */
+    adjustPosition = () => {
+        const rect = this.props.getPosition();
+        const tempStyle = {};
+        if (!rect) {
+            tempStyle.display = 'none'
+        } else {
+            tempStyle.position = 'absolute';
+            tempStyle.width = 320;
+            tempStyle.display = 'block';
+            if (window.innerHeight - rect.top < 230) {
+                tempStyle.bottom = `${window.innerHeight - rect.top - window.pageYOffset}px`;
+                tempStyle.left = `${rect.left + window.pageXOffset + 10}px`;
+            } else {
+                tempStyle.top = `${rect.top + window.pageYOffset}px`;
+                tempStyle.left = `${rect.left + window.pageXOffset}px`;
+            }
+        }
+        this.setState({
+            style: tempStyle
+        })
+    }
+
+
+    render () {
+        const CompletionComponent = this.props.completionComponent;
+        const { style } = this.state;
+        return (
+            <Portal
+                closeOnEsc
+                closeOnOutsideClick
+                isOpened={true}
+                onClose={this.props.closePortal}
+            >
+                <div style={style} className="completion-portal" ref="completionPortal">
+                    {this.props.children}
+                </div>
+            </Portal>
+        );
+    }
+}
+
+CompletionPortal.propTypes = {
+    closePortal: PropTypes.func.isRequired,
+    getPosition: PropTypes.func.isRequired,
+}
+
+export default CompletionPortal;
