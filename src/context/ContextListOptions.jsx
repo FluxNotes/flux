a6ac9e6f45899c6a0053b37d3cbc157c769360cb
Dylan Phelan <dtphelan1@gmail.com>
2019-05-28 15:45:15 -0400

    Separated completion portal from contextListOptions

--- src/context/ContextListOptions.jsx
+++ src/context/ContextListOptions.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import Portal from 'react-portal';
 import PropTypes from 'prop-types';
 import ContextItem from './ContextItem';
+import './ContextListOptions.css'
 
 const UP_ARROW_KEY = 38
 const DOWN_ARROW_KEY = 40
@@ -9,15 +9,12 @@ const ENTER_KEY = 13
 const RESULT_SIZE = 5
 
 class ContextListOptions extends React.Component {
-    constructor() { 
-        super();
+    constructor(props) { 
+        super(props);
         this.state = {
             selectedIndex: -1,
             style: {}
         };
-    }
-    componentDidMount() {
-        this.adjustPosition();
     }
 
     componentWillUnmount() { 
@@ -29,33 +26,6 @@ class ContextListOptions extends React.Component {
             selectedIndex
         });
     }
-    
-    /*
-     * Adjust the rendering position of the menu
-     */
-    adjustPosition = () => {
-        // const { style } = this.state
-        // if (!menu || !menu.style) return;
-        const rect = this.props.getPosition();
-        const tempStyle = {};
-        if (!rect) {
-            tempStyle.display = 'none'
-        } else {
-            tempStyle.position = 'absolute';
-            tempStyle.width = 320;
-            tempStyle.display = 'block';
-            if (window.innerHeight - rect.top < 230) {
-                tempStyle.bottom = `${window.innerHeight - rect.top - window.pageYOffset}px`;
-                tempStyle.left = `${rect.left + window.pageXOffset + 10}px`;
-            } else {
-                tempStyle.top = `${rect.top + window.pageYOffset}px`;
-                tempStyle.left = `${rect.left + window.pageXOffset}px`;
-            }
-        }
-        this.setState({
-            style: tempStyle
-        })
-    }
 
     /*
     * Change the menu position based on the amount of places to move
@@ -66,23 +36,22 @@ class ContextListOptions extends React.Component {
         const changePlusOriginalLength = change + this.props.contexts.length;
         const indexAfterWrapping = (this.state.selectedIndex + changePlusOriginalLength) % this.props.contexts.length;
         
-        const portalElement = this.refs.contextPortal;
-        const portalList = portalElement.firstChild;
-        const activeElement = portalList.childNodes[indexAfterWrapping];
-        // console.log('this child? : ', this.refs.contextPortal.firstChild.childNodes[this.state.selectedIndex]);
-        // this.refs.contextPortal.scrollTop = this.refs.contextPortal (this.state.selectedIndex - (numberOfElementsVisible - 1)) * 32 + 10;
-        const portalHeight = portalElement.offsetHeight;
-        const portalScrollTop = portalElement.scrollTop;
+        const list = this.refs.contextListOptions;
+        // Relevant values for the list
+        const listHeight = list.offsetHeight;
+        const listScrollTop = list.scrollTop;
+        // Relevant values for the new active element
+        const activeElement = list.childNodes[indexAfterWrapping];
         const activeElementTop = activeElement.offsetTop;
         const activeElementBottom = activeElement.offsetTop + activeElement.offsetHeight;
         
         // Need to ensure that the current element is within the scroll bounds 
-        if (activeElementBottom > (portalHeight + portalScrollTop)) { 
-            // Scroll isabove the activeElement; We want scrollTop + portalHeight === activeElementBottom; algebra 
-            portalElement.scrollTop = activeElementBottom - portalHeight;
-        } else if (activeElementTop < portalScrollTop) { 
+        if (activeElementBottom > (listHeight + listScrollTop)) { 
+            // Scroll isabove the activeElement; We want scrollTop + listHeight === activeElementBottom; algebra 
+            list.scrollTop = activeElementBottom - listHeight;
+        } else if (activeElementTop < listScrollTop) { 
             // Scroll is below the activeElement; We want scrollTop === activeElementTop
-            portalElement.scrollTop = activeElementTop;
+            list.scrollTop = activeElementTop;
         }
         this.setState({ 
             selectedIndex: indexAfterWrapping
@@ -96,8 +65,7 @@ class ContextListOptions extends React.Component {
         const keyCode = e.which;
         if (keyCode === DOWN_ARROW_KEY || keyCode === UP_ARROW_KEY) {
             e.preventDefault()
-            const height = this.refs.contextPortal.offsetHeight;
-            const numberOfElementsVisible = Math.floor(height/32);
+            e.stopPropagation();
             const positionChange = (keyCode === DOWN_ARROW_KEY) ? 1 : -1; 
             this.changeMenuPosition(positionChange)
         } else if (keyCode === ENTER_KEY && this.state.selectedIndex !== -1) {
@@ -110,31 +78,21 @@ class ContextListOptions extends React.Component {
 
     render() {
         const { contexts, state } = this.props;
-        const { style } = this.state;
         return (
-            <Portal 
-                closeOnEsc 
-                closeOnOutsideClick 
-                isOpened={true} 
-                onClose={this.props.closePortal}
-            >
-                <div style={style} className="context-portal" ref="contextPortal">
-                    <ul>
-                        {contexts.map((context, index) => {
-                            return <ContextItem
-                                key={context.key}
-                                index={index}
-                                context={context}
-                                selectedIndex={this.state.selectedIndex}
-                                setSelectedIndex={this.setSelectedIndex}
-                                onSelected={this.props.onSelected}
-                                closePortal={this.props.closePortal}
-                                state={state}
-                            />
-                        })}
-                    </ul>
-                </div>
-            </Portal>
+            <ul className="context-list-options" ref="contextListOptions">
+                {contexts.map((context, index) => {
+                    return <ContextItem
+                        key={context.key}
+                        index={index}
+                        context={context}
+                        selectedIndex={this.state.selectedIndex}
+                        setSelectedIndex={this.setSelectedIndex}
+                        onSelected={this.props.onSelected}
+                        closePortal={this.props.closePortal}
+                        state={state}
+                    />
+                })}
+            </ul>
         );
     }
 }
