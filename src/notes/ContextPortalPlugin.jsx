d4c7d8a3e03d3418fecaf6eb6aa2c2754c6c4443
Dylan Phelan <dtphelan1@gmail.com>
2019-05-28 16:04:22 -0400

    Fixed linter issues

--- src/notes/ContextPortalPlugin.jsx
+++ src/notes/ContextPortalPlugin.jsx
@@ -1,30 +1,30 @@
-export default function ContextPortalPlugin(opts) { 
+export default function ContextPortalPlugin(opts) {
 
-    const onKeyDown = (...args) => { 
+    const onKeyDown = (...args) => {
         const completionComponent = opts.getCompletionComponent();
         if (completionComponent && completionComponent.onKeyDown) {
             return completionComponent.onKeyDown(...args);
         }
-    }
+    };
 
     const onChange = (state, editor) => {
         // Step one: If we're at the beginning of a line, see if the previous node is a shortcut
         const previousNode = (state.selection.focusOffset === 0) ? state.document.getPreviousSibling(state.selection.focusKey) : null;
         const previousNodeShortcut = previousNode ? previousNode.data.get('shortcut') : null;
         // IF we're right next to a shortcut, that shortcut has a completion component and is incomplete, then let's open that completionComponent
-        if (previousNodeShortcut && previousNodeShortcut.completionComponent && !previousNodeShortcut.isComplete) { 
+        if (previousNodeShortcut && previousNodeShortcut.completionComponent && !previousNodeShortcut.isComplete) {
             // But only if the portal isn't already open
             if (!opts.getCompletionComponent()) {
-                opts.openPortal(previousNodeShortcut.completionComponent)
+                opts.openPortal(previousNodeShortcut.completionComponent);
             }
-        } else if (opts.getCompletionComponent()) { 
+        } else if (opts.getCompletionComponent()) {
             // Else, we should close any portals that were open previously that shouldn't now be open
             opts.closePortal();
         }
-    }
+    };
 
     return {
         onChange,
         onKeyDown,
-    }
+    };
 }
