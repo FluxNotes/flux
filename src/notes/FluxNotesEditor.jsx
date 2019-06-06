e8a1d4ef1bb9aeb2252ad8a824dc2abb533983e6
Dan Lee <daniellee@mitre.org>
2019-06-06 13:10:31 -0400

    Allow insertion of shortcuts that are not currently valid in context.

--- src/notes/FluxNotesEditor.jsx
+++ src/notes/FluxNotesEditor.jsx
@@ -297,25 +297,17 @@ class FluxNotesEditor extends React.Component {
             transform = this.state.state.transform();
         }
 
-        // check if shortcutTrigger is currently valid
-        if (!this.shortcutTriggerCheck(shortcutC, shortcutTrigger)) {
-            return this.insertPlainText(transform, shortcutTrigger);
-        }
         const shortcut = this.props.newCurrentShortcut(shortcutC, shortcutTrigger, text, updatePatient, source);
-        shortcut.initialContextPosition = initialContextPosition;
 
         transform = this.insertStructuredFieldTransform(transform, shortcut).collapseToStartOfNextText();
-<<<<<<< HEAD
         if (shortcut.isComplete === false) {
-=======
-        if (shortcut instanceof InsertValue && shortcut.isComplete === false) {
->>>>>>> 7d926b4f... es lint errors after rebase (heck yeah)
             this.contextManager.removeShortcutFromContext(shortcut);
             this.contextManager.contextUpdated();
         }
         if (!Lang.isNull(shortcut) && shortcut.needToSelectValueFromMultipleOptions() && (Lang.isNull(text) || text.length === 0)) {
             return this.openPortalToSelectValueForShortcut(shortcut, false, transform);
         }
+
         return transform;
     }
 
