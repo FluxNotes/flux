c97885b63378a2d4d00c3c9ebea50a567c87edd4
sgober <sgober@mitre.org>
2019-06-06 09:51:37 -0400

    initial get help menu

---
+++ src/context/ContextGetHelp.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './ContextGetHelp.css';
+
+const UP_ARROW_KEY = 38;
+const DOWN_ARROW_KEY = 40;
+const ENTER_KEY = 13;
+
+class ContextGetHelp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            getHelpSelected: false,
+        };
+    }
+
+    componentWillUnmount() {
+        this.props.closePortal();
+    }
+
+    getHelpSelected = () => {
+        // WHY DOES THIS THROW AN ERROR
+        // needed for case when clicked
+        // this.setState({ getHelpSelected: true });
+        console.log('SELECTED GET HELP: open second portal');
+    }
+
+    /*
+     * Navigate and interact with menu based on button presses
+     */
+    onKeyDown = (e) => {
+        const keyCode = e.which;
+        if (keyCode === DOWN_ARROW_KEY || keyCode === UP_ARROW_KEY) {
+            e.preventDefault();
+            e.stopPropagation();
+            const selected = this.state.getHelpSelected;
+            this.setState({ getHelpSelected: !selected });
+        } else if (keyCode === ENTER_KEY && !this.state.getHelpSelected) {
+            // NOTE: This operations might not work on SyntheticEvents which are populat in react
+            e.preventDefault();
+            e.stopPropagation();
+            this.props.closePortal();
+        }
+    }
+
+    render() {
+        return (
+            <ul className="context-get-help" ref="contextGetHelp">
+                <li
+                    className="context-menu-item"
+                    data-active={this.state.getHelpSelected}
+                    onClick={this.getHelpSelected()}
+                    onMouseEnter={this.getHelpSelected()}
+                >
+                    <span>Get Help </span>
+                </li>
+            </ul>
+        );
+    }
+}
+
+ContextGetHelp.propTypes = {
+    closePortal: PropTypes.func.isRequired
+};
+
+export default ContextGetHelp;
