a7433aa5c0661cc8ddc739c9a9997ee2a6f10840
Alex Shih <ashih@mitre.org>
2019-06-06 09:56:16 -0400

    added icon with new visualizer

---
+++ src/summary/LongitudinalTableVisualizer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Visualizer from './Visualizer';
+
+class LongitudinalTableVisualizer extends Visualizer {
+    render() {
+        return (
+            <div>
+                Hello World!
+            </div>
+        );
+    }
+}
+
+export default LongitudinalTableVisualizer;
