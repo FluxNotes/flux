74cf97f017da451f87d1b49c5e68e3cb37705f82
Julia P-A <juliapa@mitre.org>
2019-06-07 09:04:48 -0400

    commented LongitudinalViewVisualizerTable

--- src/summary/LongitudinalViewVisualizer.jsx
+++ src/summary/LongitudinalViewVisualizer.jsx
@@ -4,26 +4,27 @@ import LongitudinalViewVisualizerTable from './LongitudinalViewVisualizerTable';
 import _ from 'lodash';
 
 class LongitudinalViewVisualizer extends Visualizer {
-    constructor(props){
+
+    constructor(props) {
         super(props);
         console.log(props);
         const a = [1, 2, 4, 4, 3, 3];
         const b = _.uniq(a);
         const c = b.sort(function (n1, n2) {
-            if(n1>n2){
+            if (n1 > n2) {
                 return -1;
-            } else if(n1 < n2){
+            } else if (n1 < n2) {
                 return 1;
-            } else{
+            } else {
                 return 0;
             }
-        })
+        });
         console.log(c);
     }
     render() {
         return (
             <div>
-                <LongitudinalViewVisualizerTable/>
+                <LongitudinalViewVisualizerTable />
             </div>
         );
     }
--- src/summary/LongitudinalViewVisualizerTable.jsx
+++ src/summary/LongitudinalViewVisualizerTable.jsx
@@ -1,52 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Table, { TableHead, TableBody, TableFooter, TableCell, TableRow } from 'material-ui/Table';
 
 
-export default class LongitudinalViewVisualizerTable extends React.Component {
+export default class LongitudinalViewVisualizerTable extends Component {
+
     constructor(props) {
         super(props);
-        this.dates = ['date 1', 'date 2', 'date 3', 'date 4'];
+        this.dates = ['date 1', 'date 2', 'date 3', 'date 4']; //where our array of dates will go (have to combine dates from all mini tables, sort, and delete duplicates)
     }
-    createData(labName, unit, dates, id=0) { //dates is an array of the lab dates
-        return { id, labName, unit, dates };
+    /*
+    Assigns an id to each row
+    */
+    createData(labName, unit, datesOrData, id = 0) { //datesOrData is dates if it's the first row of the table and data if not
+        return { labName, unit, datesOrData, id }; //why is this in squiggly brackets? I think it's an object but there are no key value pairs
     }
 
     render() {
-        const data = [
-            this.createData('Frozen yoghurt', 159, this.dates),
+        //in place of 'Frozen yoghurt' will be the name of the lab (labs[id]?), 159 will be the unit (units[id]?, etc.), this.dates will be the data (or date if first row) (could be an array
+        //of all the data for that lab or could be data[column_number] a bunch of times/for each data column in the row if the data is stored by date/column instead of by lab/row)
+        const tableValues = [ //tableValues is every value in the table, row by row
+            this.createData('Frozen yoghurt', 159, this.dates), //id for this one is 0 since 0 is the default and no id is given in calling createData
             this.createData('Frozen yoghurt', 159, this.dates, 1),
             this.createData('Frozen yoghurt', 159, this.dates, 2),
             this.createData('Frozen yoghurt', 159, this.dates, 3),
             this.createData('Frozen yoghurt', 159, this.dates, 4),
-        ];
+        ]; //there's one this.createData call for each row in the table (for each lab)
 
         return (
-            <div className='tabular-list'>
-                    <Table>
-                        <TableHead>
-                            <TableRow>
-                                <TableCell>Lab Name</TableCell>
-                                <TableCell>Unit</TableCell>
-                                {this.dates.map(function (date) { 
-                                return <TableCell key={date}>{date}</TableCell> }
-                                )}
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {data.map(n => {
-                                return (
-                                    <TableRow key={n.id}>
-                                        <TableCell component="th" scope="row">
-                                            {n.labName}
-                                        </TableCell>
-                                        <TableCell numeric>{n.unit}</TableCell>
-                                        {this.dates.map(function (date) {return <TableCell key={date}>{date}</TableCell> })}
-                                    </TableRow>
-                                );
-                            })}
-                        </TableBody>
-                    </Table>
+            <div className='tabular-list'> {/* tabular-list brings in all the right formatting stuff so that the table format matches the rest of the tables*/}
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Lab Name</TableCell>
+                            <TableCell>Unit</TableCell>
+                            {this.dates.map(function (date) { //makes a new date column-heading for each date in the dates array defined in the constructor
+                                return <TableCell key={date}>{date}</TableCell>;
+                            }
+                            )}
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {tableValues.map(n => {
+                            return (
+                                <TableRow key={n.id}>
+                                    <TableCell component="th" scope="row">
+                                        {n.labName}
+                                    </TableCell>
+                                    <TableCell numeric>{n.unit}</TableCell>
+                                    {this.dates.map(function (date) {
+                                        return <TableCell key={date}>{date}</TableCell>;
+                                    })}
+                                </TableRow>
+                            );
+                        })}
+                    </TableBody>
+                </Table>
             </div>
         );
     }
