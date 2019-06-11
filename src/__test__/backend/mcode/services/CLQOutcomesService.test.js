e062cac4cda97bf1eb2357b03713f463c89d68a3
Rob Dingwell <rob@robdingwell.org>
2019-06-11 16:08:39 -0400

    stubbed out test for CLQ service

---
+++ src/__test__/backend/mcode/services/CLQOutcomesService.test.js
@@ -0,0 +1,28 @@
+
+import {expect} from 'chai';
+import CLQOutcomesService from '../../../../mcode-pilot/services/outcomes/CLQOutcomesService'
+
+
+describe("CLQOutcomesService", () => {
+   
+
+    beforeEach(() => {
+
+    });
+
+    it("Should be able to create demographics filter", () => {
+        throw "un-implmented"
+    });
+
+    it("Should be able to create tumormarker filter filter", () => {
+        throw "un-implmented"
+    });
+
+    it("Should be able to create diagnosis filter", () => {
+        throw "un-implmented"
+    });
+
+    it("Should be able to translate results to rows", () => {
+        throw "un-implmented"
+    });
+  });
