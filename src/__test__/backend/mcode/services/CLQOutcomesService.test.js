fce0ce3cfff3264125b924f86102bf2ea068a168
Rob Dingwell <rob@robdingwell.org>
2019-06-12 15:44:48 -0400

    adding test with mocked out http call.  Updated utility function to deal with equality of objects

--- src/__test__/backend/mcode/services/CLQOutcomesService.test.js
+++ src/__test__/backend/mcode/services/CLQOutcomesService.test.js
@@ -1,5 +1,6 @@
-
-import {expect} from 'chai';
+import {
+    expect
+} from 'chai';
 import _ from 'lodash'
 import CLQOutcomesService from '../../../../mcode-pilot/services/outcomes/CLQOutcomesService'
 import BreastMainTreatmentDiabetesHypertensionJaneV05 from '../../../../dataaccess/BreastMainTreatmentDiabetesHypertensionJaneV05.json';
@@ -9,10 +10,16 @@ import '../../../../model/init.js';
 import expectedFilter from './filter.json'
 import response from './response.json'
 import rows from './rows.json'
+import processed from './processed.json';
+const nock = require('nock');
+
+
 describe("CLQOutcomesService", () => {
-  
-    let clqService = new CLQOutcomesService({});
-    
+
+    let clqService = new CLQOutcomesService({
+        serviceUrl: "http://localhost/outcomes"
+    });
+
     let patient = new PatientRecord(BreastMainTreatmentDiabetesHypertensionJaneV05);
     let similarPatientProps = getProps(patient, patient.getActiveConditions()[0])
     // select everything in the options 
@@ -23,26 +30,91 @@ describe("CLQOutcomesService", () => {
         value.selected = true
     });
 
+    beforeEach(() => {
+        nock.disableNetConnect();
+    })
+
+    afterEach(() => {
+        nock.cleanAll();
+        nock.enableNetConnect(); // just to be sure this doesn't break any other tests anywhere else
+    });
+
     it("Should be able to create demographics filter", () => {
         let clqFilter = clqService.buildDemographicsFilter(similarPatientProps);
-        expect(_.isEqual(clqFilter,expectedFilter.demographics) ).to.be.true
+        expect(_.isEqual(clqFilter, expectedFilter.demographics)).to.be.true
     });
 
     it("Should be able to create tumormarker filter filter", () => {
         let clqFilter = clqService.buildTumorMakersFilter(similarPatientProps);
-        expect(_.isEqual(clqFilter,expectedFilter.tumorMarkers) ).to.be.true
+        expect(_.isEqual(clqFilter, expectedFilter.tumorMarkers)).to.be.true
     });
 
     it("Should be able to create diagnosis filter", () => {
         let clqFilter = clqService.buildDiagnosisFilter(similarPatientProps);
-        expect(_.isEqual(clqFilter,expectedFilter.diagnosis) ).to.be.true
-        
+        expect(_.isEqual(clqFilter, expectedFilter.diagnosis)).to.be.true
+
     });
 
     it("Should be able to translate results to rows", () => {
-        
         let clqResults = clqService.generateOutcomeData(response.outcomes.survival.data)
-        expect(_.isEqual(clqResults,rows) ).to.be.true
-        
+        expect(_.isEqual(clqResults, rows)).to.be.true
     });
-  });
+
+    it("Should be able to process results", () => {
+        let clqResults = clqService.processResults(response, [{
+            "code": "A",
+            "displayName": "A",
+            "codeSystem": "2.16.840.1.113883.6.88",
+            "codeSystemName": "RXNORM"
+        }], [{
+                "code": "A",
+                "displayName": "A",
+                "codeSystem": "2.16.840.1.113883.6.88",
+                "codeSystemName": "RXNORM"
+            },
+            {
+                "code": "B",
+                "displayName": "B",
+                "codeSystem": "2.16.840.1.113883.6.88",
+                "codeSystemName": "RXNORM"
+            }
+        ])
+        expect(_.isEqual(clqResults, processed)).to.be.true
+    });
+
+    it("Should be able to take a request and return results", (done) => {
+
+        const scope = nock('http://localhost/')
+            .post('/outcomes')
+            .reply(200, response)
+
+        let clqResults = clqService.processSimilarPatientOutcomes(similarPatientProps, [{
+            "code": "A",
+            "displayName": "A",
+            "codeSystem": "2.16.840.1.113883.6.88",
+            "codeSystemName": "RXNORM"
+        }], [{
+                "code": "A",
+                "displayName": "A",
+                "codeSystem": "2.16.840.1.113883.6.88",
+                "codeSystemName": "RXNORM"
+            },
+            {
+                "code": "B",
+                "displayName": "B",
+                "codeSystem": "2.16.840.1.113883.6.88",
+                "codeSystemName": "RXNORM"
+            }
+        ]).then((clqResults) => {
+            expect(_.isEqual(clqResults, processed)).to.be.true
+            scope.done();
+            done();
+        }).catch((err) => {
+            console.log(err);
+            scope.done();
+            done();
+        })
+
+
+    });
+});
