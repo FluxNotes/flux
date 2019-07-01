057daf6141f12a9301d6072b4976827186df6ccb
Dan Lee <daniellee@mitre.org>
2019-07-01 11:22:19 -0400

    Removed getText from EntryShortcut and documented definition in Shortcut class.

--- src/notes/StructuredFieldPlugin.jsx
+++ src/notes/StructuredFieldPlugin.jsx
@@ -741,12 +741,12 @@ function createStructuredField(opts, shortcut) {
         return inlines;
     }
 
-    let nodes = [Slate.Text.createFromString(String(shortcut.getText()))];
+    const nodes = [Slate.Text.createFromString(String(shortcut.getDisplayText()))];
     const properties = {
+        nodes,
         type: opts.typeStructuredField,
-        nodes: nodes,
         data: {
-            shortcut: shortcut
+            shortcut
         }
     };
     let sf = Slate.Inline.create(properties);
