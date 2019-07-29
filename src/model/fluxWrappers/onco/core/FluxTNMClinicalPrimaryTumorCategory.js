674968488154ab5fb12d2a2785f6b1a5429cbf48
Dan Lee <daniellee@mitre.org>
2019-07-29 14:54:19 -0400

    changes for TNMStagePanelMember --> CancerStageCategory and child classes.

---
+++ src/model/fluxWrappers/onco/core/FluxTNMClinicalPrimaryTumorCategory.js
@@ -0,0 +1,14 @@
+import FluxCancerStageCategory from '../onco/core/FluxCancerStageCategory';
+import TNMClinicalPrimaryTumorCategory from '../../onco/core/TNMClinicalPrimaryTumorCategory';
+
+class FluxTNMClinicalPrimaryTumorCategory extends FluxCancerStageCategory {
+    constructor(json) {
+        super(json);
+        this._cancerStageCategory = TNMClinicalPrimaryTumorCategory.fromJSON(json);
+        if (!this._cancerStageCategory.entryInfo) {
+            this._cancerStageCategory.entryInfo = this._constructEntry('http://standardhealthrecord.org/spec/onco/core/TNMClinicalPrimaryTumorCategory');
+        }
+    }
+}
+
+export default FluxTNMClinicalPrimaryTumorCategory;
