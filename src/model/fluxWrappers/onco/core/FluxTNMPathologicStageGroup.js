699b31462fd81143ebaef85034d7b1552b175b37
Dan Lee <daniellee@mitre.org>
2019-07-30 11:06:14 -0400

    changes for TNMStageGroup classes.

--- src/model/fluxWrappers/onco/core/FluxTNMPathologicStageGroup.js
+++ src/model/fluxWrappers/onco/core/FluxTNMPathologicStageGroup.js
@@ -1,4 +1,4 @@
-import TNMPathologicStageGroup from './TNMPathologicStageGroup';
+import TNMPathologicStageGroup from '../../../onco/core/TNMPathologicStageGroup';
 import PanelMembers from '../shr/base/PanelMembers';
 import FluxTNMStageGroup from './FluxTNMStageGroup';
 
@@ -7,7 +7,7 @@ class FluxTNMPathologicStageGroup extends FluxTNMStageGroup {
         super();
         this._entry = this._tnmStageGroup = TNMPathologicStageGroup.fromJSON(json);
         if (!this._tnmStageGroup.entryInfo) {
-            this._tnmStageGroup.entryInfo = this._constructEntry('http://standardhealthrecord.org/spec/oncocore/TNMPathologicStageGroup');
+            this._tnmStageGroup.entryInfo = this._constructEntry('http://standardhealthrecord.org/spec/onco/core/TNMPathologicStageGroup');
             this._tnmStageGroup.panelMembers = new PanelMembers();
             this._tnmStageGroup.panelMembers.observation = [];
         }
