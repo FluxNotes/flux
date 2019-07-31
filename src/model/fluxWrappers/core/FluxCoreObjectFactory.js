28f838fef592ca24561fca3755c592727ca71456
Dan Lee <daniellee@mitre.org>
2019-07-31 10:49:50 -0400

    changes for ProcedurePerformed --> Procedure

--- src/model/fluxWrappers/core/FluxCoreObjectFactory.js
+++ src/model/fluxWrappers/core/FluxCoreObjectFactory.js
@@ -3,6 +3,7 @@ import ShrCoreObjectFactory from '../shr/core/ShrCoreObjectFactory';
 import FluxClinicalNote from './FluxClinicalNote';
 import FluxKarnofskyPerformanceStatus from './FluxKarnofskyPerformanceStatus';
 import FluxProcedureRequest from './FluxProcedureRequest';
+import FluxProcedure from './FluxProcedure';
 
 export default class FluxCoreObjectFactory {
     static createInstance(json, type) {
@@ -14,6 +15,7 @@ export default class FluxCoreObjectFactory {
         switch (elementName) {
             case 'ClinicalNote': return new FluxClinicalNote(json);
             case 'KarnofskyPerformanceStatus': return new FluxKarnofskyPerformanceStatus(json);
+            case 'Procedure': return new FluxProcedure(json);
             case 'ProcedureRequest': return new FluxProcedureRequest(json);
             default: return ShrCoreObjectFactory.createInstance(json, type);
         }
