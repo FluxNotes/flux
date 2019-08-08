8facd511c7e45419d33fd6dd9b57b75ab15862b4
Dan Lee <daniellee@mitre.org>
2019-08-08 10:20:02 -0400

    Fixed references.

--- src/model/fluxWrappers/core/FluxMedicationStatement.js
+++ src/model/fluxWrappers/core/FluxMedicationStatement.js
@@ -1,8 +1,8 @@
 import FluxEntry from '../base/FluxEntry';
 import MedicationStatement from '../../shr/core/MedicationStatement';
 import FluxMedicationStatementAfterChange from './FluxMedicationStatementAfterChange';
-import EndDateTime from '../shr/core/EndDateTime';
-import * as codeableConceptUtils from '../CodeableConceptUtils.jsx';
+import EndDateTime from '../../shr/core/EndDateTime';
+import * as codeableConceptUtils from '../../CodeableConceptUtils.jsx';
 import Lang from 'lodash';
 import moment from 'moment';
 import Category from '../../shr/core/Category';
