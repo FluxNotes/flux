7cea5d6ecd63f59530738e6ef28154570ffe5065
Dan Lee <daniellee@mitre.org>
2019-08-12 13:04:09 -0400

    Added entry mapping for TumorMarker.

--- src/model/fluxWrappers/onco/core/FluxTumorMarkerTest.js
+++ src/model/fluxWrappers/onco/core/FluxTumorMarkerTest.js
@@ -4,49 +4,49 @@ import TumorMarkerTest from '../../../onco/core/TumorMarkerTest';
 class FluxTumorMarkerTest extends FluxEntry {
     constructor(json) {
         super(json);
-        this._entry = this._tumorMarker = TumorMarkerTest.fromJSON(json);
-        if (!this._tumorMarker.entryInfo) {
-            this._tumorMarker.entryInfo = this._constructEntry('http://standardhealthrecord.org/spec/onco/core/TumorMarkerTest');
+        this._entry = this._tumorMarkerTest = TumorMarkerTest.fromJSON(json);
+        if (!this._tumorMarkerTest.entryInfo) {
+            this._tumorMarkerTest.entryInfo = this._constructEntry('http://standardhealthrecord.org/spec/onco/core/TumorMarkerTest');
         }
     }
 
     get entryInfo() {
-        return this._tumorMarker.entryInfo;
+        return this._tumorMarkerTest.entryInfo;
     }
 
     set entryInfo(entryInfo) {
-        this._tumorMarker.entryInfo = entryInfo;
+        this._tumorMarkerTest.entryInfo = entryInfo;
     }
 
     get metadata() {
-        return this._tumorMarker.metadata;
+        return this._tumorMarkerTest.metadata;
     }
 
     set metadata(metadata) {
-        this._tumorMarker.metadata = metadata;
+        this._tumorMarkerTest.metadata = metadata;
     }
     
     get relevantTime() {
-        if (!this._tumorMarker.relevantTime) return null;
-        return this._tumorMarker.relevantTime.value;
+        if (!this._tumorMarkerTest.relevantTime) return null;
+        return this._tumorMarkerTest.relevantTime.value;
     }
 
     set relevantTime(relevantTime) {
-        this._tumorMarker.relevantTime = relevantTime;
+        this._tumorMarkerTest.relevantTime = relevantTime;
     }
 
     get dataValue() { 
-        if (!this._tumorMarker.dataValue) return null;
-        return this._tumorMarker.dataValue;
+        if (!this._tumorMarkerTest.dataValue) return null;
+        return this._tumorMarkerTest.dataValue;
     }
 
     set dataValue(dataValue) { 
-        this._tumorMarker.dataValue = dataValue;
+        this._tumorMarkerTest.dataValue = dataValue;
     }
 
     get code() {
-        if (!this._tumorMarker.code) return null;
-        return this._tumorMarker.code;
+        if (!this._tumorMarkerTest.code) return null;
+        return this._tumorMarkerTest.code;
     }
 
     get receptorTypeCodeableConcept() {
@@ -59,11 +59,11 @@ class FluxTumorMarkerTest extends FluxEntry {
 
     // Returns a human-readable display text string
     get receptorType() { 
-        if (!this._tumorMarker.code 
-            || !this._tumorMarker.code.value
-            || !this._tumorMarker.code.value.coding
-            || !this._tumorMarker.code.value.coding[0]) return null;
-        return this._displayTextOrCode(this._tumorMarker.code.value.coding[0]);
+        if (!this._tumorMarkerTest.code 
+            || !this._tumorMarkerTest.code.value
+            || !this._tumorMarkerTest.code.value.coding
+            || !this._tumorMarkerTest.code.value.coding[0]) return null;
+        return this._displayTextOrCode(this._tumorMarkerTest.code.value.coding[0]);
     }
     
     /**
@@ -72,24 +72,24 @@ class FluxTumorMarkerTest extends FluxEntry {
     get status() {
         // TODO: TumorMarkerDataValue.value can be CodeableConcept|Quantity|Ratio
         // this assumes it's only a CodeableConcept
-        if (!this._tumorMarker.dataValue 
-            || !this._tumorMarker.dataValue.value
-            || !this._tumorMarker.dataValue.value.coding
-            || !this._tumorMarker.dataValue.value.coding[0]) return null;
-        return this._displayTextOrCode(this._tumorMarker.dataValue.value.coding[0]);
+        if (!this._tumorMarkerTest.dataValue 
+            || !this._tumorMarkerTest.dataValue.value
+            || !this._tumorMarkerTest.dataValue.value.coding
+            || !this._tumorMarkerTest.dataValue.value.coding[0]) return null;
+        return this._displayTextOrCode(this._tumorMarkerTest.dataValue.value.coding[0]);
     }
 
     get specificFocusOfFinding() {
-        if (!this._tumorMarker.specificFocusOfFinding) return null;
-        return this._tumorMarker.specificFocusOfFinding.value;
+        if (!this._tumorMarkerTest.specificFocusOfFinding) return null;
+        return this._tumorMarkerTest.specificFocusOfFinding.value;
     }
 
     set specificFocusOfFinding(val) {
-        this._tumorMarker.specificFocusOfFinding = val;
+        this._tumorMarkerTest.specificFocusOfFinding = val;
     }
 
     toJSON() {
-        return this._tumorMarker.toJSON();
+        return this._tumorMarkerTest.toJSON();
     }
 }
 
