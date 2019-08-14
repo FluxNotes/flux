12fc4b6fbcb1e796c8284a65d5f776f0c8c13f8e
Dan Lee <daniellee@mitre.org>
2019-08-14 10:53:23 -0400

    Added entry mapping for QuestionAnswer.

--- src/model/fluxWrappers/base/FluxBaseObjectFactory.js
+++ src/model/fluxWrappers/base/FluxBaseObjectFactory.js
@@ -1,7 +1,6 @@
 import { getNamespaceAndName } from '../../json-helper';
 import FluxPatientIdentifier from './FluxPatientIdentifier';
 import ShrBaseObjectFactory from '../../shr/base/ShrBaseObjectFactory';
-import FluxQuestionAnswer from './FluxQuestionAnswer';
 
 export default class FluxBaseObjectFactory {
     static createInstance(json, type, patientRecord) {
@@ -11,7 +10,6 @@ export default class FluxBaseObjectFactory {
         }
         // returns Flux wrapper class if found, otherwise use ShrBaseObjectFactory
         switch (elementName) {
-            case 'QuestionAnswer': return new FluxQuestionAnswer(json, type, patientRecord);
             case 'PatientIdentifier': return new FluxPatientIdentifier(json, type, patientRecord);
             default: return ShrBaseObjectFactory.createInstance(json, type, patientRecord);
         }
