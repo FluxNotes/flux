73847d0401cb44a7fc61118749910403abf300cf
Dan Lee <daniellee@mitre.org>
2019-08-15 18:35:31 -0400

    Added entry mapping for genetic mutation tests.

---
+++ src/model/fluxWrappers/onco/core/FluxGeneticMutationTestResult.js
@@ -0,0 +1,53 @@
+import GeneticMutationTestResult from "../../../onco/core/GeneticMutationTestResult";
+import FluxEntry from "../../base/FluxEntry";
+
+class FluxGeneticMutationTestResult extends FluxEntry {
+    constructor(json) {
+        super();
+
+        this._geneticMutationTestResult = this._entry = GeneticMutationTestResult.fromJSON(json);
+    }
+
+    get entryInfo() {
+        return this._geneticMutationTestResult.entryInfo;
+    }
+
+    get abbreviatedName() {
+        // TODO: Create lookup table for genetic mutation codes?
+        switch (this.mutationCode) {
+            case '405827002': {
+                return 'BRCA1';
+            }
+            case '405828007': {
+                return 'BRCA2';
+            }
+            case 'C104668': {
+                return 'KIT';
+            }
+            case 'C1335201': {
+                return 'PDGFRA';
+            }
+            default: {
+                return this._displayTextOrCode(this._geneticMutationTestResult.mutationTested.variantIdentifier.value.coding[0]);
+            }
+        }
+    }
+
+    get value() {
+        return this._displayTextOrCode(this._geneticMutationTestResult.dataValue.value.coding[0]);
+    }
+
+    get statusSign() {
+        return (this.value === 'Positive' ? '+' : '-');
+    }
+
+    get mutationCode() {
+        return this._geneticMutationTestResult.mutationTested.variantIdentifier.value.coding[0].codeValue.code;
+    }
+
+    toJSON() {
+        return this._geneticMutationTestResult.toJSON();
+    }
+}
+
+export default FluxGeneticMutationTestResult;
