6a1262aa7e5ead4d49766fafffb027ceaa15040f
Matthew Gramigna <mgramigna@mitre.org>
2019-08-15 10:07:42 -0400

    Map blood pressure
    * Added some getters to wrapper for proper viz
    * Changed patient record funtion to use code getter for bp

--- src/model/fluxWrappers/core/FluxBloodPressure.js
+++ src/model/fluxWrappers/core/FluxBloodPressure.js
@@ -5,12 +5,24 @@ class FluxBloodPressure {
         this._bloodPressure = BloodPressure.fromJSON(json);
     }
 
+    get code() {
+        return this._bloodPressure.code.value.coding[0].codeValue.code;
+    }
+
     get dataValue() {
         return this._bloodPressure.dataValue;
     }
 
     get entryInfo() {
         return this._bloodPressure.entryInfo;
+    }
+
+    get relevantTime() {
+        return this._bloodPressure.relevantTime.value;
+    }
+
+    get value() {
+        return this._bloodPressure.components.map(r => r.dataValue.value.number.decimal).join('/');
     }
 
     toJSON() {
