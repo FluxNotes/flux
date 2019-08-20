60cc7337df25eb8736dc2b3d0a6bc61ee8d1fee0
Dylan <dehall@mitre.org>
2019-08-20 13:36:53 -0400

    post rebase fixes

--- src/mcode-pilot/utils/filterTreatmentData.js
+++ src/mcode-pilot/utils/filterTreatmentData.js
@@ -113,9 +113,9 @@ function isSimilarPatient(treatmentDataPatient, fOptions) {
             return false;
         // pathology
         } else if (filter.mcodeElement === 'onco.core.TumorMarkerTest') {
-            const receptorType = reference._tumorMarker._findingTopicCode.codeableConcept.coding[0].code.value;
+            const receptorType = reference._tumorMarkerTest.code.value.coding[0].codeValue.value;
             if (receptorType && (!tumorMarkersLabeled[receptorType]
-                || tumorMarkersLabeled[receptorType].value.code !== reference.findingResult._value._coding[0]._code.code)) {
+                || tumorMarkersLabeled[receptorType].value.code !== reference.dataValue.value.coding[0].codeValue.code)) {
                 return false;
             }
         } else if (filter.mcodeElement === 'onco.core.TNMClinicalStageGroup' && (!diseaseStatus.stage
