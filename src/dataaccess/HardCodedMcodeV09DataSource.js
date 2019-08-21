84ae8aa068b94b6f34048b670cd286abd53a411a
Rob Dingwell <rob@robdingwell.org>
2019-08-21 17:13:05 -0400

    adding extra demo patients and updating hardcoded data source to support using those patients

--- src/dataaccess/HardCodedMcodeV09DataSource.js
+++ src/dataaccess/HardCodedMcodeV09DataSource.js
@@ -1,5 +1,7 @@
 import IDataSource from './IDataSource';
-import BreastMainTreatmentDiabetesHypertensionJaneV09 from './BreastMainTreatmentDiabetesHypertensionJaneV09.json';
+import BreastMainTreatmentDiabetesHypertensionJaneV09_01 from './BreastMainTreatmentDiabetesHypertensionJaneV09.json';
+import BreastMainTreatmentDiabetesHypertensionJaneV09_02 from './BreastMainTreatmentDiabetesHypertensionJaneV09_02.json';
+import BreastMainTreatmentDiabetesHypertensionJaneV09_03 from './BreastMainTreatmentDiabetesHypertensionJaneV09_03.json';
 import PatientRecord from '../patient/PatientRecord.jsx';
 import  * as McodeV05EntryMapper from './McodeV05EntryMapper';
 
@@ -32,16 +34,22 @@ class HardCodedMcodeV09DataSource extends IDataSource {
 
     getPatient(id) {
         let patientJSON;
-        if (BreastMainTreatmentDiabetesHypertensionJaneV09[0].ShrId.Value === id) {
-            patientJSON = BreastMainTreatmentDiabetesHypertensionJaneV09;
-        } else {
+
+        if (BreastMainTreatmentDiabetesHypertensionJaneV09_01[0].ShrId.Value === id) {
+            patientJSON = BreastMainTreatmentDiabetesHypertensionJaneV09_01;
+        } else  if (BreastMainTreatmentDiabetesHypertensionJaneV09_02[0].ShrId.Value === id) {
+            patientJSON = BreastMainTreatmentDiabetesHypertensionJaneV09_02;
+        }  else  if (BreastMainTreatmentDiabetesHypertensionJaneV09_03[0].ShrId.Value === id) {
+            patientJSON = BreastMainTreatmentDiabetesHypertensionJaneV09_03;
+        } 
+        else {
             console.error("loading of patients other than the hard-coded demo patient is not implemented in hard-coded read only data source.");
         }
         return new PatientRecord(McodeV05EntryMapper.mapEntries(patientJSON));
     }
 
     getListOfPatients() {
-        const patients = [ BreastMainTreatmentDiabetesHypertensionJaneV09 ];
+        const patients = [ BreastMainTreatmentDiabetesHypertensionJaneV09_01, BreastMainTreatmentDiabetesHypertensionJaneV09_02, BreastMainTreatmentDiabetesHypertensionJaneV09_03 ];
         return patients.map(p => new PatientRecord(p));
     }
 
