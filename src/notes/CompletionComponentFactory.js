69f355acd24728722519a89dca42784c61d5aecf
Dylan Phelan <dtphelan1@gmail.com>
2019-08-30 18:43:43 -0400

    Fixed spacing; fixed const; fixed scoping bug in CreatorChild

--- src/notes/CompletionComponentFactory.js
+++ src/notes/CompletionComponentFactory.js
@@ -6,19 +6,19 @@ export default class CompletionComponentFactory {
     static createInstance(completionComponentName) {
         // Can't do an instance of with a switch
         switch (completionComponentName) {
-        case "number":
-            console.error(`We don't currently support a completion component for ${completionComponentName}-subtypes; trying to get a completionComponent for ${completionComponentName}`);
-            return null;
-        case "date":
-            return ContextCalendar;
-        case "multi-choice":
-        case "choice":
-            return ContextListOptions;
-        case "menu":
-            return ContextGetHelp;
-        default:
-            console.error(`We don't currently support a completion component for ${completionComponentName}-subtypes; trying to get a completionComponent for ${completionComponentName}`);
-            return null;
+            case "number":
+                console.error(`We don't currently support a completion component for ${completionComponentName}-subtypes; trying to get a completionComponent for ${completionComponentName}`);
+                return null;
+            case "date":
+                return ContextCalendar;
+            case "multi-choice":
+            case "choice":
+                return ContextListOptions;
+            case "menu":
+                return ContextGetHelp;
+            default:
+                console.error(`We don't currently support a completion component for ${completionComponentName}-subtypes; trying to get a completionComponent for ${completionComponentName}`);
+                return null;
         }
     }
 }
